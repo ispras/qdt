648183cbb449b7e4161d2127620470fdee5af7c6
Efimov Vasily <real@ispras.ru>
2015-11-10 16:19:20 +0300

    SysBusDeviceType: improve code generation

--- qemu/__init__.py
+++ qemu/__init__.py
@@ -240,6 +240,10 @@ class SysBusDeviceType(QOMType):
         self.pio_num = pio_num
         self.struct_name = "{}State".format(self.qtn.for_struct_name)
 
+        self.mmio_size_macros = []
+        self.pio_size_macros = []
+        self.pio_address_macros = []
+
         # Define header file
         self.header = Header("hw/%s/%s.h" % (directory, 
             self.qtn.for_header_name))
@@ -281,7 +285,7 @@ class SysBusDeviceType(QOMType):
     __attribute__((unused)) {Struct} *s = {UPPER}(dev);
 """.format(
         Struct = self.state_struct.name,
-        UPPER = self.qtn.for_macros,
+        UPPER = self.type_cast_macro.name,
         ),
             args = [Type.lookup("DeviceState").gen_var("dev", True)],
             static = True,
@@ -296,7 +300,7 @@ class SysBusDeviceType(QOMType):
     __attribute__((unused)) {Struct} *s = {UPPER}(dev);
 """.format(
         Struct = self.state_struct.name,
-        UPPER = self.qtn.for_macros,
+        UPPER = self.type_cast_macro.name,
         ),
             args = [
                 Type.lookup("DeviceState").gen_var("dev", True),
@@ -310,6 +314,8 @@ class SysBusDeviceType(QOMType):
         instance_init_code = ''
         instance_init_used_types = []
         instance_init_used_globals = []
+        
+        mmio_def_size = 0x100
 
         if self.mmio_num > 0:
             instance_init_used_types.extend([
@@ -320,6 +326,13 @@ class SysBusDeviceType(QOMType):
             )
 
         for mmioN in range(0, self.mmio_num):
+            size_macro = Macro(
+                name = self.gen_Ith_mmio_size_macro_name(mmioN),
+                text = "0x%X" % mmio_def_size)
+        
+            self.header.add_type(size_macro)
+            instance_init_used_types.append(size_macro)
+            
             component = self.get_Ith_mmio_id_component(mmioN)
             
             read_func = Type.lookup("MemoryRegionOps_read").use_as_prototype(
@@ -357,12 +370,84 @@ class SysBusDeviceType(QOMType):
             instance_init_used_globals.append(ops)
 
             instance_init_code += """
-    memory_region_init_io(&s->{mmio}, obj, &{ops}, s, TYPE_{UPPER}, 0x1000);
+    memory_region_init_io(&s->{mmio}, obj, &{ops}, s, TYPE_{UPPER}, {size});
     sysbus_init_mmio(SYS_BUS_DEVICE(obj), &s->{mmio});
 """.format(
     mmio = self.state_struct.get_Ith_mmio_name(mmioN),
     ops = self.gen_Ith_mmio_ops_name(mmioN),
-    UPPER = self.qtn.for_macros
+    UPPER = self.qtn.for_macros,
+    size = size_macro.name
+)
+        pio_def_size = 0x4
+        pio_cur_addres = 0x1000
+
+        if self.pio_num > 0:
+            instance_init_used_types.extend([
+                Type.lookup("sysbus_add_io"),
+                Type.lookup("memory_region_init_io"),
+                Type.lookup("Object"),
+                Type.lookup("sysbus_init_ioports")
+                ]
+            )
+
+        for pioN in range(0, self.pio_num):
+            size_macro = Macro(
+                name = self.gen_Ith_pio_size_macro_name(pioN),
+                text = "0x%X" % pio_def_size)
+            address_macro = Macro(
+                name = self.gen_Ith_pio_address_macro_name(pioN),
+                text = "0x%X" % pio_cur_addres)
+            pio_cur_addres += pio_def_size
+            
+            self.header.add_types([size_macro, address_macro])
+            instance_init_used_types.extend([size_macro, address_macro])
+            
+            component = self.get_Ith_pio_id_component(pioN)
+            
+            read_func = Type.lookup("MemoryRegionOps_read").use_as_prototype(
+                name = self.qtn.for_id_name + "_" + component + "_read",
+                body = "    return 0;\n",
+                static = True
+            )
+
+            write_func = Type.lookup("MemoryRegionOps_write").use_as_prototype(
+                name = self.qtn.for_id_name + "_" + component + "_write",
+                static = True
+            )
+
+            self.source.add_types([read_func, write_func])
+
+            ops_init = Initializer(
+                used_types = [read_func, write_func],
+                code = """{{
+    .read = {read},
+    .write = {write}
+}}""".format (
+    read = read_func.name,
+    write = write_func.name
+)
+            )
+
+            ops = Type.lookup("MemoryRegionOps").gen_var(
+                name = self.gen_Ith_pio_ops_name(pioN),
+                pointer = False,
+                initializer = ops_init,
+                static = True
+            )
+
+            self.source.add_global_variable(ops)
+            instance_init_used_globals.append(ops)
+
+            instance_init_code += """
+    memory_region_init_io(&s->{pio}, obj, &{ops}, s, TYPE_{UPPER}, {size});
+    sysbus_add_io(SYS_BUS_DEVICE(obj), {addr}, &s->{pio});
+    sysbus_init_ioports(SYS_BUS_DEVICE(obj), {addr}, {size});
+""".format(
+    pio = self.state_struct.get_Ith_io_name(pioN),
+    ops = self.gen_Ith_pio_ops_name(pioN),
+    UPPER = self.qtn.for_macros,
+    size = size_macro.name,
+    addr = address_macro.name
 )
 
         if self.out_irq_num > 0:
@@ -378,13 +463,54 @@ class SysBusDeviceType(QOMType):
     sysbus_init_irq(SYS_BUS_DEVICE(obj), &s->%s);
 """ % self.state_struct.get_Ith_irq_name(irqN)
 
+        if self.in_irq_num > 0:
+            self.irq_handler = Type.lookup("qemu_irq_handler").\
+use_as_prototype(
+                name = self.qtn.for_id_name + "_irq_handler",
+                static = True,
+                used_types = [
+                    self.state_struct,
+                    self.type_cast_macro],
+                body =  """\
+    __attribute__((unused)) {Struct} *s = {UPPER}(opaque);
+""".format(
+        Struct = self.state_struct.name,
+        UPPER = self.type_cast_macro.name,
+        )   )
 
-        instance_init_used_types.append(self.state_struct)
+            self.source.add_type(self.irq_handler)
+
+            in_irq_macro = Macro(
+                name = "%s_IN_IRQ_NUM" % self.qtn.for_macros,
+                text = "%d" % self.in_irq_num
+                )
+
+            self.header.add_type(in_irq_macro)
+
+            instance_init_code += """
+    qdev_init_gpio_in(DEVICE(obj), {handler}, {irqs});
+""".format(
+    handler = self.irq_handler.name,
+    irqs = in_irq_macro.name
+)
+
+            instance_init_used_types.extend([
+                self.irq_handler,
+                in_irq_macro,
+                Type.lookup("qdev_init_gpio_in"),
+                Type.lookup("DEVICE")
+                ])
+
+
+        instance_init_used_types.extend([
+            self.state_struct,
+            self.type_cast_macro
+            ])
         self.instance_init = Function(
             name = self.gen_instance_init_name(),
             body = """\
     {Struct} *s = {UPPER}(obj);
-{extra_code}
+{extra_code}\
 """.format(
     Struct = self.state_struct.name,
     UPPER = self.qtn.for_macros,
@@ -533,9 +659,28 @@ Type.lookup("void").gen_var("opaque", True),
     def get_Ith_mmio_id_component(self, i):
         return self.state_struct.get_Ith_mmio_name(i)
 
+    def gen_Ith_mmio_size_macro_name(self, i):
+        UPPER = self.get_Ith_mmio_id_component(i).upper()
+        return "%s_%s_SIZE" % (self.qtn.for_macros, UPPER)
+
     def gen_Ith_mmio_ops_name(self, i):
         return self.qtn.for_id_name + "_" \
             + self.get_Ith_mmio_id_component(i) + "_ops"
+
+    def get_Ith_pio_id_component(self, i):
+        return self.state_struct.get_Ith_io_name(i)
+
+    def gen_Ith_pio_ops_name(self, i):
+        return self.qtn.for_id_name + "_" \
+            + self.get_Ith_pio_id_component(i) + "_ops"
+
+    def gen_Ith_pio_size_macro_name(self, i):
+        UPPER = self.get_Ith_pio_id_component(i).upper()
+        return "%s_%s_SIZE" % (self.qtn.for_macros, UPPER)
+
+    def gen_Ith_pio_address_macro_name(self, i):
+        UPPER = self.get_Ith_pio_id_component(i).upper()
+        return "%s_%s_ADDR" % (self.qtn.for_macros, UPPER)
 
     def gen_instance_init_name(self):
         return "%s_instance_init" % self.qtn.for_id_name
