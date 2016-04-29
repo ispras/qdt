916be091d85fa5298a50873b17d66aa802a648e0
Efimov Vasily <real@ispras.ru>
2016-04-29 18:42:59 +0300

    source: add common types: unsigned int, uint8_t and bool

--- source/__init__.py
+++ source/__init__.py
@@ -543,10 +543,16 @@ is not added to a source", self.name)
 Type(name = "void", incomplete = True, base = True)
 Type(name = "int", incomplete = False, base = True)
 Type(name = "unsigned", incomplete = False, base = True)
+Type(name = "unsigned int", incomplete = False, base = True)
 Type(name = "const char", incomplete = False, base = True)
 
 Header("stdint.h", is_global=True).add_types([
     Type(name = "uint64_t", incomplete = False, base = False)
+    , Type(name = "uint8_t", incomplete = False, base = False)
+    ])
+
+Header("stdbool.h", is_global=True).add_types([
+    Type(name = "bool", incomplete = False, base = False)
     ])
 
 class Function(Type):
