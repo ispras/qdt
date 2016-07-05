ac86a5f887ce4b3f9a55f4772bb82b3b33c9147d
Efimov Vasily <real@ispras.ru>
2016-07-05 13:34:05 +0300

    phy-test: rewrite DnD, cross marking, object accounting

--- phy-test.py
+++ phy-test.py
@@ -43,92 +43,75 @@ class CrossTest(CanvasDnD):
     def __init__(self, master):
         CanvasDnD.__init__(self, master)
 
-        self.v1_0 = self.canvas.create_rectangle(
-            100, 100,
-            125, 125,
-            fill = "red",
-            tags = "DnD"
-        )
+        self.segments = []
+        self.crosses = []
+        self.drag_points = {}
+        self.dp_w2 = 10
 
-        self.v1_1 = self.canvas.create_rectangle(
-            100, 200,
-            125, 225,
-            fill = "green",
-            tags = "DnD"
-        )
-
-        self.v2_0 = self.canvas.create_rectangle(
-            200, 100,
-            225, 125,
-            fill = "blue",
-            tags = "DnD"
-        )
-
-        self.v2_1 = self.canvas.create_rectangle(
-            200, 200,
-            225, 225,
-            fill = "brown",
-            tags = "DnD"
-        )
-
-        self.v1 = self.canvas.create_line(
-            0, 0, 0, 0,
-            fill = "orange"
-        )
-
-        self.v2 = self.canvas.create_line(
-            0, 0, 0, 0
-        )
-
-        self.inter = None
+        self.segments.extend([
+            CanvasSegment(self.canvas,
+                Vector(100, 100),
+                Vector(0, 100),
+                fill = "red"
+            ),
+            CanvasSegment(self.canvas,
+                Vector(200, 100),
+                Vector(0, 100),
+                fill = "green"
+            ),
+        ])
 
         self.bind('<<DnDMoved>>', self.dnd_moved)
 
+        self.create_drags()
         self.update()
 
+    def create_drags(self):
+        for s in self.segments:
+            id = self.canvas.create_rectangle(
+                s.x - self.dp_w2, s.y - self.dp_w2,
+                s.x + self.dp_w2, s.y + self.dp_w2,
+                fill = "white",
+                tags = "DnD"
+            )
+            self.drag_points[id] = s, 0
+            id = self.canvas.create_rectangle(
+                s.x + s.d.x - self.dp_w2, s.y + s.d.y - self.dp_w2,
+                s.x + s.d.x + self.dp_w2, s.y + s.d.y + self.dp_w2,
+                fill = "white",
+                tags = "DnD"
+            )
+            self.drag_points[id] = s, 1
+
     def update(self):
-        p1_0 = self.canvas.coords(self.v1_0)[:2]
-        p1_1 = self.canvas.coords(self.v1_1)[:2]
+        for i in self.segments:
+            i.update()
 
-        v1 = Segment(
-            begin = Vector(p1_0[0] + 12.5, p1_0[1] + 12.5),
-            direction = Vector(
-                p1_1[0] - p1_0[0], p1_1[1] - p1_0[1],
+        for c in self.crosses:
+            self.canvas.delete(c)
+
+        crosses = []
+        for idx, s in enumerate(self.segments[:-1]):
+            for s1 in self.segments[idx + 1:]:
+                c = s.Intersects(s1)
+                if c:
+                    crosses.append(c)
+
+        for c in crosses:
+            id = self.canvas.create_oval(
+                c.x - 10, c.y - 10,
+                c.x + 10, c.y + 10
             )
-        )
-
-        p2_0 = self.canvas.coords(self.v2_0)[:2]
-        p2_1 = self.canvas.coords(self.v2_1)[:2]
-
-        v2 = Segment(
-            begin = Vector(p2_0[0] + 12.5, p2_0[1] + 12.5),
-            direction = Vector(
-                p2_1[0] - p2_0[0], p2_1[1] - p2_0[1],
-            )
-        )
-
-        apply(self.canvas.coords, [self.v1] + \
-            [v1.x, v1.y, v1.x + v1.d.x, v1.y + v1.d.y])
-
-        apply(self.canvas.coords, [self.v2] + \
-            [v2.x, v2.y, v2.x + v2.d.x, v2.y + v2.d.y])
-
-        inter_point = v1.Intersects(v2)
-
-        if inter_point:
-            if not self.inter:
-                self.inter = self.canvas.create_oval(0, 0, 0, 0)
-
-            apply(self.canvas.coords, [self.inter] + \
-                [inter_point.x - 10, inter_point.y - 10,
-                 inter_point.x + 10, inter_point.y + 10]
-            )
-        else:
-            if self.inter:
-                self.canvas.delete(self.inter)
-                self.inter = None
+            self.crosses.append(id)
 
     def dnd_moved(self, event):
+        for dp, do in self.drag_points.iteritems():
+            coords = self.canvas.coords(dp)[:2]
+            do[0].SetPoint(
+                Vector(coords[0] + self.dp_w2, coords[1] + self.dp_w2),
+                do[1]
+            )
+
         self.update()
 
 def main():
