d650e7b312f85c9938f3e59e8d256abe6c3a36a2
Efimov Vasily <real@ispras.ru>
2016-07-11 15:50:00 +0300

    update/generate translation (with gettext etc.) script was added

---
+++ update-translation.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python2
+
+from subprocess import call
+import os
+
+files = [
+    "qdc-gui.py"
+]
+
+langs = [
+    "ru_RU",
+    "en_US"
+]
+
+for l in langs:
+    directory = "locale/" + l + "/LC_MESSAGES/"
+    if not os.path.isdir(directory):
+        os.makedirs(directory)
+
+    call(
+        [   "xgettext",
+            "-o", directory + "messages.po",
+        ] + files
+    )
+
+    if not os.path.isfile(directory + "qdc.po"):
+        call(
+            [   "mv",
+                directory + "messages.po",
+                directory + "qdc.po"
+            ]
+        )
+    else:
+        call(
+            [   "msgmerge",
+                "-N", directory + "qdc.po",
+                directory + "messages.po",
+                "-o" , directory + "new.po"
+            ]
+        )
+        call(
+            [   "rm",
+                directory + "messages.po"
+            ]
+        )
+
+    call(
+        [   "mv",
+            directory + "new.po",
+            directory + "qdc.po"
+        ]
+    )
+
+    call(
+        [
+            "msgfmt",
+            "-o", directory + "qdc.mo",
+            directory + "qdc.po"
+        ]
+    )
