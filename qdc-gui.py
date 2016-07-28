4082b86c0b6f9b7165210355b08f501dd74ad57f
Efimov Vasily <real@ispras.ru>
2016-07-28 17:13:01 +0300

    GUI: Undo/redo commands (with hot keys) for machine editing were added

--- qdc-gui.py
+++ qdc-gui.py
@@ -32,6 +32,16 @@ class QDCGUIWindow(VarTk):
                 self.on_delete,
                 key_code = 24, # Q
                 description = _("Shutdown the application.")
+            ),
+            HotKeyBinding(
+                self.undo,
+                key_code = 52, # Z
+                description = _("Revert previous editing.")
+            ),
+            HotKeyBinding(
+                self.redo,
+                key_code = 29, # Y
+                description = _("Make reverted editing again.")
             )
         ])
 
@@ -45,7 +55,24 @@ class QDCGUIWindow(VarTk):
             accelerator = hotkeys.get_keycode_string(self.on_delete)
         )
         menubar.add_cascade(label=_("File"), menu = filemenu)
-    
+
+        self.editmenu = editmenu = VarMenu(menubar, tearoff = False)
+        editmenu.add_command(
+            label = _("Undo"),
+            command = self.undo,
+            accelerator = hotkeys.get_keycode_string(self.undo)
+        )
+        self.undo_idx = editmenu.count - 1
+
+        editmenu.add_command(
+            label = _("Redo"),
+            command = self.redo,
+            accelerator = hotkeys.get_keycode_string(self.redo)
+        )
+        self.redo_idx = editmenu.count - 1
+
+        menubar.add_cascade(label = _("Edit"), menu = editmenu)
+
         self.config(menu = menubar)
 
         # Widget layout
@@ -56,9 +83,38 @@ class QDCGUIWindow(VarTk):
         self.mw = MachineWidget(self, self.mach)
         self.mw.grid(column = 0, row = 0, sticky = "NEWS")
 
+        self.mw.mht.add_on_changed(self.on_changed)
+        self.chack_undo_redo()
+
         self.protocol("WM_DELETE_WINDOW", self.on_delete)
 
         self.on_enter_main_loop_id = self.after(0, self.on_enter_main_loop)
+
+    def chack_undo_redo(self):
+        can_do = self.mw.mht.can_do()
+
+        self.hk.set_enabled(self.redo, can_do)
+        if can_do:
+            self.editmenu.entryconfig(self.redo_idx, state = "normal")
+        else:
+            self.editmenu.entryconfig(self.redo_idx, state = "disabled")
+
+        can_undo = self.mw.mht.can_undo()
+
+        self.hk.set_enabled(self.undo, can_undo)
+        if can_undo:
+            self.editmenu.entryconfig(self.undo_idx, state = "normal")
+        else:
+            self.editmenu.entryconfig(self.undo_idx, state = "disabled")
+
+    def on_changed(self, *args, **kw):
+        self.chack_undo_redo()
+
+    def undo(self):
+        self.mw.mht.undo()
+
+    def redo(self):
+        self.mw.mht.do()
 
     def on_enter_main_loop(self):
         self.on_enter_main_loop_id = None
