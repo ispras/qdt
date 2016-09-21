c1f867d75d9823cf8604420d99b5c7bbcbaa329d
Efimov Vasily <real@ispras.ru>
2016-09-21 17:16:31 +0300

    project: project history tracker machine proxy was added
    The proxy class caches machine reference that reduces amount of operands needed
    for 'stage' mathod invocation

--- qemu/project_editing.py
+++ qemu/project_editing.py
@@ -21,6 +21,23 @@ class ProjectOperation(InverseOperation):
     def __read_set__(self):
         return []
 
+class MachineProxyTracker(object):
+    def __init__(self, project_history_tracker, machine_description):
+        self.pht = project_history_tracker
+        self.mach = machine_description
+
+    def stage(self, op_class, *op_args, **op_kw):
+        return self.pht.stage(
+            op_class,
+            *(op_args + (self.mach,)),
+            **op_kw
+        )
+
+    def __getattr__(self, name):
+        if name == "stage":
+            return MachineProxyTracker.stage
+        else:
+            return getattr(self.pht, name)
 
 class ProjectHistoryTracker(HistoryTracker):
     def __init__(self, project, *args, **kw):
@@ -32,3 +49,6 @@ class ProjectHistoryTracker(HistoryTracker):
             op_class,
             *(op_args + (self.p,)), **op_kw
         )
+
+    def get_machine_proxy(self, machine_description):
+        return MachineProxyTracker(self, machine_description)
