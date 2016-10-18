1b9fff1339f68dd4b728a573f5d6032ccfd382aa
Efimov Vasily <real@ispras.ru>
2016-10-18 18:22:26 +0300

    test script for VarCombobox widget was added

---
+++ varcomboboxtest.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python2
+
+from widgets import \
+    VarCombobox, \
+    VarTk, \
+    VarLabel
+
+from Tkinter import \
+    StringVar
+
+from random import \
+    random
+
+class VarComboboxTest(VarCombobox):
+    def __init__(self, *args, **kw):
+        VarCombobox.__init__(self, *args, **kw)
+        self.after(0, self.on_tick)
+
+    def on_tick(self):
+        for v in self.cget("values"):
+            v.set(str(random()))
+
+        self.after(3000, self.on_tick)
+
+if __name__ == "__main__":
+    root = VarTk()
+
+    root.title(StringVar(value = "VarCombobox test"))
+
+    VarComboboxTest(root,
+        values = [ StringVar() for i in range(0, 10) ]
+    ).pack()
+
+    v = StringVar()
+
+    VarComboboxTest(root,
+        values = [ StringVar() for i in range(0, 10) ],
+        state = "readonly",
+        textvariable = v
+    ).pack()
+
+    VarLabel(root, text = v).pack()
+
+    root.mainloop()
