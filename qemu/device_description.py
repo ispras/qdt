b75336f012ec2daf2ae3fe8071193e2f380f6f06
Efimov Vasily <real@ispras.ru>
2016-11-29 16:38:09 +0300

    PCIExpressDeviceDescription: python serialization was implemented

--- qemu/device_description.py
+++ qemu/device_description.py
@@ -68,6 +68,34 @@ class PCIExpressDeviceDescription(QOMDescription):
         self.subsys = subsys
         self.subsys_vendor = subsys_vendor
 
+    def gen_id_get(self, gen, id):
+        if isinstance(id, PCIId):
+            id = id.id
+
+        gen.write(gen.gen_const(id))
+
+    def __gen_code__(self, gen):
+        gen.reset_gen(self)
+        gen.gen_field('name = ' + gen.gen_const(self.name))
+        gen.gen_field('directory = ' + gen.gen_const(self.directory))
+        gen.gen_field('vendor = ')
+        self.gen_id_get(gen, self.vendor)
+        gen.gen_field('device = ')
+        self.gen_id_get(gen, self.device)
+        gen.gen_field('pci_class = ')
+        self.gen_id_get(gen, self.pci_class)
+        gen.gen_field("irq_num = " + gen.gen_const(self.irq_num))
+        gen.gen_field("mem_bar_num = " + gen.gen_const(self.mem_bar_num))
+        gen.gen_field("msi_messages_num = " +
+            gen.gen_const(self.msi_messages_num)
+        )
+        gen.gen_field("revision = " + gen.gen_const(self.revision))
+        gen.gen_field('subsys = ')
+        self.gen_id_get(gen, self.subsys)
+        gen.gen_field('subsys_vendor = ')
+        self.gen_id_get(gen, self.subsys_vendor)
+        gen.gen_end()
+
     def gen_type(self):
         kw = {}
 
