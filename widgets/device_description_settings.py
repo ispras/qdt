d0b1c1d71d104c5e2ba0f9923b5d3590e421218b
Efimov Vasily <real@ispras.ru>
2016-11-30 20:13:15 +0300

    DeviceDescriptionSettingsWidget: support PCI Id as attribute type

--- widgets/device_description_settings.py
+++ widgets/device_description_settings.py
@@ -2,6 +2,8 @@ from qom_settings import \
     QOMDescriptionSettingsWidget
 
 from qemu import \
+    PCIId, \
+    DOp_SetPCIIdAttr, \
     POp_AddDesc, \
     DOp_SetAttr
 
@@ -20,6 +22,12 @@ from hotkey import \
 
 from common import \
     mlget as _
+
+from obj_ref_var import \
+    ObjRefVar
+
+from pci_id_widget import \
+    PCIIdWidget
 
 class DeviceDescriptionSettingsWidget(QOMDescriptionSettingsWidget):
     def __init__(self, fields_and_names, *args, **kw):
@@ -42,6 +50,15 @@ class DeviceDescriptionSettingsWidget(QOMDescriptionSettingsWidget):
             if val_type is long:
                 v = StringVar()
                 w = HKEntry(f, textvariable = v)
+            elif val_type is PCIId:
+                """ Value of PCI Id could be presented either by PCIId object
+                or by a string. So the actual widget/variable pair will be
+                assigned during refresh. """
+                v = None
+                w = GUIFrame(f)
+                w.grid()
+                w.rowconfigure(0, weight = 1)
+                w.columnconfigure(0, weight = 1)
             else:
                 raise Exception("Not implemented value type")
 
@@ -60,6 +77,39 @@ class DeviceDescriptionSettingsWidget(QOMDescriptionSettingsWidget):
 
             e = getattr(self, "w_" + field)
             e.config(bg = "white")
+        elif val_type is PCIId:
+            if not PCIId.db.built and val is None:
+                # use string values only without database
+                val = ""
+            # use appropriate widget/variable pair
+            if isinstance(val, str):
+                if type(var) is not StringVar:
+                    var = StringVar()
+
+                    setattr(self, "var_" + field, var)
+
+                    frame = getattr(self, "w_" + field)
+
+                    for w in frame.winfo_children():
+                        w.destroy()
+
+                    e = HKEntry(frame, textvariable = var)
+                    e.grid(row = 0, column = 0, sticky = "NEWS")
+            elif val is None or isinstance(val, PCIId):
+                if type(var) is not ObjRefVar:
+                    var = ObjRefVar()
+
+                    setattr(self, "var_" + field, var)
+
+                    frame = getattr(self, "w_" + field)
+
+                    for w in frame.winfo_children():
+                        w.destroy()
+
+                    w = PCIIdWidget(var, frame)
+                    w.grid(row = 0, column = 0, sticky = "NEWS")
+
+            var.set(val)
         else:
             raise Exception("Not implemented value type")
 
@@ -87,6 +137,8 @@ class DeviceDescriptionSettingsWidget(QOMDescriptionSettingsWidget):
                     continue
                 else:
                     w.config(bg = "white")
+            elif val_type is PCIId:
+                pass
             else:
                 raise Exception("Not implemented value type")
 
@@ -97,6 +149,35 @@ class DeviceDescriptionSettingsWidget(QOMDescriptionSettingsWidget):
                     self.pht.stage(DOp_SetAttr,
                         field, new_val, self.desc
                     )
+            elif val_type is PCIId:
+                if new_val == "":
+                    new_val = None
+
+                if isinstance(new_val, str):
+                    # Was type of value changed?
+                    if isinstance(cur_val, str):
+                        if cur_val != new_val:
+                            self.pht.stage(DOp_SetAttr, field, new_val,
+                                self.desc
+                            )
+                    else:
+                        if cur_val is not None:
+                            self.pht.stage(DOp_SetPCIIdAttr, field, None,
+                                self.desc
+                            )
+                        self.pht.stage(DOp_SetAttr, field, new_val, self.desc)
+                else:
+                    if cur_val is None or isinstance(cur_val, PCIId):
+                        if cur_val is not new_val:
+                            self.pht.stage(DOp_SetPCIIdAttr, field, new_val,
+                                self.desc
+                            )
+                    else:
+                        self.pht.stage(DOp_SetAttr, field, None, self.desc)
+                        if new_val is not None:
+                            self.pht.stage(DOp_SetPCIIdAttr, field, new_val,
+                                self.desc
+                            )
             else:
                 raise Exception("Not implemented value type")
 
