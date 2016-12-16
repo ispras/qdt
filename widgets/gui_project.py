f3bd3b9295b398b9a0a6219f0e03b476f4d61e64
Efimov Vasily <real@ispras.ru>
2016-12-16 18:18:16 +0300

    GUIProject: use GUILayout

--- widgets/gui_project.py
+++ widgets/gui_project.py
@@ -8,48 +8,85 @@ from gui_proj_ht import \
 from common import \
     History
 
+from gui_layout import \
+    GUILayout
+
+from itertools import \
+    count
+
 class GUIProject(QProject):
     def __init__(self, layouts = [], build_path = None, **kw):
         QProject.__init__(self, **kw)
 
         self.build_path = build_path
-        self.layouts = layouts
+
+        self.layouts = {}
+        for l in layouts:
+            # backward compatibility
+            if isinstance(l, tuple):
+                self.add_layout(*l)
+                continue
+
+            dn = l.desc_name
+            try:
+                l_dict = self.layouts[dn]
+            except KeyError:
+                self.layouts[dn] = l_dict = {}
+            l_dict[l.lid] = l
+
         self.pht = GUIProjectHistoryTracker(self, History())
 
-    def add_layout(self, desc_name, layout):
-        self.layouts.append((desc_name, layout))
+    def add_layout_object_auto_id(self, l):
+        try:
+            l_dict = self.layouts[l.desc_name]
+        except KeyError:
+            self.layouts[l.desc_name] = l_dict = {}
+        for lid in count(0):
+            if lid not in l_dict:
+                l.lid = lid
+                l_dict[lid] = l
+                break
+
+    def add_layout(self, desc_name, layout_opaque):
+        l = GUILayout(desc_name, layout_opaque)
+        self.add_layout_object_auto_id(l)
 
     def delete_layouts(self, desc_name):
-        # filter out existing layouts for the description
-        new_layouts = [
-            (name, l) for name, l in self.layouts if name != desc_name
-        ]
-        self.layouts = new_layouts
+        del self.layouts[desc_name]
 
     # replaces all layouts for description with new layout
-    def set_layout(self, desc_name, layout):
+    def set_layout(self, desc_name, layout_opaque):
         self.delete_layouts(desc_name)
-        self.add_layout(desc_name, layout)
+        self.add_layout(desc_name, layout_opaque)
 
-    def set_layouts(self, desc_name, layouts):
+    def set_layouts(self, desc_name, layout_opaques):
         self.delete_layouts(desc_name)
 
-        for l in layouts:
-            self.add_layout(desc_name, l)
+        for lo in layout_opaques:
+            self.add_layout(desc_name, lo)
 
     def get_layouts(self, desc_name):
-        return [ l for name, l in self.layouts if name == desc_name ]
+        try:
+            lys = self.layouts[desc_name]
+        except KeyError:
+            return []
+        return [ l.opaque for l in lys.values() ]
 
     def get_machine_descriptions(self):
         return [ d for d in self.descriptions if isinstance(d, MachineNode) ]
 
+    def get_all_layouts(self):
+        return [
+            l for ld in self.layouts.values() for l in ld.values()
+        ]
+
     def __children__(self):
-        return list(self.descriptions)
+        return list(self.descriptions) + self.get_all_layouts()
 
     def __gen_code__(self, gen):
         gen.reset_gen(self)
         gen.gen_field("layouts = ")
-        gen.pprint(self.layouts)
+        gen.pprint(self.get_all_layouts())
         gen.gen_field("build_path = " + gen.gen_const(self.build_path))
         gen.gen_field("descriptions = [")
         gen.line()
