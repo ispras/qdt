eeac5d76ecab02d9efdfc53bbf29b82f8067a6d1
Efimov Vasily <real@ispras.ru>
2016-12-16 22:25:07 +0300

    GUILayout: make "shown" flag attribute

--- widgets/gui_layout.py
+++ widgets/gui_layout.py
@@ -2,13 +2,25 @@ from copy import \
     deepcopy
 
 class GUILayout(object):
-    def __init__(self, desc_name, opaque):
+    def __init__(self, desc_name, opaque, shown = None):
         self.desc_name = desc_name
         self.opaque = opaque
         # ID of the layout
         self.lid = None;
         # cached widget reference
         self.widget = None
+
+        try:
+            cfg = self.opaque[-1]
+        except KeyError:
+            self.shown = shown if shown is not None else True
+        else:
+            try:
+                self.shown = cfg["shown"]
+            except KeyError:
+                self.shown = shown if shown is not None else True
+            else:
+                del cfg["shown"]
 
     def __deepcopy__(self, memo):
         ret = type(self)(self.desc_name, deepcopy(self.opaque, memo))
@@ -27,6 +39,7 @@ class GUILayout(object):
         g.gen_field("desc_name = " + g.gen_const(self.desc_name))
         g.gen_field("opaque = ")
         g.pprint(self.opaque)
+        g.gen_field("shown = " + g.gen_const(self.shown))
         g.gen_end()
 
         if self.lid is not None:
