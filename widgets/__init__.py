903eea65cbf257a4bfe0178579f08ce7f23ae325
Efimov Vasily <real@ispras.ru>
2016-12-20 18:52:56 +0300

    GUI ed.: Qemu build path setting operation is defined

--- widgets/__init__.py
+++ widgets/__init__.py
@@ -66,6 +66,7 @@ from project_widget import \
 
 from gui_editing import \
     GUIProjectOperation, \
+        GUIPOp_SetBuildPath, \
         GUIDescriptionOperation, \
             POp_SetDescLayout
 
