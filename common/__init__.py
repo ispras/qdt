02b914dd2308f1f49706758413c7a5a6cf040367
Efimov Vasily <real@ispras.ru>
2017-02-02 20:21:33 +0300

    common: remove alternative Tk unbind  clone

--- common/__init__.py
+++ common/__init__.py
@@ -23,19 +23,6 @@ from inverse_operation import \
     History, \
     HistoryTracker
 
-def unbind(tk_widget, sequence, funcid = None):
-    if funcid is None:
-        tk_widget.tk.call('bind', tk_widget._w, sequence, '')
-    else:
-        binded = tk_widget.tk.call("bind", tk_widget._w, sequence)
-
-        new_binded = '\n'.join([ s for s in binded.split('\n') 
-                             if s and not funcid in s ]) + '\n'
-
-        tk_widget.tk.call("bind", tk_widget._w, sequence, new_binded)
-
-        tk_widget.deletecommand(funcid)
-
 from class_tools import \
     get_class, \
     get_class_defaults, \
