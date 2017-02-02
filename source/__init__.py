5a4d34622ba553b38c4ab1346a30e13381444e18
Efimov Vasily <real@ispras.ru>
2017-02-02 21:20:01 +0300

    source/__init__.py: sanitize imports

--- source/__init__.py
+++ source/__init__.py
@@ -1,2 +1,38 @@
-from model import *
-from base_types import *
+from model import \
+    Source, \
+        Header, \
+    AddTypeRefToDefinerException, \
+    TypeNotRegistered, \
+    Type, \
+        TypeReference, \
+        Structure, \
+        Function, \
+        Pointer, \
+        Macro, \
+    Initializer, \
+    Variable, \
+    Usage, \
+    Operand, \
+        VariableOperand, \
+    Operator, \
+        BinaryOperator, \
+            AssignmentOperator, \
+    CodeNode, \
+    SourceChunk, \
+        HeaderInclusion, \
+        MacroDefinition, \
+        PointerTypeDeclaration, \
+        PointerVariableDeclaration, \
+        VariableDeclaration, \
+        VariableDefinition, \
+        VariableUsage, \
+        StructureDeclarationBegin, \
+        StructureDeclaration, \
+        FunctionDeclaration, \
+        FunctionDefinition, \
+    SourceFile, \
+        HeaderFile, \
+    SourceTreeContainer
+
+from base_types import \
+    add_base_types
