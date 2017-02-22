24e1cf1deec22f9f1c29c3162befda3477e3ba53
Efimov Vasily <real@ispras.ru>
2017-02-22 20:33:43 +0300

    QOMDescription: add char_num attribute

--- qemu/qom_desc.py
+++ qemu/qom_desc.py
@@ -1,8 +1,9 @@
 class QOMDescription(object):
-    def __init__(self, name, directory, timer_num = 0):
+    def __init__(self, name, directory, timer_num = 0, char_num = 0):
         self.name = name
         self.directory = directory
         self.timer_num = timer_num
+        self.char_num = char_num
         self.project = None
 
     def __children__(self):
