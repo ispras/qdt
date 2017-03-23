a6cc7ba36fc53128a3f6f523cd0d34673ec77e0f
Efimov Vasily <real@ispras.ru>
2017-03-23 13:13:12 +0300

    PyGenerator: add method for pretty text printing

--- common/pygen.py
+++ common/pygen.py
@@ -129,6 +129,30 @@ class PyGenerator(object):
 
         self.first_field = True
 
+    def pprint_text(self, text):
+        # Double and single quote count
+        dquote = 0
+        squote = 0
+
+        text_utf8 = text_type('')
+
+        for c in text:
+            if c == '"':
+                dquote += 1
+            elif c == "'":
+                squote += 1
+            elif c == "\\":
+                c = "\\\\"
+
+            text_utf8 += text_type(c)
+
+        if dquote > squote:
+            escaped = text_utf8.replace(u"'", u"\\'")
+            self.write(u"u'" + escaped + u"'")
+        else:
+            escaped = text_utf8.replace(u'"', u'\\"')
+            self.write(u'u"' + escaped + u'"')
+
     def pprint(self, val):
         if isinstance(val, list):
             if not val:
