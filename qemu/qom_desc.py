f1ba784d9564ecdf7f4f152d7a1a71999bfefece
Efimov Vasily <real@ispras.ru>
2017-04-07 15:44:13 +0300

    qemu: inherit ObjectDescription from QOMDescription

--- qemu/qom_desc.py
+++ qemu/qom_desc.py
@@ -12,25 +12,15 @@ class QOMDescription(object):
     def remove_from_project(self):
         self.project.remove_description(self)
 
-class ObjectDescription(object):
+class ObjectDescription(QOMDescription):
     def __init__(self, name, directory,
             block_num = 0,
             timer_num = 0,
             char_num = 0
         ):
+        super(ObjectDescription, self).__init__()
         self.name = name
         self.directory = directory
         self.timer_num = timer_num
         self.char_num = char_num
         self.block_num = block_num
-        self.project = None
-
-    def __children__(self):
-        return []
-
-    def gen_type(self):
-        raise Exception("Attempt to create type model from interface type " \
-                        + str(self.__class__) + ".")
-
-    def remove_from_project(self):
-        self.project.remove_description(self)
