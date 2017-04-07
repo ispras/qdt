cde845093b4794b1d157c08fb61eee7f794bdbc6
Efimov Vasily <real@ispras.ru>
2017-04-07 15:43:26 +0300

    qemu: add QOMDescription class with base functionalities

--- qemu/qom_desc.py
+++ qemu/qom_desc.py
@@ -1,3 +1,17 @@
+class QOMDescription(object):
+    def __init__(self):
+        self.project = None
+
+    def __children__(self):
+        return []
+
+    def gen_type(self):
+        raise Exception("Attempt to create type model from interface type " \
+                        + str(self.__class__) + ".")
+
+    def remove_from_project(self):
+        self.project.remove_description(self)
+
 class ObjectDescription(object):
     def __init__(self, name, directory,
             block_num = 0,
