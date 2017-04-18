42932cde0a71538bc1fc7235ad087def7c684a6d
Efimov Vasily <real@ispras.ru>
2017-04-18 18:33:22 +0300

    TkCoDispatcher: log long iterations

--- widgets/tk_co_dispatcher.py
+++ widgets/tk_co_dispatcher.py
@@ -1,6 +1,11 @@
 from common import \
     CoDispatcher
 
+from time import \
+    time
+
+import sys
+
 class TkCoDispatcher(CoDispatcher):
     """
     The coroutine task dispatcher uses Tk.after method to spread task work
@@ -19,7 +24,20 @@ peaces among Tkinter event driven GUI model.
         self.tk = tk
 
     def iteration(self):
+        t0 = time()
         ready = CoDispatcher.iteration(self)
+        ti = time() - t0
+        if ti > 0.1:
+
+            sys.stderr.write(("Iteration consumed %f sec\n" % ti)
+                + "Active tasks:\n  "
+                + "\n  ".join(
+                    t.generator.__name__ for t in self.active_tasks
+                )
+                + "\n"
+            )
+            # Note that, a task may consume so many time just before a call or
+            # finish. It will not be presented in the list.
 
         self.tk.after(1 if ready else self.wait_msec, self.iteration)
 
