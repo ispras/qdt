036d2112acb59dbb90f97d045e184ad49b6e8290
Efimov Vasily <real@ispras.ru>
2017-09-08 11:52:34 +0300

    git tools: add `CommitDesc` (copy from `qemu.version_description`)

--- common/git_tools.py
+++ common/git_tools.py
@@ -1,2 +1,12 @@
 __all__ = [
+    "CommitDesc"
 ]
+
+class CommitDesc(object):
+    def __init__(self, sha, parents, children):
+        self.sha = sha
+        self.parents = parents
+        self.children = children
+
+        # serial number according to the topological sorting
+        self.num = None
