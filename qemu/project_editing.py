cc1126e7a00ac6943d7a3fd3ec1c9102b63e12eb
Efimov Vasily <real@ispras.ru>
2017-10-20 20:03:32 +0300

    obj. creation helper: use class_tools.get_class to get class object by name
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- qemu/project_editing.py
+++ qemu/project_editing.py
@@ -1,5 +1,6 @@
 from common import \
     mlget as _, \
+    get_class, \
     gen_class_args, \
     get_class_defaults, \
     InverseOperation
@@ -12,9 +13,6 @@ from six import \
     string_types, \
     text_type, \
     integer_types
-
-from importlib import \
-    import_module
 
 from qemu import \
     QOMDescription
@@ -158,10 +156,6 @@ _MyClassName__my_attr in this case. It is Python internals...
             return self.export_value(*valdesc)
 
     def new(self):
-        segments = self._nc.split(".")
-        module, class_name = ".".join(segments[:-1]), segments[-1]
-        Class = getattr(import_module(module), class_name)
-
         args = []
         for n in self.al:
             args.append(self.get_arg(n))
@@ -176,6 +170,7 @@ _MyClassName__my_attr in this case. It is Python internals...
                 val = self.export_value(*valdesc)
                 kw[n] = val
 
+        Class = get_class(self._nc)
         return Class(*args, **kw)
 
     class CannotImport (Exception):
