4835d70cc0d6ce4c57891d5ea380eb7fcbb48d91
Efimov Vasily <real@ispras.ru>
2017-11-01 19:12:08 +0300

    update-translation: auto get list of files to be translated
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- update-translation.py
+++ update-translation.py
@@ -4,31 +4,48 @@ from subprocess import \
     call
 
 from os import \
+    walk, \
     makedirs
 
 from os.path import \
+    dirname, \
     join, \
     isdir, \
     isfile
 
-files = [
-    join("widgets", "device_settings.py"),
-    join("widgets", "device_settings_window.py"),
-    join("widgets", "device_tree_widget.py"),
-    join("widgets", "irq_settings.py"),
-    join("widgets", "machine_diagram_widget.py"),
-    join("widgets", "pci_device_settings.py"),
-    join("widgets", "settings_window.py"),
-    join("widgets", "sysbusdevset.py"),
-    join("widgets", "hotkey.py"),
-    join("widgets", "bus_settings.py"),
-    join("qdc-gui.py"),
-    join("history-test.py")
-]
+from re import \
+    compile
+
+locale_files = []
+
+ml_pattern = compile(" mlget +as +_[, \n]")
+
+for root, dirs, files in walk(dirname(__file__)):
+    for file in files:
+        if file[-3:] != ".py":
+            continue
+
+        file_name = join(root, file)
+        f = open(file_name, "rb")
+        lines = list(f.readlines())
+        f.close()
+
+        for line in lines:
+            if ml_pattern.search(line):
+                break
+        else:
+            continue
+
+        locale_files.append(file_name)
 
 langs = [
     "ru_RU"
 ]
+
+print("Updating *.po file by those files:")
+for f in locale_files:
+    print("    " + f)
+print("...")
 
 for l in langs:
     directory = join("locale", l, "LC_MESSAGES")
@@ -38,7 +55,7 @@ for l in langs:
     call(
         [   "xgettext",
             "-o", join(directory, "messages.po"),
-        ] + files
+        ] + locale_files
     )
 
     if not isfile(join(directory, "qdc.po")):
