831012389c3c616755a5b191dc32efc5e753c760
Efimov Vasily <real@ispras.ru>
2017-12-05 19:12:11 +0300

    common.__init__.py: sort imports
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- common/__init__.py
+++ common/__init__.py
@@ -1,41 +1,21 @@
-from .topology import *
-
-from .pygen import *
-
-from .ml import *
-
-from .co_dispatcher import *
-
-from .inverse_operation import *
-
 from .class_tools import *
-
-from .reflection import *
-
-from .visitor import *
-
-from .search_helper import *
-
-from .variable import *
-
-from .formated_string_var import *
-
-from .extra_math import *
-
+from .co_dispatcher import *
 from .co_signal import *
-
 from .compat import *
-
-from .version import *
-
-from .ordered_set import *
-
-from .legacy import *
-
-from .os_wrappers import *
-
-from .notifier import *
-
-from .mechanics import *
-
+from .extra_math import *
+from .formated_string_var import *
 from .git_tools import *
+from .inverse_operation import *
+from .legacy import *
+from .mechanics import *
+from .ml import *
+from .notifier import *
+from .ordered_set import *
+from .os_wrappers import *
+from .pygen import *
+from .reflection import *
+from .search_helper import *
+from .topology import *
+from .variable import *
+from .version import *
+from .visitor import *
