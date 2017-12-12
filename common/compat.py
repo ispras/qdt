422325cd52288f06b6230ce658a4dc0b2eafa236
Efimov Vasily <real@ispras.ru>
2017-12-12 18:52:55 +0300

    common: `execfile` adds (and then removes) file directory to sys.path
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- common/compat.py
+++ common/compat.py
@@ -1,3 +1,7 @@
+from sys import path as py_path
+from os.path import split
+from os import getcwd
+
 def execfile(filename, globals = None, locals = None):
     f = open(filename, "rb")
     content = f.read()
@@ -9,4 +13,18 @@ def execfile(filename, globals = None, locals = None):
     globals["__file__"] = filename
     globals["__name__"] = "__main__"
 
-    exec(content, globals, locals)
+    file_path = split(filename)[0]
+
+    if not file_path:
+        file_path = getcwd()
+
+    new_path = file_path not in py_path
+
+    if new_path:
+        py_path.append(file_path)
+
+    try:
+        exec(content, globals, locals)
+    finally:
+        if new_path:
+            py_path.remove(file_path)
