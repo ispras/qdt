72ee4ce206e22f2fa613584c29f3014e56c223fc
Efimov Vasily <real@ispras.ru>
2017-12-19 18:27:25 +0300

    GUIDialog: disable master's hotkeys for operation time
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- widgets/gui_dialog.py
+++ widgets/gui_dialog.py
@@ -6,5 +6,20 @@ class GUIDialog(VarToplevel):
     def __init__(self, *args, **kw):
         VarToplevel.__init__(self, *args, **kw)
 
-        self.transient(self.master)
+        master = self.master
+
+        try:
+            hk = master.winfo_toplevel().hk
+        except:
+            hk = None
+        else:
+            hk.disable_hotkeys()
+            self.bind("<Destroy>", self.__on_destroy, "+")
+            self.hk = hk
+
+        self.transient(master)
         self.grab_set()
+
+    def __on_destroy(self, e, **kw):
+        if e.widget is self:
+            self.hk.enable_hotkeys()
