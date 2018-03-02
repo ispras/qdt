186d59d2b60a96204c8d276e89d24abeec57a6d8
Efimov Vasily <real@ispras.ru>
2018-03-02 17:42:48 +0300

    ply2path-test: warn about presence a global `ply`
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- ply2path-test.py
+++ ply2path-test.py
@@ -1,3 +1,10 @@
+try:
+    from ply import *
+except ImportError:
+    pass
+else:
+    print("WARNING: There is a global ply version. Keep it in mind.")
+
 print(",\n    '".join(str(globals()).split(", '")))
 
 from common.ply2path import *
