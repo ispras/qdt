066718455e3abca4229813a561a3224d79d27c91
Efimov Vasily <laer.18@gmail.com>
2018-04-06 16:09:59 +0300

    logo: add GIF version of logo and use it if PNG is not supported by Tkinter
    Signed-off-by: Efimov Vasily <laer.18@gmail.com>

--- widgets/logo.py
+++ widgets/logo.py
@@ -24,11 +24,17 @@ def set_logo(self):
     if not script_dir:
         script_dir = "."
 
-    logo_file = join(script_dir, "logo.png")
+    png_logo_file = join(script_dir, "logo.png")
 
     try:
         # see: https://stackoverflow.com/questions/18537918/set-window-icon
-        icon = PhotoImage(file = logo_file)
+        try:
+            icon = PhotoImage(file = png_logo_file)
+        except:
+            # Some versions of Tkinter do not support PNG. Use GIF instead.
+            gif_logo_file = join(script_dir, "logo.gif")
+            icon = PhotoImage(file = gif_logo_file)
+
         self.tk.call("wm", "iconphoto", self._w, icon)
     except:
         global reported
