ac04960cee32ea33068cee019f6d5cbb3bc1ff2a
Efimov Vasily <real@ispras.ru>
2018-04-09 19:35:32 +0300

    os_wrappers: `fixpath` helper that fixes UNIX-like paths under Windows
    Signed-off-by: Efimov Vasily <laer.18@gmail.com>

--- common/os_wrappers.py
+++ common/os_wrappers.py
@@ -1,13 +1,20 @@
 __all__ = [
     "remove_file"
+  , "fixpath"
 ]
 
 from os import (
+    sep,
+    name as os_name,
     remove
 )
 from errno import (
     ENOENT
 )
+if os_name == "nt":
+    from re import (
+        compile
+    )
 
 def remove_file(file_name):
     try:
@@ -16,3 +23,21 @@ def remove_file(file_name):
         # errno.ENOENT = no such file or directory
         if e.errno != ENOENT:
             print("Error: %s - %s." % (e.filename, e.strerror))
+
+if os_name == "nt":
+    drive_letter = compile("(/)([a-zA-Z])($|/.*)")
+
+    def fixpath(path):
+        "Fixes UNIX-like paths under Windows normally produced by MSYS."
+        mi = drive_letter.match(path)
+        if mi:
+            tail = mi.group(3)
+            if tail:
+                tail = sep.join(tail.split("/"))
+            else:
+                tail = sep
+            path = mi.group(2) + ":" + tail
+
+        return path
+else:
+    fixpath = lambda x : x
