939c5507bcb1e34bf7ea9de1f69db50e55a22371
Danila Bogomolov <bda@ispras.ru>
2018-05-23 18:26:42 +0300

    Variable: make `separ` as parameter

--- source/model.py
+++ source/model.py
@@ -1280,9 +1280,10 @@ class Variable():
     def get_definition_chunks(self, generator,
         indent = "",
         enum = False,
-        append_nl = True
+        append_nl = True,
+        separ = ";"
     ):
-        ch = VariableDefinition(self, indent, append_nl, enum)
+        ch = VariableDefinition(self, indent, append_nl, enum, separ)
 
         refs = generator.provide_chunks(self.type)
 
@@ -1699,7 +1700,7 @@ class VariableDeclaration(SourceChunk):
 class VariableDefinition(SourceChunk):
     weight = 5
 
-    def __init__(self, var, indent="", append_nl = True, enum = False):
+    def __init__(self, var, indent= "", append_nl = True, enum = False, separ = ";"):
         init_code = ''
         if var.initializer is not None:
             raw_code = var.type.gen_usage_string(var.initializer)
@@ -1723,7 +1724,7 @@ class VariableDefinition(SourceChunk):
         var_name = var.name,
         array_decl = gen_array_declaration(var.array_size),
         init = init_code,
-        separ = "," if enum else ";",
+        separ = separ,
         nl = "\n" if append_nl else ""
     )
             )
