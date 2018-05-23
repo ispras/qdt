884d0ece3a4c7155a2dfe16de2d2c6521627da6f
Danila Bogomolov <bda@ispras.ru>
2018-05-23 18:26:42 +0300

    ChunkGenerator.provide_chunks: not add newline for Enumeration

--- source/model.py
+++ source/model.py
@@ -117,6 +117,7 @@ class ChunkGenerator(object):
                         chunks = origin.gen_declaration_chunks(self, **kw)
                     elif isinstance(self.stack[-2], Enumeration):
                         kw["enum"] = True
+                        kw["append_nl"] = False
                         chunks = origin.get_definition_chunks(self, **kw)
                     else:
                         chunks = origin.get_definition_chunks(self, **kw)
