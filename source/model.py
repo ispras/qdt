2bd54455badd6846544eb0a733da364d24c6a42f
Efimov Vasily <real@ispras.ru>
2018-06-13 14:32:51 +0300

    source model: a global variable references the header declaring it
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- source/model.py
+++ source/model.py
@@ -240,6 +240,14 @@ class Source(object):
                                 file = s.path
                             ))
                         self.add_inclusion(s)
+        elif type(self) is Header: # exactly a header
+            if var.declarer is not None:
+                raise RuntimeError(
+                    "Variable '%s' is already declared by '%s'" % (
+                        var.name, self.path
+                    )
+                )
+            var.declarer = self
 
         self.global_variables[var.name] = var
 
@@ -1497,6 +1505,8 @@ class Variable(object):
         self.const = const
         self.array_size = array_size
         self.used = used
+        # a header
+        self.declarer = None
 
     def gen_declaration_chunks(self, generator,
         indent = "",
