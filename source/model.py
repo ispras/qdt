4d0193a2017592480919be38ddb2d5565a71104f
Efimov Vasily <real@ispras.ru>
2018-06-14 17:03:39 +0300

    ChunkGenerator: pretty printer for chunks generation process stack
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- source/model.py
+++ source/model.py
@@ -193,6 +193,23 @@ class ChunkGenerator(object):
 
         return list(res)
 
+    def stringify_stack(self):
+        frames = deque()
+        for frame in self.stack:
+            definer = None
+
+            if isinstance(frame, TypeReference):
+                definer = frame.type.definer.path
+            elif isinstance(frame, Type):
+                definer = frame.definer.path
+
+            definer = "" if definer is None else ("  (%s)" % definer)
+
+            frames.append("    %-20s %s%s" % (
+                type(frame).__name__, frame.name, definer
+            ))
+        return "\n".join(frames)
+
 # Source code models
 
 
