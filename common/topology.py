a7ca11e104e37eac799bec995a639a4f14dbb74f
Efimov Vasily <real@ispras.ru>
2018-06-21 17:04:06 +0300

    dfs: do not add `__dfs_visited__` into objects, use visiting(-ed) sets instead
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- common/topology.py
+++ common/topology.py
@@ -6,40 +6,42 @@ __all__ = [
 class GraphIsNotAcyclic(ValueError):
     pass
 
-def dfs(node):
-    try:
-        if node.__dfs_visited__ == 1:
-            raise GraphIsNotAcyclic()
-        # This is not actually needed
-        # elif node.__dfs_visited__ == 2:
-        #     raise StopIteration
-    except AttributeError:
-        node.__dfs_visited__ = 1
-        for n in node.__dfs_children__():
-            for nn in dfs(n):
-                yield nn
-        node.__dfs_visited__ = 2
-        yield node
+
+def dfs(node, visiting, visited):
+    nid = id(node)
+
+    if nid in visiting:
+        raise GraphIsNotAcyclic()
+
+    if nid in visited:
+        return
+
+    visiting.add(nid)
+
+    for n in node.__dfs_children__():
+        for nn in dfs(n, visiting, visited):
+            yield nn
+
+    visiting.remove(nid)
+    visited.add(nid)
+
+    yield node
+
 
 def sort_topologically(roots = []):
     """
-    All objects (nodes) should NOT have attribute __dfs_visited__.
-
     All objects in the trees should implement __dfs_children__ method. This
     method should return list of objects, to which an edge exists, or [], if
     the object is a leaf.
-
-    __dfs_visited__ (for cycle detection):
-        AttributeError - not visited
-        1 - under processing
-        2 - processed
     """
 
     ret = []
+    visiting = set()
+    visited = set()
+
     # reverse sequence returned
     for node in roots:
-        for n in dfs(node):
+        for n in dfs(node, visiting, visited):
             ret.append(n)
-    for node in ret:
-        del node.__dfs_visited__
+
     return ret
