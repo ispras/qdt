0ec2c4ad6dc641d3c39ee33bcf517a58431cd1af
goremykin <oleg.goremikin@yandex.ru>
2018-07-13 19:09:18 +0300

    Enumeration: keep enum type in Variable

--- source/model.py
+++ source/model.py
@@ -969,10 +969,11 @@ class Enumeration(Type):
         super(Enumeration, self).__init__(type_name)
         self.elems = []
         self.enum_name = enum_name
+        int_type = Type.lookup("int")
         for key, val in elems_dict.items():
             self.elems.append(
-                Variable(key, Type.lookup("int"),
-                    initializer = Initializer(str(val))
+                Variable(key, self,
+                    initializer = Initializer(str(val), [int_type])
                 )
             )
 
