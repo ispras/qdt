aad799e666dd775914608b8def2dbcc819e24b5d
Efimov Vasily <laer.18@gmail.com>
2018-08-12 20:52:03 +0300

    vp: use `DnDGroup` & `bbox_center` from `widgets` package
    Signed-off-by: Efimov Vasily <laer.18@gmail.com>

--- vp.py
+++ vp.py
@@ -4,6 +4,8 @@ from common import (
     mlget as _
 )
 from widgets import (
+    bbox_center,
+    DnDGroup,
     VarButton,
     HKEntry,
     VarToplevel,
@@ -34,9 +36,6 @@ from os.path import (
 from traceback import (
     print_exc,
     format_exc
-)
-from six import (
-    integer_types
 )
 from os import (
     rename,
@@ -171,176 +170,10 @@ class Mod(Bin):
         return a % b
 
 
-class DnDGroup(object):
-    def __init__(self, w, anchor_id, items):
-        self.anchor_id = anchor_id
-        self.items = list(items)
-        w.bind("<<DnDDown>>", self.on_dnd_down, "+")
-
-    def on_dnd_down(self, event):
-        w = event.widget
-        a_id = self.anchor_id
-        if w.dnd_dragged != a_id:
-            return
-        self.prev = w.canvas.coords(a_id)[:2]
-        self.__moved = w.bind("<<DnDMoved>>", self.on_dnd_moved, "+")
-        self.__up = w.bind("<<DnDUp>>", self.on_dnd_up, "+")
-
-    def add_item(self, iid, first_coord = None, end = None):
-        if first_coord is None:
-            self.items.append(iid)
-        else:
-            self.items.append((iid, first_coord, end))
-
-    def del_item(self, iid):
-        self.items = [i for i in self.items if (
-            (i != iid) if isinstance(i, integer_types) else (i[0] != iid)
-        )]
-
-    def on_dnd_moved(self, event):
-        w = event.widget
-        a_id = self.anchor_id
-        px, py = self.prev
-
-        x, y = w.canvas.coords(a_id)[:2]
-        dx, dy = x - px, y - py
-        self.prev = x, y
-
-        coords = w.canvas.coords
-        for i in self.items:
-            if isinstance(i, integer_types):
-                xy = coords(i)
-                if len(xy) == 2:
-                    x0, y0 = xy
-                    coords(i, x0 + dx, y0 + dy)
-                else:
-                    x0, y0, x1, y1 = xy                    coords(i, x0 + dx, y0 + dy, x1 + dx, y1 + dy)
-            elif isinstance(i, tuple):
-                iid, first_coord, end = i
-                c = coords(iid)
-                if end is None:
-                    end = len(c)
-                for idx in range(first_coord, end):
-                    if idx & 1:
-                        c[idx] += dy
-                    else:
-                        c[idx] += dx
-                coords(iid, *c)
-            else:
-                raise ValueError(
-                    "Unsupported type of item: " + type(i).__name__
-                )
-
-    def on_dnd_up(self, event):
-        w = event.widget
-        w.unbind("<<DnDMoved>>", self.__moved)
-        w.unbind("<<DnDUp>>", self.__up)
-
-    def rotate(self, w, a, cx, cy):
-        coords = w.canvas.coords
-        bbox = w.canvas.bbox
-        cosa, sina = cos(a), sin(a)
-        for i in self.items:
-            if isinstance(i, integer_types):
-                # only "center point" of item is rotated, item's points are
-                # translated accordingly
-                ix, iy = bbox_center(bbox(i))
-                ry, rx = iy - cy, ix - cx
-                # (nx, ny) = |rotation matrix| x (rx, ry)
-                nx, ny = cosa * rx - sina * ry, sina * rx + cosa * ry
-                dx, dy = nx - rx, ny - ry
-
-                xy = coords(i)
-                if len(xy) == 2:
-                    x0, y0 = xy
-                    coords(i, x0 + dx, y0 + dy)
-                else:
-                    x0, y0, x1, y1 = xy
-                    coords(i, x0 + dx, y0 + dy, x1 + dx, y1 + dy)
-            elif isinstance(i, tuple):
-                # item's points are rotated individually
-                iid, first_coord, end = i
-                c = coords(iid)
-                if end is None:
-                    end = len(c)
-                lasty = end - 1
-
-                # Coordinate range may start from y. But x is required to
-                # compute rotation. So, align start index by x coordinate
-                # boundary.
-                start_idx = (first_coord >> 1) << 1
-
-                citer = enumerate(c[start_idx:], start_idx)
-                while True:
-                    (xidx, x), (yidx, y) = next(citer), next(citer)
-
-                    ry, rx = y - cy, x - cx
-                    nx, ny = cosa * rx - sina * ry, sina * rx + cosa * ry
-                    dx, dy = nx - rx, ny - ry
-
-                    # unaligned ranges are possible
-                    if first_coord >= xidx:
-                        c[xidx] = x + dx
-                    if yidx < end:
-                        c[yidx] = y + dy
-
-                    if yidx == lasty:
-                        break
-
-                coords(iid, *c)
-            else:
-                raise ValueError(
-                    "Unsupported type of item: " + type(i).__name__
-                )
-
-    def scale(self, w, s, cx, cy):
-        coords = w.canvas.coords
-        bbox = w.canvas.bbox
-        for i in self.items:
-            if isinstance(i, integer_types):
-                # only "center point" of item is scalled, item's points are
-                # translated accordingly
-                ix, iy = bbox_center(bbox(i))
-                ry, rx = iy - cy, ix - cx
-                # (nx, ny) = (rx, ry) * s
-                nx, ny = rx * s, ry * s
-                # translation is same for all the points
-                dx, dy = nx - rx, ny - ry
-
-                xy = coords(i)
-                if len(xy) == 2:
-                    x0, y0 = xy
-                    coords(i, x0 + dx, y0 + dy)
-                else:
-                    x0, y0, x1, y1 = xy
-                    coords(i, x0 + dx, y0 + dy, x1 + dx, y1 + dy)
-            elif isinstance(i, tuple):
-                # item's points are translated individually
-                iid, first_coord, end = i
-                c = coords(iid)
-                if end is None:
-                    end = len(c)
-                for idx in range(first_coord, end):
-                    if idx & 1:
-                        ry = c[idx] - cy
-                        ny = ry * s
-                        c[idx] = cy + ny
-                    else:
-                        rx = c[idx] - cx
-                        nx = rx * s
-                        c[idx] = cx + nx
-                coords(iid, *c)
-            else:
-                raise ValueError(
-                    "Unsupported type of item: " + type(i).__name__
-                )
 
 class Wgt(object):
     def __init__(self, instance):
         self.inst = instance
-
-def bbox_center(bbox):
-    return (bbox[0] + bbox[2]) / 2, (bbox[1] + bbox[3]) / 2
 
 def slot(x, y):
     return x - 10, y - 10, x + 10, y + 10
@@ -473,7 +306,6 @@ class OpWgt(Wgt):
         return self.inst.operands[self.in_slots[iid]]
 
 CONST_PADDING = 5
-
 
 class ConstWgt(Wgt):
     def __init__(self, const = None):
