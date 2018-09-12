fdb67496ea0a1a13cf912d10152c9a190920a609
goremykin <oleg.goremikin@yandex.ru>
2018-09-12 17:34:33 +0300

    BodyTree: variable usage analysis

--- source/function/bindings.py
+++ source/function/bindings.py
@@ -2,6 +2,14 @@ __all__ = [
     "BodyTree"
 ]
 
+from .tree import (
+    Declare,
+    Node,
+    OpAssign
+)
+from ..model import (
+    Variable
+)
 from six import (
     StringIO
 )
@@ -11,12 +19,42 @@ from common import (
 )
 
 
+class VarUsageAnalyzer(ObjectVisitor):
+
+    def __init__(self, root):
+        super(VarUsageAnalyzer, self).__init__(root,
+            field_name = "__node__"
+        )
+
+    def on_visit(self):
+        cur = self.cur
+
+        if isinstance(cur, Variable):
+            parent = self.path[-3][0]
+
+            if isinstance(parent, Declare):
+                return
+            elif isinstance(parent, OpAssign):
+                """ A variable in a function body is unused
+                if it's at the left of an assignment inside a declaration.
+                """
+                parent_1 = self.path[-5][0]
+
+                if isinstance(parent_1, Declare):
+                    cur_idx = self.path[-1][1]
+                    if cur_idx == 0:
+                        return
+
+            cur.used = True
+
+
 class BodyTree(Node):
 
     def __init__(self):
         super(BodyTree, self).__init__()
 
     def __str__(self):
+        VarUsageAnalyzer(self).visit()
         res = CodeWriter(backend = StringIO())
         res.new_line = True
         self.__c__(res)
--- source/model.py
+++ source/model.py
@@ -735,6 +735,7 @@ class Type(object):
         initializer = None,
         static = False,
         array_size = None,
+        used = False
     ):
         if self.incomplete:
             if not pointer:
@@ -747,12 +748,14 @@ class Type(object):
                 initializer = initializer,
                 static = static,
                 array_size = array_size,
+                used = used
             )
         else:
             return Variable(name, self,
                 initializer = initializer,
                 static = static,
                 array_size = array_size,
+                used = used
             )
 
     def get_definers(self):
@@ -1325,6 +1328,7 @@ class Variable(object):
         static = False,
         const = False,
         array_size = None,
+        used = False
     ):
         self.name = name
         self.type = _type
@@ -1332,6 +1336,7 @@ class Variable(object):
         self.static = static
         self.const = const
         self.array_size = array_size
+        self.used = used
 
     def gen_declaration_chunks(self, generator,
         indent = "",
@@ -1388,6 +1393,8 @@ class Variable(object):
         writer.write(self.name)
         if self.array_size is not None:
             writer.write("[%d]" % self.array_size)
+        if not self.used:
+            writer.write("@b__attribute__((unused))")
 
     __type_references__ = ["type", "initializer"]
 
