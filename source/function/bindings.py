e3a71d5a90fea556d4baafea4e5d6f4bcad27d5d
goremykin <oleg.goremikin@yandex.ru>
2018-09-19 18:18:35 +0300

    BodyTree: variable usage analysis

--- source/function/bindings.py
+++ source/function/bindings.py
@@ -3,14 +3,50 @@ __all__ = [
 ]
 
 from .tree import (
+    Declare,
+    OpAssign,
     Node
+)
+from ..model import (
+    Variable
 )
 from six import (
     StringIO
 )
 from common import (
+    ObjectVisitor,
     CodeWriter
 )
+
+
+class VarUsageAnalyzer(ObjectVisitor):
+
+    def __init__(self, root):
+        super(VarUsageAnalyzer, self).__init__(root,
+            field_name = "__node__"
+        )
+
+    def on_visit(self):
+        cur = self.cur
+
+        if isinstance(cur, Variable):
+            parent = self.path[-3][0]
+
+            if isinstance(parent, Declare):
+                return
+            elif isinstance(parent, OpAssign):
+                """Presence of a function local variable at the left
+                of an assignment inside a variable declaration is not a usage
+                this analyzer does looking for.
+                """
+                parent_1 = self.path[-5][0]
+
+                if isinstance(parent_1, Declare):
+                    cur_idx = self.path[-1][1]
+                    if cur_idx == 0:
+                        return
+
+            cur.used = True
 
 
 class BodyTree(Node):
@@ -19,6 +55,7 @@ class BodyTree(Node):
         super(BodyTree, self).__init__()
 
     def __str__(self):
+        VarUsageAnalyzer(self).visit()
         cw = CodeWriter(backend = StringIO())
         cw.new_line = True
         self.__c__(cw)
