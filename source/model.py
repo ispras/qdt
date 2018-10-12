57aba5b56ae3a90ccab46aa46f50122b9e4e8257
Danila Bogomolov <bda@ispras.ru>
2018-10-12 22:31:40 +0300

    TypeReference, Structure, Function, Macro: add __c__ method

--- source/model.py
+++ source/model.py
@@ -940,6 +940,9 @@ class TypeReference(Type):
     def __hash__(self):
         return hash(self.type)
 
+    def __c__(self, writer):
+        self.type.__c__(writer)
+
 
 class Structure(Type):
 
@@ -1080,6 +1083,9 @@ class Structure(Type):
             fields_code.append("    .%s@b=@s%s" % (name, val_str))
 
         return "{\n" + ",\n".join(fields_code) + "\n}";
+
+    def __c__(self, writer):
+        writer.write(self.name)
 
     __type_references__ = ["fields"]
 
@@ -1254,6 +1260,9 @@ class Function(Type):
             static = static
         )
 
+    def __c__(self, writer):
+        writer.write(self.name)
+
     __type_references__ = ["ret_type", "args", "body"]
 
 
@@ -1390,6 +1399,9 @@ class Macro(Type):
             args = _dict[HDB_MACRO_ARGS] if HDB_MACRO_ARGS in _dict else None,
             text = _dict[HDB_MACRO_TEXT] if HDB_MACRO_TEXT in _dict else None
         )
+
+    def __c__(self, writer):
+        writer.write(self.name)
 
 
 class MacroType(Type):
