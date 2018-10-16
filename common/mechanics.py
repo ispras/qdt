d588c4491790c32f23445c73c6aa702999ba8beb
Efimov Vasily <real@ispras.ru>
2018-10-16 17:07:12 +0300

    common.mechanics: AABB getting for box and circle
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- common/mechanics.py
+++ common/mechanics.py
@@ -26,6 +26,11 @@ class PhObject(object):
         self.spacing = spacing
         self.static = static
 
+    def aabb(self):
+        x, y, = self.x, self.y
+        return x, y, x + self.width, y + self.height
+
+
 class PhBox(PhObject):
     """ Axis aligned box or vertical line (width == 0) or horizontal line
     (height == 0). """
