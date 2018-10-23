be62a0600978f8b673cc6da579bcf2998227409f
Efimov Vasily <real@ispras.ru>
2018-10-23 17:46:27 +0300

    common.lazy: add version of `lazy` decorator that supports resetting
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- common/lazy.py
+++ common/lazy.py
@@ -1,7 +1,10 @@
 __all__ = [
     "lazy"
+      , "cached"
+  , "reset_cache"
 ]
 
+# See: https://habr.com/post/122082/
 
 class lazy(object):
 
@@ -14,9 +17,31 @@ class lazy(object):
     def __get__(self, obj, cls):
         getter = self.getter
         val = getter(obj)
-        """ Replace itself with the computed value to prevent consequent getter
-        calls.
-        see: https://stackoverflow.com/questions/23552536/override-property-getter-in-runtime
-        """
-        setattr(obj, getter.__name__, val)
+        # add evaluated value to `__dict__` of `obj` to prevent consequent call
+        # to `__get__` of this descriptor.
+        obj.__dict__[getter.__name__] = val
         return val
+
+
+class cached(lazy):
+    """ Variant of `lazy` attribute decorator that saves names of evaluated
+lazy attributes to list with name `__lazy__`. An instance with `cached`
+attributes must provide such an attribute (by `__init__`, for example).
+    """
+
+    def __get__(self, obj, cls):
+        getter = self.getter
+        val = getter(obj)
+        name = getter.__name__
+        obj.__dict__[name] = val
+        obj.__lazy__.append(name)
+        return val
+
+def reset_cache(obj):
+    "Resets lazily evaluated `cached` attributes of `obj`."
+
+    l = obj.__lazy__
+    d = obj.__dict__
+    for name in l:
+        del d[name]
+    del l[:]
