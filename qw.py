7ade081fe6b31b848e0531752ee8d2f435423af2
Efimov Vasily <real@ispras.ru>
2018-10-25 19:14:38 +0300

    qw: an API for representation of dynamic state of QOM
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- qw.py
+++ qw.py
@@ -1,5 +1,167 @@
 #!/usr/bin/python
 
+from collections import (
+    defaultdict
+)
+from debug import (
+    TYPE_CODE_PTR
+)
+from common import (
+    lazy
+)
+
+
+class RQOMTree(object):
+    "QEmu object model tree descriptor at runtime"
+
+    def __init__(self):
+        self.name2type = {}
+        self.addr2type = {}
+
+        # Types are found randomly (i.e. not in parent-first order).
+        self.unknown_parents = defaultdict(list)
+
+    def account(self, impl, name = None, parent = None):
+        """ Add a type.
+    :type impl: debug.Value
+    :param impl:
+        is the value of type's `TypeImpl` struct
+        """
+
+        if impl.type.code == TYPE_CODE_PTR:
+            # Pointer `impl` is definitely a value on the stack. It cannot be
+            # used as a global. Same time `TypeImpl` is on the heap. Hence, it
+            # can. I.e. a dereferenced `Value` should be used.
+            impl = impl.dereference()
+        if not impl.is_global:
+            impl = impl.to_global()
+
+        info_addr = impl.address
+
+        t = RQOMType(self, impl, name = name, parent = parent)
+
+        name = t.name
+        parent = t.parent
+
+        self.addr2type[info_addr] = t
+        self.name2type[name] = t
+
+        unk_p = self.unknown_parents
+
+        n2t = self.name2type
+        if parent in n2t:
+            n2t[parent].children.append(t)
+        else:
+            unk_p[parent].append(t)
+
+        if name in unk_p:
+            t.children.extend(unk_p.pop(name))
+
+        return t
+
+    def __getitem__(self, addr_or_name):
+        if isinstance(addr_or_name, str):
+            return self.name2type[addr_or_name]
+        else:
+            return self.addr2type[addr_or_name]
+
+
+class RQOMType(object):
+    "QEmu object model type descriptor at runtime"
+
+    def __init__(self, tree, impl, name = None, parent = None):
+        """
+    :type impl: debug.Value
+    :param impl:
+        is a global variable of type `TypeImpl`
+
+    :type name: str
+    :param name:
+        is given if it is already known else it will be got from `impl`
+
+    :type parent: str
+    :param parent:
+        is given if it is already known else it will be got from `impl`
+
+        """
+        self.tree = tree
+        self.impl = impl
+        if name is None:
+            name = impl["name"].fetch_c_string()
+        if parent is None:
+            parent = impl["parent"].fetch_c_string()
+            # Parent may be None
+        self.name, self.parent = name, parent
+
+        self.children = []
+
+        # Instance pointer can be casted to different C types. Remember those
+        # types.
+        self.instance_casts = set()
+
+        # "device"
+        self.realize = None
+
+    def instance_casts(self):
+        """ A QOM instance can also be casted to C types those corresponds to
+ancestors.
+    :returns: list of possible casts (debug.Type)
+        """
+        ret = set(self.instance_casts)
+        for a in self.iter_ancestors():
+            for cast in a.instance_casts:
+                ret.add(cast)
+        return ret
+
+    # TODO: there is too many boilerplate code for `TypeImpl` fields access.
+    # Consider to rewrite it in a common way. `__getitem__` ?
+
+    @lazy
+    def instance_init(self):
+        impl = self.impl
+
+        addr = impl["instance_init"].fetch_pointer()
+        if addr:
+            return impl.dic.subprogram(addr)
+        return None
+
+    @lazy
+    def class_init(self):
+        impl = self.impl
+
+        addr = impl["class_init"].fetch_pointer()
+        if addr:
+            return impl.dic.subprogram(addr)
+        return None
+
+    def __dfs_children__(self):
+        return self.children
+
+    def iter_ancestors(self):
+        n2t = self.tree.name2type
+        cur = self.parent
+
+        while cur is not None:
+            t = n2t[cur]
+            yield t
+            cur = t.parent
+
+    def implements(self, name):
+        if name == self.name:
+            return True
+
+        try:
+            t = self.tree.name2type[name]
+        except KeyError:
+            # the type given is unknown, `self` cannot implement it
+            return False
+
+        for a in self.iter_ancestors():
+            if a is t:
+                return True
+        return False
+
+
 def main():
     return 0
 
