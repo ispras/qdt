8542b6c105b565b05a389d981439c921b20e3a96
Efimov Vasily <real@ispras.ru>
2018-11-02 11:27:27 +0300

    autobuild.sh: more precise modification time observation
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- docs/autobuild.sh
+++ docs/autobuild.sh
@@ -2,12 +2,16 @@
 
 File=$1
 
+function timestamp() {
+ ls -al --full-time $1
+}
+
 echo Monitoring $File
 
-Prev=$(ls -al $File)
+Prev=$(timestamp $File)
 while true; do
  sleep 1
- Cur=$(ls -al $File)
+ Cur=$(timestamp $File)
  if ! [ "$Prev" = "$Cur" ]; then
   echo Changed
   Prev=$Cur
