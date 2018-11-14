0e2400d071586382c7c433cd951a4357b2dde96f
Efimov Vasily <real@ispras.ru>
2018-11-14 16:21:43 +0300

    RSPReader: preprocess logged input using `repr`
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- gdb/co_rsp.py
+++ gdb/co_rsp.py
@@ -115,7 +115,7 @@ class RSPReader(CoTask):
                     c = yield
                 checksum = (yield) + (yield)
 
-                print("-> %" + data + "#" + checksum)
+                print("-> " + repr("%" + data + "#" + checksum))
 
                 if rsp._ack: # `_ack` is dynamic, do not cache!
                     if rsp_check_pkt(data, checksum):
@@ -133,7 +133,7 @@ class RSPReader(CoTask):
                     c = yield
                 checksum = (yield) + (yield)
 
-                print("-> $" + data + "#" + checksum)
+                print("-> " + repr("$" + data + "#" + checksum))
 
                 if rsp._ack:
                     if rsp_check_pkt(data, checksum):
