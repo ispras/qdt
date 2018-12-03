d52368f44ba006e0e993e0cae325a6c6233eb3c0
Danila Bogomolov <bda@ispras.ru>
2018-12-03 22:35:25 +0300

    OpSDeref: support type references

--- source/function/tree.py
+++ source/function/tree.py
@@ -67,6 +67,7 @@ from ..c_const import (
 )
 from ..model import (
     Type,
+    TypeReference,
     Pointer,
     Macro,
     NodeVisitor,
@@ -660,6 +661,8 @@ class OpSDeref(Operator):
             struct = _type.type
         else: # _type expected to be a Structure
             struct = _type
+        if isinstance(struct, TypeReference):
+            struct = struct.type
 
         # for type collection
         self.struct = struct
