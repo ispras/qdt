c39d958dd920aff06d19dd86605938a90da54cfe
Efimov Vasily <real@ispras.ru>
2018-12-05 17:12:44 +0300

    qemu/machine_description.py: a small coding style fix
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- qemu/machine_description.py
+++ qemu/machine_description.py
@@ -44,15 +44,17 @@ class MachineNode(QOMDescription):
         self.max_id = 0
         self.id2node = {}
 
-        for n in self.devices + self.buses + self.irqs + self.mems + \
-        self.irq_hubs:
+        for n in (self.devices + self.buses + self.irqs + self.mems +
+            self.irq_hubs
+        ):
             self.assign_id(n)
 
     def __dfs_children__(self):
         self.link()
 
-        return QOMDescription.__dfs_children__(self) \
-            + self.devices + self.buses + self.irqs + self.mems + self.irq_hubs
+        return (QOMDescription.__dfs_children__(self) +
+            self.devices + self.buses + self.irqs + self.mems + self.irq_hubs
+        )
 
     def __gen_code__(self, gen):
         gen.reset_gen(self)
