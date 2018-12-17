3f720770bff49fc7239d3e75d7c0a5bab88f3daf
Efimov Vasily <real@ispras.ru>
2018-12-17 22:06:53 +0300

    Runtime.get_val: implement using `RSP.dump`
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- debug/expression.py
+++ debug/expression.py
@@ -213,10 +213,7 @@ class Deref(Expression):
             )
 
         # Note that `get_val` should handle target endianness.
-        str_val = runtime.get_val(addr, size)
-
-        data = int(str_val, 16)
-        return data
+        return runtime.get_val(addr, size)
 
 
 class ObjDeref(Expression):
