b143d83cbf9306f840212acc420d1b830d2bdd10
Danila Bogomolov <bda@ispras.ru>
2019-01-10 19:44:57 +0300

    version.py: simplify `bfd_get*` functions registration

--- qemu/version.py
+++ qemu/version.py
@@ -547,46 +547,16 @@ def define_only_qemu_2_6_0_types():
         Type("bfd_vma", False),
         Type("bfd_byte", False),
         Type("const bfd_byte", False),
-        Function("bfd_getl64",
-            ret_type = Type["bfd_vma"],
-            args = [
-                Pointer(
-                    Type["const bfd_byte"]
-                ).gen_var("addr", pointer = True)
+        *[
+            Function(name,
+                ret_type = Type["bfd_vma"],
+                args = [
+                    Pointer(Pointer(Type["const bfd_byte"])).gen_var("addr")
+                ]
+            ) for name in ["bfd_getl64", "bfd_getl32", "bfd_getb32",
+                "bfd_getl16", "bfd_getb16"
             ]
-        ),
-        Function("bfd_getl32",
-            ret_type = Type["bfd_vma"],
-            args = [
-                Pointer(
-                    Type["const bfd_byte"]
-                ).gen_var("addr", pointer = True)
-            ]
-        ),
-        Function("bfd_getb32",
-            ret_type = Type["bfd_vma"],
-            args = [
-                Pointer(
-                    Type["const bfd_byte"]
-                ).gen_var("addr", pointer = True)
-            ]
-        ),
-        Function("bfd_getl16",
-            ret_type = Type["bfd_vma"],
-            args = [
-                Pointer(
-                    Type["const bfd_byte"]
-                ).gen_var("addr", pointer = True)
-            ]
-        ),
-        Function("bfd_getb16",
-            ret_type = Type["bfd_vma"],
-            args = [
-                Pointer(
-                    Type["const bfd_byte"]
-                ).gen_var("addr", pointer = True)
-            ]
-        )
+        ]
     ])
 
 
