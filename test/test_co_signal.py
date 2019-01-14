250f72683ab2bfae87fd5fd58df20d984f660b53
Efimov Vasily <real@ispras.ru>
2019-01-14 18:20:47 +0300

    test: test different `CoSignal.emit` invocations
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- test/test_co_signal.py
+++ test/test_co_signal.py
@@ -37,6 +37,30 @@ class CoSignalTest(TestCase):
             "Delivered '%s' instead of 'test'" % self._delivered
         )
 
+    def test_emits(self):
+        self._delivered = 0
+
+        def watcher(a = 1):
+            self._delivered |= a
+
+        self._sig.watch(watcher)
+
+        self._sig.emit()
+        self._sig.emit(2)
+        self._sig.emit(a = 4)
+
+        # all signals is expected to be delivered in four iterations
+        limit = 5
+
+        while self._delivered < 7 and limit:
+            self._co_disp.iteration()
+            limit -= 1
+
+        self.assertGreater(limit, 0, "Signal has not been delivered")
+        self.assertEqual(self._delivered, 7,
+            "Delivered '%s' instead of '7'" % self._delivered
+        )
+
 
 if __name__ == "__main__":
     main()
