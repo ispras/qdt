ecf2ce7547001cb4c69b20bf62d0cf5be89b538f
bda <bda@ispras.ru>
2019-01-21 02:14:56 +0300

    Function: add `gen_definition` helper

--- source/model.py
+++ source/model.py
@@ -1194,6 +1194,7 @@ class Function(Type):
         self.inline = inline
         self.ret_type = Type.lookup("void") if ret_type is None else ret_type
         self.args = args
+        self.declaration = None
 
         if isinstance(body, str):
             self.body = FunctionBodyString(
@@ -1264,13 +1265,32 @@ class Function(Type):
         CopyFixerVisitor(new_f).visit()
         return new_f
 
+    def gen_definition(self,
+        body = None,
+        used_types = None,
+        used_globals = None
+    ):
+        new_f = Function(
+            name = self.name + ".definition",
+            body = body,
+            ret_type = self.ret_type,
+            args = self.args,
+            static = self.static,
+            inline = self.inline,
+            used_types = used_types,
+            used_globals = used_globals
+        )
+        CopyFixerVisitor(new_f).visit()
+        new_f.declaration = self
+        return new_f
+
     def gen_var(self, name, initializer = None, static = False):
         return Variable(name, Pointer(self),
             initializer = initializer,
             static = static
         )
 
-    __type_references__ = ["ret_type", "args", "body"]
+    __type_references__ = ["ret_type", "args", "body", "declaration"]
 
 
 class Pointer(Type):
@@ -1697,6 +1717,11 @@ class TypeFixerVisitor(ObjectVisitor):
 
             if t.definer is self.source:
                 return
+
+            if isinstance(t, Function) and t.declaration is not None:
+                t = t.declaration
+                if type(t) is TypeReference:
+                    t = t.type
 
             # replace foreign type with reference to it
             try:
