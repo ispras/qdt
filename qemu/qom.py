3404b54d22f87bc5e2fd7a1e6ec26351078cfdea
Danila Bogomolov <bda@ispras.ru>
2019-01-28 19:58:49 +0300

    gen_reg_cases: prevent try to deref nonexistent field

--- qemu/qom.py
+++ qemu/qom.py
@@ -386,7 +386,7 @@ def gen_reg_cases(regs, access, offset_name, val, ret, s):
 
         if access in reg.access:
             qtn = QemuTypeName(name)
-            s_deref_war = OpSDeref(
+            s_deref_war = lambda : OpSDeref(
                 s,
                 qtn.for_id_name + "_war"
             )
@@ -410,7 +410,7 @@ def gen_reg_cases(regs, access, offset_name, val, ret, s):
                         # no read only bits: set WAR mask to 0xF...F
                         case.add_child(
                             OpAssign(
-                                s_deref_war,
+                                s_deref_war(),
                                 OpNot(0)
                             )
                         )
@@ -419,7 +419,7 @@ def gen_reg_cases(regs, access, offset_name, val, ret, s):
                         # write mask
                         case.add_child(
                             OpAssign(
-                                s_deref_war,
+                                s_deref_war(),
                                 wm
                             )
                         )
@@ -436,13 +436,13 @@ def gen_reg_cases(regs, access, offset_name, val, ret, s):
                             OpOr(
                                 OpAnd(
                                     val,
-                                    s_deref_war,
+                                    s_deref_war(),
                                     parenthesis = True
                                 ),
                                 OpAnd(
                                     s_deref,
                                     OpNot(
-                                        s_deref_war
+                                        s_deref_war()
                                     ),
                                     parenthesis = True
                                 )
