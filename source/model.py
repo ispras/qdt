6fad4becfed09f32a59a69d363794703ec9b2115
Danila Bogomolov <bda@ispras.ru>
2019-01-30 22:10:53 +0300

    Type: support nameless type

--- source/model.py
+++ source/model.py
@@ -759,19 +759,23 @@ class Type(object):
         except TypeNotRegistered:
             return False
 
-    def __init__(self, name,
+    def __init__(self,
+        name = None,
         incomplete = True,
         base = False
     ):
-        self.name = name
+        self.is_named = name is not None
+
+        self.name = name or ""
         self.incomplete = incomplete
         self.definer = None
         self.base = base
 
-        if name in Type.reg:
-            raise RuntimeError("Type %s is already registered" % name)
+        if self.is_named:
+            if name in Type.reg:
+                raise RuntimeError("Type %s is already registered" % name)
 
-        Type.reg[name] = self
+            Type.reg[name] = self
 
     def gen_var(self, name,
         pointer = False,
