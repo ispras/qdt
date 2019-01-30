88923e5d4dc6b933e4036dde57e729e7c44994d2
Danila Bogomolov <bda@ispras.ru>
2019-01-30 22:20:29 +0300

    model.py: add newlines for code readability

--- source/model.py
+++ source/model.py
@@ -894,6 +894,7 @@ class Structure(Type):
 
     def __init__(self, name, fields = None):
         super(Structure, self).__init__(name = name, incomplete = False)
+
         self.fields = OrderedDict()
         self.append_fields(fields)
         self.nested = False
@@ -1036,6 +1037,7 @@ class Enumeration(Type):
 
     def __init__(self, type_name, elems_dict, enum_name = ""):
         super(Enumeration, self).__init__(name = type_name)
+
         self.elems = []
         self.enum_name = enum_name
         t = [ Type["int"] ]
@@ -1109,12 +1111,14 @@ class Function(Type):
         used_globals = None
     ):
         # args is list of Variables
+
         super(Function, self).__init__(
             name = name,
             # function cannot be a 'type' of variable. Only function
             # pointer type is permitted.
             incomplete = True
         )
+
         self.static = static
         self.inline = inline
         self.ret_type = Type.lookup("void") if ret_type is None else ret_type
