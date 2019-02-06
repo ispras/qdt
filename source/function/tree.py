13f41530bdf75b8f1d53f672ecfd5acd59add40a
Danila Bogomolov <bda@ispras.ru>
2019-02-06 03:24:17 +0300

    func.tree: use `join` method

--- source/function/tree.py
+++ source/function/tree.py
@@ -463,12 +463,7 @@ class StrConcat(CNode):
         self.delim = kw_args.get("delim", "")
 
     def __c__(self, writer):
-        first_child = self.children[0]
-        first_child.__c__(writer)
-
-        for c in self.children[1:]:
-            writer.write(self.delim)
-            c.__c__(writer)
+        writer.join(self.delim, self.children, self.out_child)
 
 
 class SemicolonPresence(CNode):
@@ -506,14 +501,7 @@ class Call(SemicolonPresence):
         self.func.__c__(writer)
 
         writer.write("(@a")
-        if self.args:
-            first_child = self.args[0]
-            first_child.__c__(writer)
-
-            for c in self.args[1:]:
-                writer.write(",@s")
-                c.__c__(writer)
-
+        writer.join(",@s", self.args, self.out_child)
         writer.write("@c)")
 
 
@@ -590,14 +578,7 @@ class MCall(SemicolonPresence):
 
         if self.children:
             writer.write("(@a")
-
-            first_child = self.children[0]
-            first_child.__c__(writer)
-
-            for c in self.children[1:]:
-                writer.write(",@s")
-                c.__c__(writer)
-
+            writer.join(",@s", self.children, self.out_child)
             writer.write("@c)")
 
 
@@ -646,15 +627,7 @@ class Operator(SemicolonPresence):
             writer.write("(")
 
         writer.write(self.prefix)
-
-        if self.children:
-            first_child = self.children[0]
-            first_child.__c__(writer)
-
-            for c in self.children[1:]:
-                writer.write(self.delim)
-                c.__c__(writer)
-
+        writer.join(self.delim, self.children, self.out_child)
         writer.write(self.suffix)
         if self.parenthesis:
             writer.write(")")
