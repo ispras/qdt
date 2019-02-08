3e42ede541d0f8b8387c2b03d15fcb92043be990
Danila Bogomolov <bda@ispras.ru>
2019-02-08 17:27:21 +0300

    func.tree: extract superclass `Node`

--- source/function/tree.py
+++ source/function/tree.py
@@ -1,59 +1,60 @@
 __all__ = [
-    "CNode"
-      , "Comment"
-      , "Label"
-      , "NewLine"
-      , "MacroBranch"
-      , "LoopWhile"
-      , "LoopDoWhile"
-      , "LoopFor"
-      , "BranchIf"
-      , "BranchSwitch"
-      , "BranchElse"
-      , "SwitchCase"
-      , "StrConcat"
-      # SemicolonPresence
-          , "Break"
-          , "Call"
-          , "Goto"
-          , "Declare"
-          , "MCall"
-          , "Return"
-          # Operator
-              , "OpIndex"
-              , "OpSDeref"
-              # UnaryOperator
-                  , "OpAddr"
-                  , "OpPredDec"
-                  , "OpPostDec"
-                  , "OpPredInc"
-                  , "OpPostInc"
-                  , "OpDeref"
-                  , "OpNot"
-                  , "OpCast"
-              # BinaryOperator
-                  , "OpAssign"
-                  , "OpCombAssign"
-                  , "OpAdd"
-                  , "OpSub"
-                  , "OpMul"
-                  , "OpDiv"
-                  , "OpRem"
-                  , "OpAnd"
-                  , "OpOr"
-                  , "OpXor"
-                  , "OpLShift"
-                  , "OpRShift"
-                  , "OpLogAnd"
-                  , "OpLogOr"
-                  , "OpLogNot"
-                  , "OpEq"
-                  , "OpNEq"
-                  , "OpGE"
-                  , "OpLE"
-                  , "OpGreater"
-                  , "OpLower"
-                  , "ExtCaseRange"
+    "Node"
+      , "CNode"
+          , "Comment"
+          , "Label"
+          , "NewLine"
+          , "MacroBranch"
+          , "LoopWhile"
+          , "LoopDoWhile"
+          , "LoopFor"
+          , "BranchIf"
+          , "BranchSwitch"
+          , "BranchElse"
+          , "SwitchCase"
+          , "StrConcat"
+          # SemicolonPresence
+              , "Break"
+              , "Call"
+              , "Goto"
+              , "Declare"
+              , "MCall"
+              , "Return"
+              # Operator
+                  , "OpIndex"
+                  , "OpSDeref"
+                  # UnaryOperator
+                      , "OpAddr"
+                      , "OpPredDec"
+                      , "OpPostDec"
+                      , "OpPredInc"
+                      , "OpPostInc"
+                      , "OpDeref"
+                      , "OpNot"
+                      , "OpCast"
+                  # BinaryOperator
+                      , "OpAssign"
+                      , "OpCombAssign"
+                      , "OpAdd"
+                      , "OpSub"
+                      , "OpMul"
+                      , "OpDiv"
+                      , "OpRem"
+                      , "OpAnd"
+                      , "OpOr"
+                      , "OpXor"
+                      , "OpLShift"
+                      , "OpRShift"
+                      , "OpLogAnd"
+                      , "OpLogOr"
+                      , "OpLogNot"
+                      , "OpEq"
+                      , "OpNEq"
+                      , "OpGE"
+                      , "OpLE"
+                      , "OpGreater"
+                      , "OpLower"
+                      , "ExtCaseRange"
 ]
 
 from ..c_const import (
@@ -73,7 +74,7 @@ from six import (
 )
 
 
-class CNode(object):
+class Node(object):
 
     # traverse order indicator for `ObjectVisitor`
     __node__ = ("children",)
@@ -94,11 +95,6 @@ class CNode(object):
         return self
 
     def add_child(self, child):
-        if isinstance(child, str):
-            child = CConst.parse(child)
-        elif isinstance(child, integer_types):
-            child = CINT(child)
-
         self.children.append(child)
 
     def out_children(self, writer):
@@ -112,6 +108,17 @@ class CNode(object):
 
         if self.indent_children:
             writer.pop_indent()
+
+
+class CNode(Node):
+
+    def add_child(self, child):
+        if isinstance(child, str):
+            child = CConst.parse(child)
+        elif isinstance(child, integer_types):
+            child = CINT(child)
+
+        self.children.append(child)
 
 
 class Comment(CNode):
