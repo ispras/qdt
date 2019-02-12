9867a1ac778d0c86bdc3bf81a6435db5b6da7172
Efimov Vasily <real@ispras.ru>
2019-02-12 19:08:31 +0300

    qw: `Digraph` produces DOT as `str`ing
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- qw.py
+++ qw.py
@@ -83,6 +83,7 @@ with pypath("pyrsp"):
         AMD64
     )
     from pyrsp.utils import (
+        s,
         find_free_port,
         wait_for_tcp_port
     )
@@ -341,13 +342,13 @@ the QOM tree by fetching relevant data.
             if t.instance_casts:
                 label += "\\n*"
                 for cast in t.instance_casts:
-                    label += "\\n" + cast.name
+                    label += "\\n" + s(cast.name)
 
             graph.node(n, label = label)
             if t.parent:
                 graph.edge(gv_node(t.parent), n)
 
-        with open(dot_file_name, "wb") as f:
+        with open(dot_file_name, "w") as f:
             f.write(graph.source)
 
 
