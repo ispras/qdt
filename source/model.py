a80a96496718ef61d2134bbd1727fc603a5dbff5
Danila Bogomolov <bda@ispras.ru>
2019-02-14 23:08:10 +0300

    model: add `TypeReferencesVisitor` and `NodeVisitor` visitors

--- source/model.py
+++ source/model.py
@@ -32,6 +32,8 @@ __all__ = [
       , "UsageTerminator"
   , "SourceFile"
   , "SourceTreeContainer"
+  , "TypeReferencesVisitor"
+  , "NodeVisitor"
 ]
 
 from os import (
@@ -1394,6 +1396,22 @@ class CPPMacro(Macro):
 
 
 # Data models
+
+
+class TypeReferencesVisitor(ObjectVisitor):
+
+    def __init__(self, root):
+        super(TypeReferencesVisitor, self).__init__(root,
+            field_name = "__type_references__"
+        )
+
+
+class NodeVisitor(ObjectVisitor):
+
+    def __init__(self, root):
+        super(NodeVisitor, self).__init__(root,
+            field_name = "__node__"
+        )
 
 
 class TypesCollector(ObjectVisitor):
