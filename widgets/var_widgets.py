e14384d84b3bc1441773159de20a63dabeab8836
Efimov Vasily <real@ispras.ru>
2019-02-19 21:42:48 +0300

    VarCheckbutton: first argument to `StringVar` is `master`, a widget
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- widgets/var_widgets.py
+++ widgets/var_widgets.py
@@ -38,7 +38,7 @@ class VarCheckbutton(Checkbutton):
             self.text_var = kw.pop("text")
             kw["text"] = self.text_var.get()
         else:
-            self.text_var = StringVar("")
+            self.text_var = StringVar(self, "")
         Checkbutton.__init__(self, *args, **kw)
         self.text_var.trace_variable("w", self.on_var_changed)
 
