da3e27a6aa69f7fd580df067597821943149d4b3
Efimov Vasily <real@ispras.ru>
2019-03-07 19:05:03 +0300

    add_scrollbar: auto hide & show the scroll bar
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- widgets/scrollframe.py
+++ widgets/scrollframe.py
@@ -34,11 +34,27 @@ vertical scroll bar for created widget.
         orient = "vertical",
         command = canvas.yview
     )
-    scrollbar.grid(row = 0, column = 1, sticky = "NESW")
+    scrollbar._visible = False
 
     canvas.configure(yscrollcommand = scrollbar.set)
 
+    def scrollbar_visibility():
+        "Automatically shows & hides the scroll bar."
+
+        cnv_h = canvas.winfo_height()
+        inner_h = inner.winfo_height()
+
+        if inner_h <= cnv_h:
+            if scrollbar._visible:
+                scrollbar.grid_forget()
+                scrollbar._visible = False
+        elif inner_h > cnv_h:
+            if not scrollbar._visible:
+                scrollbar.grid(row = 0, column = 1, sticky = "NESW")
+                scrollbar._visible = True
+
     def inner_configure(_):
+        scrollbar_visibility()
         canvas.configure(
             scrollregion = canvas.bbox(ALL),
             # Require as many space as inner widget do.
@@ -49,6 +65,7 @@ vertical scroll bar for created widget.
     inner.bind("<Configure>", inner_configure, "+")
 
     def canvas_configure(_):
+        scrollbar_visibility()
         canvas.itemconfig(inner_id, width = canvas.winfo_width())
 
     canvas.bind("<Configure>", canvas_configure, "+")
