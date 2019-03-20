99a98e2a16dee61bd426f20b7c8c766ebb9c375d
Koltunov Dmitry <koltunov@ispras.ru>
2019-03-20 18:06:08 +0300

    add c2t configuration mechanism and configuration sample
    Signed-off-by: Koltunov Dmitry <koltunov@ispras.ru>
    
    TestBuilder: no semantic names for build runs
    
    Signed-off-by: Efimov Vasily <real@ispras.ru>
    
    c2t: update `config_sample`
    
    Signed-off-by: Koltunov Dmitry <koltunov@ispras.ru>

---
+++ c2t/config.py
@@ -0,0 +1,80 @@
+__all__ = [
+    "C2TConfig"
+  , "Run"
+  , "get_new_rsp"
+  , "DebugClient"
+  , "DebugServer"
+  , "TestBuilder"
+]
+
+from collections import (
+    namedtuple
+)
+from common import (
+    pypath
+)
+with pypath("..pyrsp"):
+    from pyrsp.rsp import (
+        RSP,
+        archmap
+    )
+
+# CPU Testing Tool configuration components
+C2TConfig = namedtuple(
+    "C2TConfig",
+    "rsp_target qemu gdbserver target_compiler oracle_compiler"
+)
+Run = namedtuple(
+    "Run",
+    "executable args"
+)
+
+
+def get_new_rsp(regs, pc, regsize, little_endian = True):
+    class CustomRSP(RSP):
+
+        def __init__(self, *a, **kw):
+            self.arch = dict(
+                regs = regs,
+                endian = little_endian,
+                bitsize = regsize
+            )
+            self.pc_reg = pc
+            super(CustomRSP, self).__init__(*a, **kw)
+    return CustomRSP
+
+
+class DebugClient(object):
+
+    def __init__(self, march, new_rsp = None, sp = None, qemu_reset = False):
+        self.march = march
+        if march in archmap:
+            self.rsp = archmap[march]
+        elif new_rsp is not None:
+            self.rsp = new_rsp
+        else:
+            self.rsp = None
+        self.sp = sp
+        self.qemu_reset = qemu_reset
+
+
+class DebugServer(object):
+
+    def __init__(self, run):
+        self.run = run
+
+    @property
+    def run_script(self):
+        return ' '.join(self.run)
+
+
+class TestBuilder(tuple):
+
+    def __new__(cls, *runs):
+        return tuple.__new__(cls, runs)
+
+    # TODO: how to operate without runs?
+    @property
+    def run_script(self):
+        for run in self:
+            yield ' '.join(run)
