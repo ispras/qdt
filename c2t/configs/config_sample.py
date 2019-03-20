0dc8d1a353e006b5808e904720a4195a5fd5d572
Koltunov Dmitry <koltunov@ispras.ru>
2019-03-20 18:06:08 +0300

    add c2t configuration mechanism and configuration sample
    Signed-off-by: Koltunov Dmitry <koltunov@ispras.ru>
    
    TestBuilder: no semantic names for build runs
    
    Signed-off-by: Efimov Vasily <real@ispras.ru>
    
    c2t: update `config_sample`
    
    Signed-off-by: Koltunov Dmitry <koltunov@ispras.ru>

---
+++ c2t/configs/config_sample.py
@@ -0,0 +1,71 @@
+# Cpu Testing Tool configuration
+c2t_cfg = C2TConfig(
+    # describe RSP target for QEMU
+    rsp_target = DebugClient(
+        # existing RSP target name or new RSP target name
+        march = "",
+        # if required RSP target doesn't exist, it can be described here
+        # (it is optional)
+        new_rsp = get_new_rsp(
+            # list of registers names
+            regs = [""],
+            # program register name
+            pc = "",
+            # register size in bits, e. g. 16, 32, 64
+            regsize =
+        ),
+        # set the stack pointer in QEMU, if necessary
+        # (value = ELF .text section address + 0x10000)
+        # (it is optional)
+        sp = "",
+        # reset QEMU before testing, if necessary (default False)
+        # (it is optional)
+        qemu_reset =
+    ),
+    # set QEMU command line run description
+    qemu = DebugServer(
+        Run(
+            executable = "",
+            args = ""
+        )
+    ),
+    # set gdbserver command line run description
+    gdbserver = DebugServer(
+        Run(
+            executable = "",
+            args = ""
+        )
+    ),
+    # set command line runs description of test building for 'target'
+    target_compiler = TestBuilder(
+        Run(
+            executable = "",
+            args = "",
+        ),
+        Run(
+            executable = "",
+            args = ""
+        ),
+        # ...
+        Run(
+            executable = "",
+            args = ""
+        )
+    ),
+    # set command line runs description of test building for 'oracle'
+    oracle_compiler = TestBuilder(
+        Run(
+            executable = "",
+            args = "",
+        ),
+        Run(
+            executable = "",
+            args = ""
+        ),
+        # ...
+        Run(
+            executable = "",
+            args = ""
+        )
+    )
+)
