c63cf7e1281d146068023d5646f576f7a5b472bc
Koltunov Dmitry <koltunov@ispras.ru>
2019-03-20 19:24:38 +0300

    c2t: add support of configuration from file and its verification
    Signed-off-by: Koltunov Dmitry <koltunov@ispras.ru>

--- c2t.py
+++ c2t.py
@@ -5,7 +5,10 @@ from sys import (
     stderr
 )
 from os.path import (
-    basename
+    basename,
+    dirname,
+    join,
+    exists
 )
 from os import (
     killpg,
@@ -24,6 +27,14 @@ with pypath("pyrsp"):
     from pyrsp.rsp import (
         archmap
     )
+from c2t import (
+    C2TConfig,
+    Run,
+    get_new_rsp,
+    DebugClient,
+    DebugServer,
+    TestBuilder
+)
 
 C2T_ERRMSG_FORMAT = "{prog}:\x1b[31m error:\x1b[0m {msg}\n"
 
@@ -34,6 +45,44 @@ def c2t_exit(msg, prog = __file__):
         msg = msg
     ))
     killpg(0, SIGKILL)
+
+
+C2T_DIR = dirname(__file__) or '.'
+C2T_CONFIGS_DIR = join(C2T_DIR, "c2t", "configs")
+
+c2t_cfg = None
+
+
+def verify_config_components(config):
+    if c2t_cfg.rsp_target.rsp is None:
+        c2t_exit("unsupported GDB RSP target: %s" % c2t_cfg.rsp_target.march,
+            prog = config
+        )
+
+    errmsg1 = "compiler specified with frontend or backend"
+    errmsg2 = "frontend or backend are not specified"
+
+    if c2t_cfg.target_compiler.compiler is not None:
+        if (    c2t_cfg.target_compiler.frontend is not None
+            or  c2t_cfg.target_compiler.backend is not None
+        ):
+            c2t_exit(errmsg1, prog = "%s: target_compiler" % config)
+    elif (    c2t_cfg.target_compiler.frontend is None
+          or  c2t_cfg.target_compiler.backend is None
+    ):
+        c2t_exit(errmsg2, prog = "%s: target_compiler" % config)
+
+    if c2t_cfg.oracle_compiler.compiler is not None:
+        if (    c2t_cfg.oracle_compiler.frontend is not None
+            or  c2t_cfg.oracle_compiler.backend is not None
+        ):
+            c2t_exit(errmsg1, prog = "%s: oracle_compiler" % config)
+    elif (    c2t_cfg.oracle_compiler.frontend is None
+          or  c2t_cfg.oracle_compiler.backend is None
+    ):
+        c2t_exit(errmsg2, prog = "%s: oracle_compiler" % config)
+
+
 class C2TArgumentParser(ArgumentParser):
     """ ArgumentParser with custom error method """
 
@@ -54,8 +103,62 @@ def main():
             rsp = ', '.join(archmap.keys())
         ))
     )
+    parser.add_argument("-c", "--config",
+        type = str,
+        dest = "config",
+        help = ("configuration file for {prog} (see sample and examples in "
+            "{dir})".format(
+                prog = parser.prog,
+                dir = C2T_CONFIGS_DIR
+            )
+        )
+    )
 
     args = parser.parse_args()
+
+    if not args.config:
+        parser.error("requires more input arguments to run")
+
+    config = args.config
+    if not exists(config):
+        config_name = ("%s.py" % args.config if not args.config.endswith(".py")
+            else args.config
+        )
+        config = join(C2T_CONFIGS_DIR, config_name)
+        if not exists(config):
+            config = join(C2T_DIR, config_name)
+            if not exists(config):
+                parser.error(
+                    "configuration file doesn't exist: {config}".format(
+                        config = args.config
+                    )
+                )
+
+    glob = {
+        "C2TConfig": C2TConfig,
+        "Run": Run,
+        "get_new_rsp": get_new_rsp,
+        "DebugClient": DebugClient,
+        "DebugServer": DebugServer,
+        "TestBuilder": TestBuilder
+    }
+    proxy = {}
+
+    # getting `c2t_cfg` configuration for cpu testing tool
+    try:
+        execfile(config, glob, proxy)
+    except Exception as e:
+        c2t_exit(e, prog = config)
+    else:
+        global c2t_cfg
+        c2t_cfg = proxy.get("c2t_cfg")
+        if c2t_cfg is None:
+            c2t_exit(("`c2t_cfg` not found (see `config_sample.py` and "
+                    "examples in {dir})".format(dir = C2T_CONFIGS_DIR)
+                ),
+                prog = config
+            )
+    verify_config_components(config)
 
 
 if __name__ == "__main__":
