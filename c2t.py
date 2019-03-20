023ac67c7ffb3c6c1b597ec1b65a8c73f004d97c
Koltunov Dmitry <koltunov@ispras.ru>
2019-03-20 15:58:57 +0300

    c2t: add a custom argument parser
    Signed-off-by: Koltunov Dmitry <koltunov@ispras.ru>

--- c2t.py
+++ c2t.py
@@ -1,9 +1,43 @@
 #!/usr/bin/env python
 """ QEMU CPU Testing Tool """
 
+from sys import (
+    stderr
+)
+from argparse import (
+    ArgumentParser
+)
+from common import (
+    pypath
+)
+with pypath("pyrsp"):
+    from pyrsp.rsp import (
+        archmap
+    )
+
+C2T_ERRMSG_FORMAT = "{prog}:\x1b[31m error:\x1b[0m {msg}\n"
+
+
+class C2TArgumentParser(ArgumentParser):
+    """ ArgumentParser with custom error method """
+
+    def error(self, msg):
+        self.print_usage(stderr)
+        self.exit(2, C2T_ERRMSG_FORMAT.format(
+            prog = self.prog,
+            msg = msg
+        ))
+
 
 def main():
-    pass
+    parser = C2TArgumentParser(
+        description = "QEMU CPU Testing Tool",
+        epilog = ("supported GDB RSP targets: {rsp}".format(
+            rsp = ', '.join(archmap.keys())
+        ))
+    )
+
+    args = parser.parse_args()
 
 
 if __name__ == "__main__":
