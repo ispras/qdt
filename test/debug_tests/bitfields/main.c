51bd681b18a524660ec208f224b21d826ceebe7b
Efimov Vasily <real@ispras.ru>
2019-03-20 20:32:05 +0300

    test: update C program for testing of bit fields support of `debug` package
    Two different commits also test line adaptation mechanism.
    
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- test/debug_tests/bitfields/main.c
+++ test/debug_tests/bitfields/main.c
@@ -6,8 +6,24 @@ typedef struct {
     int n :8;
 } S;
 
+/* Bit fields are not at the structure beginning */
+typedef struct {
+    int pad4;
+    short pad2;
+    char pad1;
+    int a :7;
+    int b :15;
+    int c :8;
+} S2;
+
 int main(int argc, char **argv)
 {
+    S2 s2;
+
+    s2.a = 5;
+    s2.b = 6;
+    s2.c = 7;
+
     S s;
     s.j = 1;
     s.k = 2;
