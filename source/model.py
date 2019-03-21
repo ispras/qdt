d3c8d97d1f52bb47cc043ad849bd49ad3a056584
Danila Bogomolov <bda@ispras.ru>
2019-03-21 22:47:55 +0300

    CPPMacro: use `Fileless` as definer

--- source/model.py
+++ source/model.py
@@ -1510,6 +1510,7 @@ class CPPMacro(Macro):
 
     def __init__(self, *args, **kw):
         super(CPPMacro, self).__init__(*args, **kw)
+        self.definer = Fileless()
 
     def gen_chunks(self, _):
         # CPPMacro does't require referenced types
