3aa59f4d2ca75e876d4ff36b521059049d455e62
Koltunov Dmitry <koltunov@ispras.ru>
2019-03-21 13:32:03 +0300

    add simple loop tests
    Signed-off-by: Koltunov Dmitry <koltunov@ispras.ru>

---
+++ c2t/tests/c_loop_u8_1.c
@@ -0,0 +1,15 @@
+/* Control flow instruction */
+
+#include <stdint.h>
+
+void main(void)
+{
+    volatile uint8_t c, i;
+
+    for(i = 0; i < 0x7; i++) {
+        c = i;
+        c = 0; //$ch.c, ch.i, chc.c, chc.i
+    }
+
+    return;    //$bre
+}
