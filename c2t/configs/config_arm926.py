aa5a2b76eccc0308bc0fbe021898349039f7f97d
Koltunov Dmitry <koltunov@ispras.ru>
2019-03-21 19:24:54 +0300

    configs: add config for arm926
    Signed-off-by: Koltunov Dmitry <koltunov@ispras.ru>

---
+++ c2t/configs/config_arm926.py
@@ -0,0 +1,48 @@
+common_frontend = Run(
+    executable = "/usr/local/bin/clang",
+    args = "-g -O0 -emit-llvm -c {src} -o {ir}.bc"
+)
+
+c2t_cfg = C2TConfig(
+    rsp_target = DebugClient(
+        march = "arm926",
+        new_rsp = get_new_rsp(
+            regs = ["r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8",
+                "r9", "r10", "r11", "r12", "sp", "lr", "pc",
+                "xpsr", "msp", "psp", "special"],
+            pc = "pc",
+            regsize = 32
+        ),
+        sp = "sp"
+    ),
+    qemu = DebugServer(Run(
+        executable = "/usr/bin/qemu-system-arm",
+        args = ("-M versatilepb -cpu arm926 -m 128M -S "
+            "-gdb tcp:localhost:{port} -nographic"
+        )
+    )),
+    gdbserver = DebugServer(Run(
+        executable = "/usr/bin/gdbserver",
+        args = "localhost:{port} {bin}"
+    )),
+    target_compiler = TestBuilder(
+        frontend = common_frontend,
+        backend = Run(
+            executable = "/usr/local/bin/llc",
+            args = ("-O0 -march=arm -mcpu=arm926ej-s -filetype=obj {ir}.bc -o "
+                "{bin}.o"
+            )
+        )
+    ),
+    oracle_compiler = TestBuilder(
+        frontend = common_frontend,
+        backend = Run(
+            executable = "/usr/local/bin/llc",
+            args = "-O0 -march=x86-64 -filetype=obj {ir}.bc -o {bin}.o"
+        ),
+        linker = Run(
+            executable = "/usr/bin/ld",
+            args = "-e main {bin}.o -o {bin}"
+        )
+    )
+)
