47e7e799ee60ab889c0f70336052aa1ac47ae523
Danila Bogomolov <bda@ispras.ru>
2019-03-21 19:58:25 +0300

    TypeFixerVisitor: add definerless types to the Source automatically

--- source/model.py
+++ source/model.py
@@ -1796,8 +1796,11 @@ class TypeFixerVisitor(TypeReferencesVisitor):
             if isinstance(t, (Pointer, MacroType)) and not t.is_named:
                 return
 
-            # Do not add definerless types to the Source automatically
+            # Add definerless types to the Source automatically
             if t.definer is None:
+                if t.is_named:
+                    self.source.add_type(t)
+                    raise BreakVisiting()
                 return
 
             if t.definer is self.source:
