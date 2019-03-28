3da4c40a7b336b0ee46980ce00d2dc8373b92094
Koltunov Dmitry <koltunov@ispras.ru>
2019-03-28 18:41:47 +0300

    c2t: add loading of a configuration file with verification
    Signed-off-by: Koltunov Dmitry <koltunov@ispras.ru>

--- c2t.py
+++ c2t.py
@@ -5,6 +5,9 @@ from sys import (
     stderr
 )
 from os.path import (
+    dirname,
+    join,
+    exists,
     basename
 )
 from os import (
@@ -24,6 +27,14 @@ with pypath("pyrsp"):
     from pyrsp.rsp import (
         archmap
     )
+from c2t import (
+    C2TConfig,
+    Run,
+    get_new_rsp,
+    DebugClient,
+    DebugServer,
+    TestBuilder
+)
 
 C2T_ERRMSG_FORMAT = "{prog}:\x1b[31m error:\x1b[0m {msg}\n"
 
@@ -34,6 +45,39 @@ def c2t_exit(msg, prog = __file__):
         msg = msg
     ))
     killpg(0, SIGKILL)
+
+
+C2T_DIR = dirname(__file__) or '.'
+C2T_CONFIGS_DIR = join(C2T_DIR, "c2t", "configs")
+
+c2t_cfg = None
+
+
+def verify_config_components(config):
+    if c2t_cfg.rsp_target.rsp is None:
+        c2t_exit("unsupported GDB RSP target: %s" % c2t_cfg.rsp_target.march,
+            prog = config
+        )
+
+    for compiler, compiler_name in (
+        (c2t_cfg.target_compiler, "target_compiler"),
+        (c2t_cfg.oracle_compiler, "oracle_compiler")
+    ):
+        if not any((compiler.compiler, compiler.frontend, compiler.backend)):
+            c2t_exit("%s is not specified" % compiler_name, prog = config)
+        else:
+            if compiler.compiler:
+                if compiler.frontend or compiler.backend:
+                    msg = "compiler specified with %s" % (
+                        "frontend" if compiler.frontend else "backend"
+                    )
+                    c2t_exit(msg, prog = "%s: %s" % (config, compiler_name))
+            else:
+                if not compiler.frontend or not compiler.backend:
+                    msg = "%s is not specified" % (
+                        "frontend" if not compiler.frontend else "backend"
+                    )
+                    c2t_exit(msg, prog = "%s: %s" % (config, compiler_name))
 
 
 class C2TArgumentParser(ArgumentParser):
@@ -56,8 +100,59 @@ def main():
             rsp = ', '.join(archmap.keys())
         ))
     )
+    parser.add_argument(
+        type = str,
+        dest = "config",
+        help = ("configuration file for {prog} (see sample and examples in "
+            "{dir})".format(
+                prog = parser.prog,
+                dir = C2T_CONFIGS_DIR
+            )
+        )
+    )
 
     args = parser.parse_args()
+
+    config = args.config
+    if not exists(config):
+        cfg_file = "%s.py" % config if not config.endswith(".py") else config
+        config = join(C2T_CONFIGS_DIR, cfg_file)
+        if not exists(config):
+            config = join(C2T_DIR, cfg_file)
+            if not exists(config):
+                parser.error(
+                    "configuration file doesn't exist: {config}".format(
+                        config = args.config
+                    )
+                )
+
+    glob = {
+        "C2TConfig": C2TConfig,
+        "Run": Run,
+        "get_new_rsp": get_new_rsp,
+        "DebugClient": DebugClient,
+        "DebugServer": DebugServer,
+        "TestBuilder": TestBuilder
+    }
+
+    # getting `c2t_cfg` configuration for cpu testing tool
+    try:
+        execfile(config, glob)
+    except Exception as e:
+        c2t_exit(e, prog = config)
+    else:
+        global c2t_cfg
+        for val in glob.values():
+            if isinstance(val, C2TConfig):
+                c2t_cfg = val
+                break
+        if c2t_cfg is None:
+            c2t_exit(("`c2t_cfg` not found (see sample and examples in "
+                    "{dir})".format(dir = C2T_CONFIGS_DIR)
+                ),
+                prog = config
+            )
+    verify_config_components(config)
 
 
 if __name__ == "__main__":
