99798b7c394757f21cda9aa367fc68af7106771c
Efimov Vasily <real@ispras.ru>
2019-04-16 17:39:09 +0300

    c2t: add a helper class that builds tests
    Signed-off-by: Koltunov Dmitry <koltunov@ispras.ru>
    
    c2t: rework configuration approach
    
    Now a compiler must output file with name {bin}. Intermediate / auxiliary
    files must use {ir} as prefix for names.
    
    Signed-off-by: Efimov Vasily <real@ispras.ru>
    
    C2TTestBuilder: don't build test if it exists
    
    Signed-off-by: Koltunov Dmitry <koltunov@ispras.ru>

--- c2t.py
+++ c2t.py
@@ -32,6 +32,9 @@ from subprocess import (
     Popen,
     PIPE
 )
+from platform import (
+    machine
+)
 from common import (
     filefilter,
     cli_repr,
@@ -68,6 +71,8 @@ C2T_TEST_DIR = join(C2T_DIR, "c2t", "tests")
 C2T_TEST_IR_DIR = join(C2T_TEST_DIR, "ir")
 C2T_TEST_BIN_DIR = join(C2T_TEST_DIR, "bin")
 
+ORACLE_CPU = machine()
+
 c2t_cfg = None
 
 
@@ -87,6 +92,48 @@ class ProcessWithErrCatching(Process):
         _, err = process.communicate()
         if process.returncode != 0:
             c2t_exit(err, prog = self.prog)
+
+
+class C2TTestBuilder(Process):
+    """ A helper class that builds tests """
+
+    def __init__(self, compiler, tests, tests_tail, tests_queue, verbose):
+        super(C2TTestBuilder, self).__init__()
+        self.compiler = compiler
+        self.tests = tests
+        self.tests_tail = tests_tail
+        self.tests_queue = tests_queue
+        self.verbose = verbose
+
+    def test_build(self, test_src, test_ir, test_bin):
+        for run_script in self.compiler.run_script:
+            cmd = run_script.format(
+                src = test_src,
+                ir = test_ir,
+                bin = test_bin,
+                c2t_dir = C2T_DIR,
+                c2t_test_dir = C2T_TEST_DIR
+            )
+            if self.verbose:
+                print(cmd)
+            cmpl_unit = ProcessWithErrCatching(cmd)
+            cmpl_unit.start()
+            cmpl_unit.join()
+
+    def run(self):
+        for test in self.tests:
+            test_name = test[:-2]
+            test_src = join(C2T_TEST_DIR, test)
+            test_bin = join(C2T_TEST_BIN_DIR,
+                test_name + "_%s" % self.tests_tail
+            )
+
+            if not exists(test_bin):
+                test_ir = join(C2T_TEST_IR_DIR, test_name)
+
+                self.test_build(test_src, test_ir, test_bin)
+
+            self.tests_queue.put((test_src, test_bin))
 
 
 class testfilter(filefilter):
