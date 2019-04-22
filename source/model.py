f74239b8434df7de9463226ae206fe7009474d6b
Danila Bogomolov <bda@ispras.ru>
2019-04-22 22:57:23 +0300

    ChunkGenerator: add Source in chunk_cache

--- source/model.py
+++ source/model.py
@@ -108,9 +108,9 @@ macro_forbidden = compile("[^0-9A-Z_]")
 class ChunkGenerator(object):
     """ Maintains context of source code chunks generation process. """
 
-    def __init__(self, for_header = False, inherit_global_headers = False):
-        self.chunk_cache = {}
-        self.for_header = for_header
+    def __init__(self, definer, inherit_global_headers = False):
+        self.chunk_cache = { definer: [] }
+        self.for_header = isinstance(definer, Header)
         self.inherit_global_headers = inherit_global_headers
         """ Tracking of recursive calls of `provide_chunks`. Currently used
         only to generate "extern" keyword for global variables in header and to
@@ -456,7 +456,7 @@ switching to that mode.
             # Preserve current types list. See the comment above.
             l = list(self.types.values()) + ref_list
 
-        gen = ChunkGenerator(for_header = isinstance(self, Header),
+        gen = ChunkGenerator(self,
             inherit_global_headers = inherit_global_headers
         )
 
