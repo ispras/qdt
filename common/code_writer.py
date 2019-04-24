4a13b95078433fcb4b563758b0e8fef32839a290
Danila Bogomolov <bda@ispras.ru>
2019-04-24 22:12:19 +0300

    code_writer: add `LanguageState` class

--- common/code_writer.py
+++ common/code_writer.py
@@ -1,6 +1,59 @@
 __all__ = [
     "CodeWriter"
+  , "LanguageState"
 ]
+
+
+class LanguageState(object):
+
+    def __init__(self, writer, indent, prefix = None):
+        self.w = writer
+
+        self.prefix = prefix or ""
+        self.indent = indent
+
+        self._indents_stack = []
+        self._states_stack = []
+
+        self.reset()
+
+    def reset(self):
+        self._indents_stack[:] = []
+        self.current_indent = ""
+        self._states_stack[:] = []
+
+    def __enter__(self):
+        self._states_stack.append(self.w.s)
+        self.w.s = self
+        return self.w
+
+    def __exit__(self, *_): # value, traceback
+        self.w.s = self._states_stack.pop()
+
+    def push_indent(self):
+        "Increases current indent by one indentation step."
+
+        self.current_indent = self.current_indent + self.indent
+
+    def pop_indent(self):
+        "Decreases current indent by one indentation step."
+
+        self.current_indent = self.current_indent[:-len(self.indent)]
+
+    def save_indent(self, reset = True):
+        "Saves current indent."
+
+        self._indents_stack.append(self.current_indent)
+        if reset:
+            self.current_indent = ""
+
+    def load_indent(self):
+        "Loads previous indent."
+
+        if self._indents_stack:
+            self.current_indent = self._indents_stack.pop()
+        else:
+            raise RuntimeError("Cannot load previous indent: stack is empty")
 
 
 class CodeWriter(object):
