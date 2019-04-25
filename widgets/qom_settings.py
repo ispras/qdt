620b165f00ff4f7f87458fad6aff11e1cf82b96d
Efimov Vasily <real@ispras.ru>
2019-04-25 17:34:45 +0300

    QOMDescriptionSettingsWidget: support `dict`s and `list`s
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- widgets/qom_settings.py
+++ widgets/qom_settings.py
@@ -6,6 +6,9 @@ from .gui_frame import (
     GUIFrame
 )
 from six.moves.tkinter import (
+    GROOVE,
+    BOTH,
+    LabelFrame,
     Checkbutton,
     BooleanVar,
     BOTH,
@@ -232,6 +235,172 @@ def gen_widgets(master, obj, attr, _input):
         )
 
     return generator(master, obj, attr, _input)
+
+
+class DictPairWidget(GUIFrame):
+
+    def __init__(self, master, _dict, key, key_desc, value_desc):
+        GUIFrame.__init__(self, master, borderwidth = 2, relief = GROOVE)
+
+        self.rowconfigure(0, weight = 0)
+        self.rowconfigure(1, weight = 1)
+        self.columnconfigure(0, weight = 1)
+
+        self._dict = _dict
+        self._key = key
+
+        # self is a proxy for the dict
+        key_w = gen_widgets(self, self, "key", key_desc)
+        key_w.grid(row = 0, column = 0, sticky = "NESW")
+
+        value_w = gen_widgets(self, self, "value", value_desc)
+        value_w.grid(row = 1, column = 0, sticky = "NESW")
+
+        self.key_w, self.value_w = key_w, value_w
+
+    @property
+    def key(self):
+        return self._key
+
+    @property
+    def value(self):
+        return self._dict[self._key]
+
+    def _refresh(self):
+        self.key_w._refresh()
+        self.value_w._refresh()
+
+
+def gen_dict_widgets(master, obj, attr, _input):
+    """
+:param _input:
+    A dict with only one entry: key is a descriptor of key for editor and
+    value is a descriptor of value for editor.
+    """
+
+    key_input, value_input = list(_input.items())[0]
+
+    w = GUIFrame(master)
+    # w.columnconfigure(0, weight = 0)
+
+    # widgets of editing of key-value pairs of the `dict`
+    w._pair_widgets = pair_widgets = []
+
+    def _refresh():
+        val = getattr(obj, attr)
+
+        new_keys = set([] if val is None else val.keys())
+        obsolete_pair_widgets = []
+
+        # check which keys are actually new
+        for pw in pair_widgets:
+            if pw._key in new_keys:
+                new_keys.remove(pw._key)
+            else:
+                obsolete_pair_widgets.append(pw)
+
+        # provide widgets for new keys
+        for new_key in new_keys:
+            # re-use obsolete widgets
+            if obsolete_pair_widgets:
+                pw = obsolete_pair_widgets.pop()
+                pw._key = new_key
+            else:
+                # no obsolete widgets left, create new one
+                pw = DictPairWidget(w, val, new_key, key_input, value_input)
+                pw.pack(expand = True, fill = BOTH)
+                pair_widgets.append(pw)
+
+        # remove unused obsolete widgets
+        for pw in obsolete_pair_widgets:
+            pair_widgets.remove(pw)
+            pw.destroy()
+
+        # refreshing of keys and values
+        for pw in pair_widgets:
+            pw._refresh()
+
+    w._refresh = _refresh
+
+    # TODO
+    w._changes = lambda : []
+
+    return w
+
+
+class ListItemWidget(LabelFrame):
+
+    def __init__(self, master, _list, index, value_desc):
+        LabelFrame.__init__(self, master, text = "")
+
+        self._list = _list
+
+        self._index = None
+        self.index = index
+
+        # self is a proxy for list
+        self.w_value = value_w = gen_widgets(self, self, "value", value_desc)
+        value_w.pack(expand = True, fill = BOTH)
+
+    @property
+    def index(self):
+        return self._index
+
+    @index.setter
+    def index(self, value):
+        if value == self._index:
+            return
+
+        self.config(text = str(value))
+
+        self._index = value
+
+    @property
+    def value(self):
+        return self._list[self._index]
+
+    def _refresh(self):
+        self.w_value._refresh()
+
+
+def gen_list_widgets(master, obj, attr, _input):
+    """
+:param _input:
+    A list with only one item: a descriptor of value for editor.
+    """
+
+    value_input = _input[0]
+
+    w = GUIFrame(master)
+    # w.columnconfigure(0, weight = 0)
+
+    w._item_widgets = item_widgets = []
+
+    def _refresh():
+        val = getattr(obj, attr)
+
+        if val is None:
+            val = []
+
+        # create widgets for new objects ...
+        for idx in range(len(item_widgets), len(val)):
+            iw = ListItemWidget(w, val, idx, value_input)
+            item_widgets.append(iw)
+            iw.pack(expand = True, fill = BOTH)
+
+        # ... or remove unused widgets
+        for _ in range(len(val), len(item_widgets)):
+            item_widgets.pop().destroy()
+
+        for iw in item_widgets:
+            iw._refresh()
+
+    w._refresh = _refresh
+
+    # TODO
+    w._changes = lambda : []
+
+    return w
 
 
 def add_highlighting(desc, widget, attr):
