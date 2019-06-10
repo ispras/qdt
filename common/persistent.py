9cb70f3f4b80c4e981f5d87210c1c884f43a0809
Efimov Vasily <real@ispras.ru>
2019-06-10 17:27:45 +0300

    Persistent: account name base of variable during loading context from the file
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- common/persistent.py
+++ common/persistent.py
@@ -39,9 +39,15 @@ persistent object. See `common.pygen.pythonizable`.
             loaded = dict(self._globals)
             try:
                 execfile(self._file_name, loaded)
-                ctx = loaded["persistent_context"]
             except:
                 return
+            else:
+                base = self.__var_base__()
+                for name, ctx in loaded.items():
+                    if name.startswith(base):
+                        break
+                else:
+                    print("No persistent data found in " + self._file_name)
 
             ctx_version = ctx.pop("_version", self._version)
 
