1367104570ee7e6628d1e6ff14a747ec741dd306
Efimov Vasily <real@ispras.ru>
2019-06-19 13:26:39 +0300

    machine: generate code for CPU reset
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- qemu/machine.py
+++ qemu/machine.py
@@ -17,6 +17,14 @@ from .qom import (
     QOMType
 )
 from source import (
+    BodyTree,
+    Declare,
+    OpAssign,
+    MCall,
+    NewLine,
+    Call,
+    Pointer,
+    Macro,
     Source,
     Type,
     TypeNotRegistered,
@@ -39,6 +47,8 @@ from .machine_nodes import (
     PCIExpressDeviceNode
 )
 from common import (
+    cached,
+    reset_cache,
     mlget as _,
     sort_topologically
 )
@@ -157,6 +167,8 @@ class MachineType(QOMType):
         ):
         super(MachineType, self).__init__(name)
 
+        self.__lazy__ = []
+
         self.desc = "TODO: provide description for " + name
 
         self.cpus = cpus
@@ -176,6 +188,8 @@ class MachineType(QOMType):
         self.init_used_types = []
         self.hub_layouts = {}
         self.provide_node_names()
+
+        reset_cache(self)
 
     def use_type(self, t):
         if t in self.init_used_types:
@@ -659,6 +673,9 @@ qdev_get_child_bus(@aDEVICE({bridge_name}),@s"{bus_child_name}")\
             elif isinstance(node, CPUNode):
                 self.use_type_name("CPUState")
                 self.use_type_name("cpu_create")
+                self.use_type_name("qemu_register_reset")
+                cpu_reset = self.cpu_reset
+                self.use_type_name(cpu_reset.name)
 
                 cpu_name = self.node_map[node]
 
@@ -671,9 +688,11 @@ qdev_get_child_bus(@aDEVICE({bridge_name}),@s"{bus_child_name}")\
 
                 def_code += """\
     {var_name}@b=@scpu_create(@a{qom_type});
+    qemu_register_reset(@a{reset},@s{var_name});
 """.format(
     var_name = cpu_name,
-    qom_type = qom_type
+    qom_type = qom_type,
+    reset = cpu_reset.name
                 )
             else:
                 raise UnknownMachineNodeType(str(type(node)))
@@ -693,3 +712,26 @@ qdev_get_child_bus(@aDEVICE({bridge_name}),@s"{bus_child_name}")\
         get_vp("machine type register template generator")(self)
 
         return self.source.generate()
+
+    @cached
+    def cpu_reset(self):
+        cpu_reset = Type["QEMUResetHandler"].use_as_prototype(
+            self.qtn.for_id_name + "_cpu_reset",
+            body = BodyTree(),
+            static = True
+        )
+
+        var_cpu = Pointer(Type["CPUState"])("cpu")
+
+        cpu_reset.body = BodyTree()(
+            Declare(OpAssign(
+                var_cpu,
+                MCall("CPU", cpu_reset.args[0]))),
+            NewLine(),
+            Call("cpu_reset", var_cpu)
+        )
+
+        self.source.add_type(cpu_reset)
+
+        return cpu_reset
+
