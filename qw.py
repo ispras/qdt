affbe9a1a59c9c5dbc317c4ef82912422fb97594
Goremykin Oleg <goremykin@ispras.ru>
2019-06-24 19:14:08 +0300

    qw: remove unused global variable

--- qw.py
+++ qw.py
@@ -1221,9 +1221,6 @@ description for QDT project.
         self.proxy.commit()
 
 
-re_qemu_system_x = compile(".*qemu-system-.+$")
-
-
 class QArgumentParser(ArgumentParser):
 
     def error(self, *args, **kw):
