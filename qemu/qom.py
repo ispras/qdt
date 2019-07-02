d750262f31c9c19d7d8613667526e6d09158ed00
Efimov Vasily <real@ispras.ru>
2019-07-02 19:22:45 +0300

    Register: fixup reset value wrapping in hexadecimal form
    Hex form of an integer has digits amount as twice as its size (in bytes).
    
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- qemu/qom.py
+++ qemu/qom.py
@@ -243,7 +243,7 @@ class Register(OpaqueRegister):
     ):
         super(Register, self).__init__(size, name)
         self.access = access
-        self.reset = None if reset is None else CINT(reset, 16, size)
+        self.reset = None if reset is None else CINT(reset, 16, size * 2)
         self.full_name = full_name
 
         if wmask is None:
@@ -278,7 +278,7 @@ class Register(OpaqueRegister):
             ret += ", access = " + repr(access)
 
         reset = self.reset
-        if reset != CINT(0, 16, size):
+        if reset != CINT(0, 16, size * 2):
             ret += ", reset = " + repr(reset)
 
         fn = self.full_name
