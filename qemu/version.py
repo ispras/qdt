bd4d7808762bfa504ee2e43c785d026bd7d365d8
Efimov Vasily <real@ispras.ru>
2019-07-02 19:24:27 +0300

    qemu: declare `memory_region_init_rom_device` function
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- qemu/version.py
+++ qemu/version.py
@@ -270,6 +270,7 @@ def define_only_qemu_2_6_0_types():
         Function(name = "memory_region_init"),
         Function(name = "memory_region_init_alias"),
         Function(name = "memory_region_init_ram"),
+        Function(name = "memory_region_init_rom_device"),
         Function(name = "memory_region_add_subregion_overlap"),
         Function(name = "memory_region_add_subregion")
     ]).add_reference(osdep_fake_type)
