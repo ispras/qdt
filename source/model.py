a8e2f17c0bcda26e20f1b13d39a45882efca739c
Danila Bogomolov <bda@ispras.ru>
2019-07-03 18:09:14 +0300

    Structure.append_field_t: mark argument `pointer` as keyword

--- source/model.py
+++ source/model.py
@@ -1117,7 +1117,7 @@ class Structure(Type):
             self.append_field(v)
 
     def append_field_t(self, _type, name, pointer = False):
-        self.append_field(_type(name, pointer))
+        self.append_field(_type(name, pointer = pointer))
 
     def append_field_t_s(self, type_name, name, pointer = False):
         self.append_field_t(Type[type_name], name, pointer)
