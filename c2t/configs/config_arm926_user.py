528d01449f9c98a2a0dbac22a7280a511e16bbe2
Koltunov Dmitry <koltunov@ispras.ru>
2019-07-04 14:27:00 +0300

    configs: add user mode config for arm926
    Signed-off-by: Koltunov Dmitry <koltunov@ispras.ru>
    
    c2t arm926 user: do not link with stdlib and set entry to `main` explicitly
    
    Signed-off-by: Efimov Vasily <real@ispras.ru>
    
    config_arm926_user: delete '-Wl,-emain' from compiler run
    
    Signed-off-by: Koltunov Dmitry <koltunov@ispras.ru>

---
+++ c2t/configs/config_arm926_user.py
@@ -0,0 +1,33 @@
+c2t_cfg = C2TConfig(
+    rsp_target = DebugClient(
+        march = "arm926",
+        new_rsp = get_new_rsp(
+            regs = ["r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8",
+                    "r9", "r10", "r11", "r12", "sp", "lr", "pc",
+                    "xpsr", "msp", "psp", "special"],
+            pc = "pc",
+            regsize = 32
+        ),
+        user = True
+    ),
+    qemu = DebugServer(Run(
+        executable = "/usr/bin/qemu-arm",
+        args = "-cpu arm926 -singlestep -g {port} {bin}"
+    )),
+    gdbserver = DebugServer(Run(
+        executable = "/usr/bin/gdbserver",
+        args = "localhost:{port} {bin}"
+    )),
+    target_compiler = TestBuilder(
+        Run( # compiler
+            executable = "/usr/bin/arm-none-eabi-gcc",
+            args = "-O0 -g -mcpu=arm926ej-s -nostdlib {src} -o {bin}"
+        ),
+    ),
+    oracle_compiler = TestBuilder(
+        Run( # compiler
+            executable = "/usr/bin/gcc",
+            args = "-g -O0 {src} -o {bin}"
+        )
+    )
+)
