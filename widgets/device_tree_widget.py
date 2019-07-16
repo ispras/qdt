a6008ca117b1b7bd50eca091b30e0355841fafba
Goremykin Oleg <goremykin@ispras.ru>
2019-07-16 17:27:59 +0300

    device_tree_widget: add arch selector for device tree widget

--- widgets/device_tree_widget.py
+++ widgets/device_tree_widget.py
@@ -12,6 +12,7 @@ from qemu import (
 )
 from six.moves.tkinter import (
     Radiobutton,
+    Checkbutton,
     StringVar
 )
 from common import (
@@ -21,7 +22,11 @@ from .gui_dialog import (
     GUIDialog
 )
 from .scrollframe import (
+    add_scrollbars,
     add_scrollbars_native
+)
+from .gui_frame import (
+    GUIFrame
 )
 
 class DeviceTreeWidget(GUIDialog):
@@ -32,10 +37,9 @@ class DeviceTreeWidget(GUIDialog):
         self.title(_("Device Tree"))
         self.grid()
 
-        self.columnconfigure(0, weight = 1)
+        self.columnconfigure(0, weight = 1, minsize = 300)
+        self.columnconfigure(2, weight = 1, minsize = 100)
         self.rowconfigure(0, weight = 1)
-
-        self.columnconfigure(2, minsize = 200)
 
         geom = "+" + str(int(root.winfo_rootx())) \
              + "+" + str(int(root.winfo_rooty()))
@@ -55,31 +59,136 @@ class DeviceTreeWidget(GUIDialog):
         dt.grid(
             row = 0,
             column = 0,
+            rowspan = 3,
             sticky = "NEWS"
         )
 
+        column_fr = VarLabelFrame(self, borderwidth = 0)
+        column_fr.grid(row = 0, column = 2, rowspan = 2, sticky = "SEWN")
+        column_fr.columnconfigure(0, weight = 1)
+        column_fr.rowconfigure(0, weight = 1)
+        column_fr.rowconfigure(1, weight = 1, minsize = 100)
+
+        fr_at = VarLabelFrame(column_fr, text = _("Select arch filter"))
+        fr_at.grid(row = 0, column = 0, sticky = "SEWN")
+
+        self.fr_qt = VarLabelFrame(column_fr, text = _("Select QOM type"))
+        self.fr_qt.grid(row = 1, column = 0, sticky = "SEWN")
+
         self.add_button = VarButton(
-            self,
+            column_fr,
             text = _("Select"),
             command = self.on_select_qom_type
         )
-        self.add_button.grid(row = 1, column = 2, sticky = "WE")
+        self.add_button.grid(row = 2, column = 0, sticky = "EW")
         self.add_button.config(state = "disabled")
 
-        self.fr_qt = VarLabelFrame(self, text = _("Select QOM type"))
-        self.fr_qt.grid(row = 0, column = 2, sticky = "SEWN")
-
-        qtype_dt = qvd_get(
+        qtype_dt = self.qtype_dt = qvd_get(
             root.mach.project.build_path,
             version = root.mach.project.target_version
         ).qvc.device_tree
 
+        arch_buttons = VarLabelFrame(fr_at, borderwidth = 0)
+        arch_buttons.pack(fill = "x")
+
+        arch_buttons.columnconfigure(0, weight = 1, minsize = 60)
+        arch_buttons.columnconfigure(1, weight = 1, minsize = 60)
+        arch_buttons.columnconfigure(2, weight = 1, minsize = 60)
+
+        bt_all_arches = VarButton(
+            arch_buttons,
+            text = _("All"),
+            command = self.select_arches
+        )
+        bt_all_arches.grid(row = 0, column = 0, sticky = "EW")
+
+        bt_none_arches = VarButton(
+            arch_buttons,
+            text = _("None"),
+            command = self.deselect_arches
+        )
+        bt_none_arches.grid(row = 0, column = 1, sticky = "EW")
+
+        bt_invert_arches = VarButton(
+            arch_buttons,
+            text = _("Invert"),
+            command = self.invert_arches
+        )
+        bt_invert_arches.grid(row = 0, column = 2, sticky = "EW")
+
+        if not qtype_dt.arches:
+            bt_all_arches.config(state = "disabled")
+            bt_none_arches.config(state = "disabled")
+            bt_invert_arches.config(state = "disabled")
+
+        arch_selector = VarLabelFrame(fr_at, borderwidth = 0)
+        arch_selector.pack(fill = "both", anchor = "w")
+        arch_selector, scrolltag = add_scrollbars(arch_selector, GUIFrame)
+
+        av = self.arches_vars = []
+        ac = self.arches_checkbox = []
+        for i, a in enumerate(sorted(list(qtype_dt.arches))):
+            var = StringVar()
+            c = Checkbutton(arch_selector,
+                text = a,
+                variable = var,
+                onvalue = a,
+                offvalue = "",
+                command = self.on_select_arch_type
+            )
+            c.grid(row = i, column = 0, sticky = "W")
+            c.bindtags((scrolltag,) + c.bindtags())
+            c.select()
+            av.append(var)
+            ac.append(c)
+
+        # Counter of Disabled Arch
+        self.cda = 0
+
+        # key: item
+        # value: (parent, index, tags)
+        self.detach_items = {}
+        self.dt_items = {}
+
         self.qom_create_tree("", qtype_dt.children)
+
+    def select_arches(self):
+        for i, v in self.detach_items.items():
+            self.device_tree.move(i, v[0], v[1])
+
+        self.detach_items = {}
+
+        self.cda = 0
+
+        for c in self.arches_checkbox:
+            c.select()
+
+    def deselect_arches(self):
+        dt = self.device_tree
+
+        detach_items = []
+        for i in self.dt_items:
+            if i not in self.detach_items:
+                detach_items.append(i)
+                self.detach_items[i] = self.dt_items[i]
+
+        else:
+            dt.detach(*detach_items)
+
+        for c in self.arches_checkbox:
+            c.deselect()
+
+        self.cda = len(self.qtype_dt.arches)
+
+    def invert_arches(self):
+        for c in self.arches_checkbox:
+            c.invoke()
 
     def qom_create_tree(self, parent_id, qt_children):
         dt = self.device_tree
+        dt_items = self.dt_items
 
-        for key in sorted(qt_children.keys()):
+        for i, key in enumerate(sorted(qt_children.keys())):
             qt = qt_children[key]
             if qt.macros:
                 value = ", ".join(qt.macros)
@@ -89,9 +198,60 @@ class DeviceTreeWidget(GUIDialog):
             cur_id = dt.insert(parent_id, "end",
                 text = qt.name,
                 values = (value, ),
+                tags = list(qt.arches)
             )
+            dt_items[cur_id] = (parent_id, i, set(qt.arches))
             if qt.children:
                 self.qom_create_tree(cur_id, qt.children)
+
+    def on_select_arch_type(self):
+        included_arches = set()
+        for v in self.arches_vars:
+            arch_str = v.get()
+            if arch_str != "":
+                included_arches.add(arch_str)
+
+        disabled_arches = self.qtype_dt.arches - included_arches
+        detach_items = []
+
+        dt = self.device_tree
+        new_cda = len(disabled_arches)
+
+        di = self.detach_items
+        dt_items = self.dt_items
+        if new_cda > self.cda:
+            # one architecture has been disabled
+            for a in disabled_arches:
+                items = dt.tag_has(a)
+                for i in items:
+                    if not(dt_items[i][2] - disabled_arches) and i not in di:
+                        # All tags in disabled_arch
+                        # Save desc of detach node
+                        di[i] = dt_items[i]
+                        detach_items.append(i)
+
+            if detach_items:
+                dt.detach(*detach_items)
+        else:
+            # one architecture has been enabled
+            del_nodes = []
+            for i, v in di.items():
+                if v[2].intersection(included_arches):
+                    cur_index = v[1]
+                    parent = v[0]
+                    ci = 0
+                    for c in dt.get_children(parent):
+                        if dt_items[c][1] > cur_index:
+                            break
+                        ci += 1
+
+                    dt.move(i, parent, ci)
+                    del_nodes.append(i)
+
+            for n in del_nodes:
+                del di[n]
+
+        self.cda = new_cda
 
     def on_select_qom_type(self):
         self.qom_type_var.set(self.v.get())
