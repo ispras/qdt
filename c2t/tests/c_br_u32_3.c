0b35b99c48bfa4e2248976374ac52a9b6cc01558
Koltunov Dmitry <koltunov@ispras.ru>
2019-08-01 16:00:36 +0300

    tests: add `br` tests with '>' operator
    Signed-off-by: Koltunov Dmitry <koltunov@ispras.ru>

---
+++ c2t/tests/c_br_u32_3.c
@@ -0,0 +1,16 @@
+/* Control flow instruction */
+
+#include <stdint.h>
+
+void main(void)
+{
+    volatile uint32_t a = 0x192ce100, b = 0x4b4da436, c = 0;
+
+    if (a > b) {
+        c = 1;  //$br
+    } else {
+        c = -1; //$br
+    }
+
+    return;     //$bre
+}
