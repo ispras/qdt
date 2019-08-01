034bb38ae83c28ce71238470c0726a300bbdbc89
Koltunov Dmitry <koltunov@ispras.ru>
2019-08-01 16:01:27 +0300

    tests: add `br` tests with '<' operator
    Signed-off-by: Koltunov Dmitry <koltunov@ispras.ru>

---
+++ c2t/tests/c_br_u64_4.c
@@ -0,0 +1,16 @@
+/* Control flow instruction */
+
+#include <stdint.h>
+
+void main(void)
+{
+    volatile uint64_t a = 0x22a7a5c8e1209c9f, b = 0xb1d279a21a8a7c48, c = 0;
+
+    if (a < b) {
+        c = 1;  //$br
+    } else {
+        c = -1; //$br
+    }
+
+    return;     //$bre
+}
