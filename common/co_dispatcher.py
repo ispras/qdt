7e5a41dcfec4b71235a1c33f0bc2e0662f2a4860
Efimov Vasily <real@ispras.ru>
2019-11-11 21:40:01 +0300

    callco: carefully log exceptions and report them using special exception type
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- common/co_dispatcher.py
+++ common/co_dispatcher.py
@@ -2,6 +2,7 @@ __all__ = [
 # RuntimeError
     "FailedCallee"
   , "CancelledCallee"
+  , "CoStackFailure"
 # object
   , "CoTask"
   , "CoDispatcher"
@@ -19,6 +20,12 @@ from .ml import (
     mlget as _
 )
 import sys
+from traceback import (
+    print_exception
+)
+from six import (
+    StringIO
+)
 
 
 class FailedCallee(RuntimeError):
@@ -369,6 +376,26 @@ after last statement in the corresponding callable object.
     def has_work(self):
         return self.tasks or self.active_tasks
 
+
+class CoStackFailure(RuntimeError):
+
+    def __init__(self, tracebacks):
+        super(CoStackFailure, self).__init__("Coroutine failure")
+        self.tracebacks = tracebacks
+
+    def __str__(self):
+        str_tracebacks = []
+
+        for etype, value, tb in reversed(self.tracebacks):
+            tmp = StringIO()
+            print_exception(etype, value, tb, file = tmp)
+            str_tracebacks.append(tmp.getvalue())
+
+        return ("Coroutine stack failure happened:\n" +
+            "\nAfter injection of exception:\n\n".join(str_tracebacks)
+        )
+
+
 # Call coroutine maintaining coroutine calling stack.
 def callco(co):
     stack = []
@@ -376,23 +403,48 @@ def callco(co):
     while True:
         try:
             if exc:
-                local_exc = exc
+                ret = co.throw(type(exc), exc)
+                # `co` successfully handled callee's exception and continued
                 exc = None
-                ret = co.throw(type(local_exc), local_exc)
             else:
                 ret = next(co)
         except StopIteration:
+            # If `exc` is not `None`, then `co` successfully handled callee's
+            # exception and finished.
+            exc = None
             try:
                 co = stack.pop()
             except IndexError:
                 break
         except BaseException as e:
+            if exc is None:
+                # Actually two cases:
+                # 1. `cc` failed during handling of `exc` of its callee.
+                # 2. `cc` failed after the handling.
+                # In the first case, `exc` is likely the cause of current
+                # exception. Hence it must be reported. This information also
+                # may be useful in second case.
+
+                # Because current exception can be caught by some caller in
+                # the `stack` we must not report something right now. Instead,
+                # we start a shadow log of tracebacks to report it all if no
+                # one will catch the failure.
+                shadow_tb = []
+
+            shadow_tb.append(sys.exc_info())
+
+            # Let caller to handle `co` (callee's) exception
             exc = e
+
             try:
                 co = stack.pop()
             except IndexError:
-                raise e
+                break
         else:
             if isinstance(ret, GeneratorType):
                 stack.append(co)
                 co = ret
+
+    if exc is not None:
+        # The `exc`eption has not been caught by last caller in the `stack`.
+        raise CoStackFailure(shadow_tb)
