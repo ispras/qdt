3ff75660956b5df6b2fbe45a01f44d42fbf82b68
Shliapin Ilia <ilil01@ispras.ru>
2019-11-20 17:53:29 +0300

    tests: add test for MSP430 'bic' instruction with 8-bit operands
    Signed-off-by: Shliapin Ilia <ilil01@ispras.ru>

---
+++ c2t/tests/l_bic_8_1.c
@@ -0,0 +1,23 @@
+/* Logical instruction */
+
+#include <stdint.h>
+
+int main(void)
+{
+    volatile int8_t a = 0x2a, b = 0x5, c;
+
+#if __MSP430__ == 1
+    asm volatile (
+        "mov %[srcb], %[dst]; \
+         bic %[srca], %[dst];"
+        : [dst] "=rm" (c)
+        : [srca] "rm" (a),
+          [srcb] "rm" (b)
+    );
+#else
+    c = ~a & b;
+#endif
+    c = 0;  //$ch.c
+
+    return 0;   //$bre
+}
