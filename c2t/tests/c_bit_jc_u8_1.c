dac5e22e77e0ca53157c8d621a73ffb8f6a3d952
Shliapin Ilia <ilil01@ispras.ru>
2019-11-28 16:18:34 +0300

    tests: added two tests for msp430 'bit' and 'jnc' control flow instructions with two 8-bit and 16-bit operands
    Signed-off-by: Shliapin Ilia <ilil01@ispras.ru>

---
+++ c2t/tests/c_bit_jc_u8_1.c
@@ -0,0 +1,28 @@
+/* Control flow instruction */
+
+#include <stdint.h>
+
+int main(void)
+{
+    volatile uint8_t a = 0x8, b = 0xaa, c;
+#if __MSP430__ == 1
+    asm volatile goto (
+        "bit.b %[src], %[dst] \n\
+         jnc %l[label]"
+        :
+        : [dst] "rm" (b),
+          [src] "rm" (a)
+        : "cc"
+        : label
+    );
+#else
+    if (a & b) {
+        goto label;
+    }
+#endif
+    c = a;  //$br
+label:
+    c = b;  //$br
+
+    return 0;   //$bre
+}
