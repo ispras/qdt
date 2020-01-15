3cbd31b5f6ba17e7590b814473fe1d5bb03ac86c
Efimov Vasily <real@ispras.ru>
2020-01-15 17:27:54 +0300

    CoDispatcher: do not print task profiling during normal operation
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- common/co_dispatcher.py
+++ common/co_dispatcher.py
@@ -28,6 +28,12 @@ from traceback import (
 from six import (
     StringIO
 )
+from .os_wrappers import (
+    ee
+)
+
+
+PROFILE_COTASK = ee("QDT_PROFILE_COTASK")
 
 
 class FailedCallee(RuntimeError):
@@ -215,7 +221,7 @@ after last statement in the corresponding callable object.
                     ready = True
 
             ti = t1 - t0
-            if ti > 0.05:
+            if PROFILE_COTASK and ti > 0.05:
                 sys.stderr.write("Task %s consumed %f sec during iteration "
                     # file:line is the line reference format supported by
                     # Eclipse IDE Console.
