fd9c9f6775b4a4b8ec0539a8d68999501d6b05e8
Efimov Vasily <real@ispras.ru>
2020-01-27 15:59:20 +0300

    XXX: notifier profiling, __del__ is not called sometimes
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- common/notifier.py
+++ common/notifier.py
@@ -8,6 +8,12 @@ from inspect import (
 from .os_wrappers import (
     ee
 )
+from time import (
+    time
+)
+
+
+PROFILING = ee("QDT_NOTIFIER_PROFILING")
 
 
 def gen_init_wrapper(wrapped_init, cb_list_name):
@@ -55,6 +61,7 @@ transparent.
     # Define callback list before calling of original __init__ to allow
     # it assign watchers.
     code += "    setattr(self, '" + cb_list_name + "', [])\n"
+
     code += "    wrapped_init(" + ", ".join(arg_to_init) + ")\n"
 
     if ee("QDT_DEBUG_NOTIFIER"):
@@ -69,11 +76,24 @@ transparent.
 # "Function factory" approach is used to meet "late binding" problem.
 # http://stackoverflow.com/questions/3431676/creating-functions-in-a-loop/
 
-def gen_event_helpers(klass, event):
+def gen_event_helpers(klass, event, event_stats):
     # Callback list is private.
     cb_list_name = "_" + klass.__name__ + "__" + event
 
     init_wrapper = gen_init_wrapper(klass.__init__, cb_list_name)
+
+    if PROFILING:
+        def profile_callback(cb, a, kw):
+            t0 = time()
+            print("notify " + str(cb))
+            try:
+                cb(*a, **kw)
+            finally:
+                t1 = time()
+                event_stats.setdefault(cb, []).append(t1 - t0)
+    else:
+        def profile_callback(cb, a, kw):
+            cb(*a, **kw)
 
     def add_callback(self, callback):
         getattr(self, cb_list_name).append(callback)
@@ -88,15 +108,21 @@ def gen_event_helpers(klass, event):
         # copied before the process.
         for callback in list(callbacks):
             # Callback denial does not take effect during current notification.
-            callback(*args, **kw)
+            profile_callback(callback, args, kw)
 
     return init_wrapper, add_callback, remove_callback, notify
 
 def notifier(*events):
     def add_events(klass, events = events):
+
+        # Reference to statistics is not explicitly stored anywhere.
+        # It's "bound" as local variable inside dynamically generated
+        # functions.
+        stats = {}
+
         for event in events:
             __init_wrapper__, add_callback, remove_callback, __notify = \
-                gen_event_helpers(klass, event
+                gen_event_helpers(klass, event, stats.setdefault(event, {})
             )
 
             klass.__init__ = __init_wrapper__
@@ -127,6 +153,24 @@ def notifier(*events):
             klass._events = tuple(events)
         else:
             klass._events += tuple(events)
+
+        if PROFILING:
+            def _del(self):
+                print("del")
+                for e in events:
+                    estats = stats[e]
+                    print(klass.__name__ + "." + e)
+                    if not estats:
+                        print("no notifications")
+                        continue
+
+                    for cb, times in estats.items():
+                        print("%s %f %f %f" % (cb.__name__,
+                            min(times), max(times),
+                            sum(times) / len(times)
+                        ))
+
+            klass.__del__ = _del
 
         return klass
 
