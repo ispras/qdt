dbbd3eb1f8a4ff4de53df0b1a2ecc8a42b8e33ab
Efimov Vasily <real@ispras.ru>
2020-03-13 16:51:31 +0300

    common: coroutine oriented pipeline API
    Signed-off-by: Efimov Vasily <real@ispras.ru>

---
+++ common/co_pipeline.py
@@ -0,0 +1,54 @@
+__all__ = [
+    "pipeline"
+  , "co_pipeline"
+  , "pipeline_iter"
+]
+
+
+def pipeline(*stages):
+    # Startup.
+    for stage in stages:
+        if next(stage) is not None:
+            raise ValueError("%s does not follow pipeline protocol" % stage)
+
+    while True:
+        # Input to first stage is always `None`.
+        # I.e. it must be an actual input producer.
+        # Other stages are never given `None` as input.
+        intermediate = None
+
+        for stage in stages:
+            try:
+                intermediate = stage.send(intermediate)
+            except StopIteration:
+                # Normal end of work.
+                return
+
+            if intermediate is None:
+                break
+        else:
+            yield intermediate
+
+
+def co_pipeline(*stages):
+    "A pipeline for `CoDispatcher`."
+    for _ in pipeline(*stages):
+        # Output of last stage is always ignored in CoDispatcher task mode.
+        # I.e. last stage must be an actual output consumer.
+
+        # The pipeline always can proceed.
+        yield True
+
+
+def pipeline_iter(iterable):
+    "An `iter` analog for co_pipeline*."
+
+    if (yield) is not None:
+        raise RuntimeError(
+            "pipeline_iter must be used as a FIRST stage of pipeline"
+        )
+
+    for item in iter(iterable):
+        yield item
+        # We actually should check `yield` returned `None` like above.
+        # But it's too expensive for fool protection.
