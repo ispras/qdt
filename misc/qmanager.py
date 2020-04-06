27f2a501800396f2bcae7a6dd209e632e4970557
Efimov Vasily <laer.18@gmail.com>
2020-04-07 00:35:15 +0300

    QMGUI: do not wrap path in " during copying to clipboard
    Signed-off-by: Efimov Vasily <laer.18@gmail.com>

--- misc/qmanager.py
+++ misc/qmanager.py
@@ -3,7 +3,6 @@
 from common import (
     QRepo,
     makedirs,
-    cli_repr,
     bidict,
     mlget as _,
     Persistent
@@ -276,7 +275,7 @@ class QMGUI(GUITk):
         item = tv.item
 
         # Path of only first selected item is copied to clipboard.
-        text = cli_repr(item(sel[0], "text"))
+        text = item(sel[0], "text")
 
         self.clipboard_clear()
         self.clipboard_append(text)
