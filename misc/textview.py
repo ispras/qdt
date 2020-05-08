02c2f2edf4a6e930f639d2d25b9bc0eab32ea207
Efimov Vasily <laer.18@gmail.com>
2020-05-08 21:02:39 +0300

    misc: text viewer (for big files)
    Signed-off-by: Efimov Vasily <laer.18@gmail.com>
    
    squashed commits:
    TextViewerWindow.co_build_index: update total lines
    TextViewerWindow: lineidx & lineno
    TextViewerWindow.draw: improve
    TextViewerWindow: one more debug byte, need correct line ending
    TextViewerWindow: horisontal scrolling
    TextViewerWindow: `yscrollcommand` callback reference
    textview: use `TextCanvas`
    TextViewerWindow: give text widget input focus

---
+++ misc/textview.py
@@ -0,0 +1,87 @@
+from argparse import (
+    ArgumentParser,
+)
+from common import (
+    ee,
+    LineNoStream,
+    mlget as _,
+)
+from widgets import (
+    add_scrollbars_native,
+    TextCanvas,
+    GUITk,
+    Statusbar,
+)
+from six.moves.tkinter_ttk import (
+    Sizegrip,
+)
+
+DEBUG = ee("DEBUG_TEXTVIEW", "False")
+DEBUG_STREAM_SIZE = 100001
+
+
+class TextViewerWindow(GUITk, object):
+
+    def __init__(self):
+        GUITk.__init__(self)
+        self.title(_("Text Viewer"))
+
+        self.columnconfigure(0, weight = 1)
+        self.columnconfigure(1, weight = 0)
+
+        row = 0
+        self.rowconfigure(row, weight = 1)
+
+        self._text = text = TextCanvas(self)
+        text.grid(row = row, column = 0, sticky = "NESW")
+
+        add_scrollbars_native(self, text, row = row)
+        row += 1;
+
+        row += 1; self.rowconfigure(row, weight = 0)
+        sb = Statusbar(self)
+        sb.grid(row = row, column = 0, sticky = "NESW")
+
+        Sizegrip(self).grid(row = row, column = 1, sticky = "NESW")
+
+        sb.right(_("%s/%s") % (text._var_lineno, text._var_total_lines))
+
+        self._file_name = None
+
+        text.focus_set()
+
+    @property
+    def file_name(self):
+        return self._file_name
+
+    @file_name.setter
+    def file_name(self, file_name):
+        if self._file_name == file_name:
+            return
+        self._file_name = file_name
+
+        if DEBUG:
+            stream = LineNoStream(size = DEBUG_STREAM_SIZE)
+        else:
+            stream = open(file_name, "rb")
+
+        text = self._text
+        text.stream = stream
+        self.enqueue(text.co_build_index())
+
+
+def main():
+    ap = ArgumentParser(
+        description = "A Text Viewer",
+    )
+    ap.add_argument("file_name")
+
+    args = ap.parse_args()
+
+    w = TextViewerWindow()
+    w.file_name = args.file_name
+    w.mainloop()
+
+
+if __name__ == "__main__":
+    exit(main() or 0)
