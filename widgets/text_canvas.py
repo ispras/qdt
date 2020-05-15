114617fe34e21c637b117a9c17187f86d6f6f321
Efimov Vasily <laer.18@gmail.com>
2020-05-15 19:29:47 +0300

    TextCanvas: selection by pointer
    Signed-off-by: Efimov Vasily <laer.18@gmail.com>

--- widgets/text_canvas.py
+++ widgets/text_canvas.py
@@ -20,6 +20,10 @@ from common import (
 )
 
 
+# TextCanvas states
+class selecting_started: pass
+class selecting: pass
+
 class TextCanvas(Canvas, object):
     """ Shows big raw stream with text.
     """
@@ -55,7 +59,8 @@ class TextCanvas(Canvas, object):
         ]
         self._main_font, self._lineno_font = fonts
         self._linespace = max(f.metrics("linespace") for f in fonts)
-        self._ylinepadding = 0 # 1
+        # Note, padding space is used for selected lines highlighting.
+        self._ylinepadding = 1
         self._lineno_pading = 10
         self._page_size = 100 # in lines
         self._page_width = 0 # in Canvas units
@@ -65,6 +70,17 @@ class TextCanvas(Canvas, object):
         self._stream = None
         # trigger the property
         self.stream = stream
+
+        self._state = None
+
+        # Selection is defined by two tuples: (lineidx, charidx)
+        self._sel_start = None # first selected char
+        self._sel_limit = None # a char immediately after last selected one
+        self.selection_threshold = 10
+
+        self.bind("<ButtonPress-1>", self._on_bt1_press, "+")
+        self.bind("<ButtonRelease-1>", self._on_bt1_release, "+")
+        self.bind("<Motion>", self._on_motion, "+")
 
     @property
     def stream(self):
@@ -215,8 +231,7 @@ class TextCanvas(Canvas, object):
         lineno_font = self._lineno_font
         main_font = self._main_font
         main_font_measure = main_font.measure
-        lineno_pading = self._lineno_pading
-        linespace = self._linespace
+        ylinepadding = self._ylinepadding
 
         # read two chunks
         citer = index.iter_chunks(stream, lineidx)
@@ -237,24 +252,28 @@ class TextCanvas(Canvas, object):
                 lines.append(u"")
 
         lines_offset = lineidx - start_line
+        picked_lines = lines[lines_offset:]
+
+        # conventionally, line enumeration starts from 1
+        cur_lineno = lineidx + 1
 
         # space for line numbers
-        lineno_width = lineno_font.measure(str(start_line + len(lines)))
-        all_xshift = lineno_width
+        lineno_width = lineno_font.measure(str(cur_lineno + len(picked_lines)))
 
-        yshift = 0
-        yinc = linespace + self._ylinepadding
+        lineno_end_x = lineno_width + self._lineno_pading
+        text_start_x = lineno_end_x - self._x_offset
 
+        lines_width = []
+
+        # y coordinate iteration parameters
         view_height = self.winfo_height()
-        x = all_xshift
-        y = self._ylinepadding + yshift
-        x_text_shift = lineno_pading - self._x_offset
-
-        max_text_width = 0
+        y_start = ylinepadding
+        y = y_start
+        y_inc = self._linespace + ylinepadding
 
         # place opaque rectangle below line numbers and above main text
         self.create_rectangle(
-            0, 0, all_xshift + lineno_pading, view_height,
+            0, 0, lineno_end_x, view_height,
             fill = "grey",
             outline = "white",
         )
@@ -263,20 +282,18 @@ class TextCanvas(Canvas, object):
         create_text = self.create_text
         lower = self.lower
 
-        # conventionally, line enumeration starts from 1
-        cur_lineno = lineidx + 1
-        for cur_lineno, line in enumerate(lines[lines_offset:], cur_lineno):
+        for cur_lineno, line in enumerate(picked_lines, cur_lineno):
             if view_height <= y:
                 break
 
-            create_text(x, y,
+            create_text(lineno_end_x, y,
                 text = cur_lineno,
                 justify = RIGHT,
                 anchor = "ne",
                 font = lineno_font,
                 fill = "white",
             )
-            line_iid = create_text(x + x_text_shift, y,
+            line_iid = create_text(text_start_x, y,
                 text = line,
                 justify = LEFT,
                 anchor = "nw",
@@ -284,14 +301,76 @@ class TextCanvas(Canvas, object):
             )
             lower(line_iid)
 
-            max_text_width = max(max_text_width, main_font_measure(line))
-            y += yinc
+            lines_width.append(main_font_measure(line))
+
+            y += y_inc
+
+        sel_start = self._sel_start
+
+        # Selected lines highlighting.
+        # It's not a loop actually.
+        # This magic is used to prevent nested `if`s using `break`.
+        while sel_start is not None:
+            ss_l, ss_c = sel_start
+            sl_l, sl_c = self._sel_limit
+
+            if ss_l < lineidx:
+                ss_l = lineidx
+                ss_c = 0
+
+                if sl_l < ss_l:
+                    # selected block is above visible lines
+                    break
+
+            first_line_idx = ss_l - lineidx
+
+            # highlighting is underlining
+            y = y_start + first_line_idx * y_inc + y_inc - ylinepadding
+            if view_height <= y:
+                # selected block is below visible lines
+                break
+
+            first_line = picked_lines[first_line_idx]
+
+            # cache
+            create_line = self.create_line
+
+            # first selected line is highlighted from first selected character
+            sel_start_x = text_start_x + main_font_measure(first_line[:ss_c])
+            try:
+                if ss_l == sl_l:
+                    # only one line is selected
+                    sel_end_x = text_start_x + \
+                        main_font_measure(first_line[:sl_c])
+                    break
+                else:
+                    sel_end_x = text_start_x + lines_width[first_line_idx]
+            finally:
+                lower(create_line(sel_start_x, y, sel_end_x, y))
+
+            y += y_inc
+
+            # intermediate selected lines are fully highlighted
+            last_line_idx = sl_l - lineidx
+            for width in lines_width[first_line_idx + 1:last_line_idx]:
+                if view_height <= y:
+                    break
+                lower(create_line(text_start_x, y, text_start_x + width, y))
+                y += y_inc
+
+            else:
+                # last selected line is highlighted to last selected char
+                last_line = picked_lines[last_line_idx]
+                sel_end_x = text_start_x + main_font_measure(last_line[:sl_c])
+                lower(create_line(text_start_x, y, sel_end_x, y))
+
+            break
 
         # update horizontal scrolling
         self._page_width = max(0,
-            self.winfo_width() - lineno_width - lineno_pading
+            self.winfo_width() - lineno_width - self._lineno_pading
         )
-        self._max_text_width = max_text_width
+        self._max_text_width = max(lines_width) if lines_width else 0
         self._update_hsb()
 
         # update page size
@@ -304,6 +383,12 @@ class TextCanvas(Canvas, object):
         if self._page_size != page_size:
             self._page_size = page_size
             self._update_vsb()
+
+        # preserve some values
+        self._text_start_x = text_start_x
+        self._picked_lines = picked_lines
+        self._y_start = y_start
+        self._y_inc = y_inc
 
     def _on_configure(self, __):
         self.draw()
@@ -358,3 +443,103 @@ class TextCanvas(Canvas, object):
 
     def _on_next(self, __):
         self._yview_scroll(1, "pages")
+
+    def _lineidx_charidx(self, x, y):
+        # cache
+        picked_lines = self._picked_lines
+
+        x -= self._text_start_x
+
+        # get line index
+        rel_lineidx = (y - self._y_start) // self._y_inc
+
+        if x < 0:
+            # map to last character in previous line
+            rel_lineidx -= 1
+
+        lineidx = self.lineidx + rel_lineidx
+
+        # get char index
+        try:
+            line = picked_lines[rel_lineidx]
+        except IndexError:
+            # _lineidx_charidx is only used for x, y of mouse pointer.
+            # So, where are two cases:
+            # 1. EOF is visible
+            # 2. a very very tall display
+            return (
+                self.lineidx + len(picked_lines) - 1,
+                len(picked_lines[-1])
+            )
+
+        # bisect is used to support for non-monospaced fonts, e.g. Serif.
+        right = len(line)
+
+        if x < 0:
+            return (lineidx, right)
+
+        # Allow to select character right after last one in the line
+        # Note, result is in range [0, {initial `right` value}).
+        right += 1
+
+        left = 0
+        measure = self._main_font.measure
+
+        while True:
+            mid = (left + right) >> 1
+            if x < measure(line[:mid]):
+                if right == mid:
+                    break
+                right = mid
+            else:
+                if left == mid:
+                    break
+                left = mid
+
+        charidx = mid
+
+        return (lineidx, charidx)
+
+    def _on_bt1_press(self, e):
+        if self._state is not None:
+            return
+        if self._sel_start is not None:
+            self._sel_start = self._sel_limit = None
+            self.draw()
+
+        self._state = selecting_started
+        self._sel_start_point = e.x, e.y
+
+    def _on_bt1_release(self, __):
+        if self._state is selecting_started:
+            del self._sel_start_point
+            self._state = None
+        elif self._state is selecting:
+            self._state = None
+            del self._sel_first
+
+    def _on_motion(self, e):
+        x, y = e.x, e.y
+
+        do_selecting = False
+
+        if self._state is selecting_started:
+            sx, sy = self._sel_start_point
+            if abs(sx - x) + abs(sy - y) > self.selection_threshold:
+                self._sel_first = self._lineidx_charidx(sx, sy)
+                self._state = selecting
+                del self._sel_start_point
+                do_selecting = True
+        elif self._state is selecting:
+            do_selecting = True
+
+        if do_selecting:
+            first = self._sel_first
+            lineidx, charidx = self._lineidx_charidx(x, y)
+            if lineidx < first[0]:
+                self._sel_start = (lineidx, charidx)
+                self._sel_limit = first
+            else:
+                self._sel_start = first
+                self._sel_limit = (lineidx, charidx + 1)
+            self.draw()
