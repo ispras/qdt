92904a6ac9ca8c70d99c65d4669ad40440974172
Efimov Vasily <laer.18@gmail.com>
2020-05-20 00:37:10 +0300

    GUIToplevel: `property` access to "topmost" attribute
    Signed-off-by: Efimov Vasily <laer.18@gmail.com>

--- widgets/gui_toplevel.py
+++ widgets/gui_toplevel.py
@@ -9,8 +9,22 @@ from .logo import (
     set_logo
 )
 
-class GUIToplevel(VarToplevel):
+
+# `object` superclass is required for `property` decorator.
+class GUIToplevel(VarToplevel, object):
+
     def __init__(self, *args, **kw):
         VarToplevel.__init__(self, *args, **kw)
 
         set_logo(self)
+
+    # Makes settings window always on top.
+    # Is there more pythonic interface?
+    # http://effbot.org/tkinterbook/wm.htm#Tkinter.Wm.attributes-method
+    @property
+    def topmost(self):
+        return self.attributes("-topmost")
+
+    @topmost.setter
+    def topmost(self, val):
+        self.attributes("-topmost", val)
