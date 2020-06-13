af37e5d2607c882637a7f2b98a443c0622f56263
Efimov Vasily <laer.18@gmail.com>
2020-06-13 17:45:06 +0300

    common: makedirs emulation for Py2
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- common/os_wrappers.py
+++ common/os_wrappers.py
@@ -8,6 +8,7 @@ __all__ = [
   , "cli_repr"
   , "bind_all_mouse_wheel"
   , "bind_mouse_wheel"
+  , "makedirs"
 ]
 
 from os import (
@@ -37,6 +38,47 @@ from .compat import (
 from platform import (
     system
 )
+from six import (
+    PY2
+)
+from os import (
+    makedirs
+)
+if PY2:
+    from os.path import (
+        split,
+        exists
+    )
+    from os import (
+        mkdir
+    )
+    from errno import (
+        EEXIST
+    )
+
+    py2_makedirs = makedirs
+
+    def makedirs(name, mode = 0o777, exist_ok = False):
+        "Py3 makedirs emulation for Py2"
+        if not exist_ok:
+            return py2_makedirs(name, mode)
+
+        head, tail = split(name)
+        if not tail:
+            head, tail = split(head)
+        if head and tail and not exists(head):
+            try:
+                makedirs(head, mode, exist_ok = True)
+            except OSError as e:
+                # be happy if someone already created the path
+                if e.errno != EEXIST:
+                    raise
+            if tail == ".": # xxx/newdir/. exists if xxx/newdir exists
+                return
+        if not exists(name) or not exist_ok:
+            # If name exists and it is not "ok", then call `mkdir` because it
+            # raises the appropriate exception.
+            mkdir(name, mode)
 
 
 # OS file path separator in binary type
