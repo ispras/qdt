57d669eefacbe039e20fd44bcc9285c79159c457
Danila Bogomolov <bda@ispras.ru>
2020-06-20 06:54:19 +0300

    instruction.py: implement the construction of the instruction tree

--- qemu/cpu/instruction.py
+++ qemu/cpu/instruction.py
@@ -4,17 +4,26 @@ __all__ = [
       , "Opcode"
       , "Reserved"
   , "Instruction"
+  , "build_instruction_tree_root"
 ]
 
 from collections import (
     OrderedDict,
+    defaultdict,
 )
 from common import (
+    ee,
     lazy,
+)
+from itertools import (
+    combinations,
 )
 from six import (
     integer_types,
 )
+
+
+SHOW_INTERSECTION_WARNINGS = ee("QDT_SHOW_INTERSECTION_WARNINGS")
 
 
 NON_OPCODE_BIT = "x"
@@ -179,3 +188,200 @@ class Instruction(object):
 
         for name, parts in operands_dict.items():
             yield name, sorted(parts, key = lambda x: (x.num, x.subnum))
+
+
+class InstructionTreeNode(object):
+
+    def __init__(self, opcode = None):
+        self.instruction = None
+        self.opcode = opcode
+        self.subtree = OrderedDict()
+        self.reads_desc = []
+
+
+def common_bits_for_opcodes(instructions):
+    "Finds bit numbers occupied by opcodes in all instructions."
+
+    return instructions[0].opcode_bits.intersection(
+        *[i.opcode_bits for i in instructions[1:]]
+    )
+
+
+def bits_to_intervals(bits):
+    """ Converts bit numbers to bit intervals.
+
+:returns: list of tuples: [(off_1, len_1), ..., (off_k, len_k), ...]
+    """
+
+    if not bits:
+        return []
+
+    bit_numbers = sorted(bits)
+    result = []
+    offset = prev_bit = bit_numbers[0]
+    length = 1
+
+    for bit in bit_numbers[1:]:
+        if bit - prev_bit == 1:
+            length += 1
+        else:
+            result.append((offset, length))
+            offset = bit
+            length = 1
+
+        prev_bit = bit
+
+    result.append((offset, length))
+    return result
+
+
+def split_intervals(intervals, read_size):
+    "Splits intervals by read_size."
+
+    new_intervals = []
+
+    for i in intervals:
+        cur_offset, length = i
+
+        if (cur_offset // read_size != (cur_offset + length - 1) // read_size):
+            while length > 0:
+                new_offset = (cur_offset // read_size + 1) * read_size
+                new_length = min(new_offset - cur_offset, length)
+
+                new_intervals.append((cur_offset, new_length))
+
+                length -= new_offset - cur_offset
+                cur_offset = new_offset
+        else:
+            new_intervals.append(i)
+
+    return new_intervals
+
+
+def build_instruction_tree(node, instructions, read_size, checked_bits,
+    show_subtree_warnings = True
+):
+    min_len = min(len(i) for i in instructions)
+    # temporary info for reads description calculation
+    node.limit_read = min_len
+
+    common_bits = common_bits_for_opcodes(instructions)
+    unchecked_bits = common_bits - checked_bits
+    unchecked_opcs = split_intervals(
+        bits_to_intervals(unchecked_bits), read_size
+    )
+
+    for opc in unchecked_opcs:
+        opcs = [i.get_opcode_part(opc) for i in instructions]
+
+        if len(set(opcs)) > 1:
+            break
+    else:
+        if len(instructions) == 1:
+            i = instructions[0]
+
+            for opc in unchecked_opcs:
+                node.opcode = opc
+                key = i.get_opcode_part(opc)
+                node.subtree[key] = n = InstructionTreeNode()
+                node = n
+                # temporary info for reads description calculation
+                node.limit_read = min_len
+
+            node.instruction = i
+            return
+        else:
+            max_len = max(len(i) for i in instructions)
+            min_len_bits = integer_set(0, min_len)
+
+            for i in instructions:
+                for f in i.fields:
+                    field_bits = integer_set(f.offset, f.length)
+                    unchecked_bits = (field_bits - checked_bits) & min_len_bits
+                    unchecked_opcs = split_intervals(
+                        bits_to_intervals(unchecked_bits), read_size
+                    )
+
+                    if not unchecked_opcs:
+                        continue
+
+                    for opc in unchecked_opcs:
+                        opcs = [j.get_opcode_part(opc) for j in instructions]
+
+                        if len(set(opcs)) > 1:
+                            break
+                    else:
+                        # not found `opc` yet
+                        continue
+                    # found `opc`
+                    break
+                else:
+                    # not found `opc` yet
+                    continue
+                # found `opc`
+                break
+            else:
+                raise RuntimeError("Unresolved conflict in instructions:"
+                    "\n    " + "\n    ".join(
+                        "{1:<{0}} {2}".format(max_len, i.opcode_bits_string,
+                            i.comment
+                        ) for i in instructions
+                    )
+                )
+
+            pe_flag = False
+
+            for i1, i2 in combinations(instructions, 2):
+                i1b = i1.opcode_bits
+                i2b = i2.opcode_bits
+
+                if not (i1b <= i2b or i1b >= i2b):
+                    pe_flag = True
+                    break
+
+            if pe_flag:
+                print("Potential error: bit check order affects instruction"
+                    " parse tree for instructions:\n    " + "\n    ".join(
+                        "{1:<{0}} {2}".format(max_len, i.opcode_bits_string,
+                            i.comment
+                        ) for i in instructions
+                    )
+                )
+
+            if (not pe_flag
+                and show_subtree_warnings
+                and SHOW_INTERSECTION_WARNINGS
+            ):
+                print("Warning: arguments and opcode intersect in instructions"
+                    ":\n    " + "\n    ".join(
+                        "{1:<{0}} {2}".format(max_len, i.opcode_bits_string,
+                            i.comment
+                        ) for i in instructions
+                    )
+                )
+
+            show_subtree_warnings = False
+
+    node.opcode = opc
+    new_checked_bits = checked_bits | integer_set(*opc)
+
+    subtree = defaultdict(list)
+
+    for i in instructions:
+        key = i.get_opcode_part(opc)
+        if key is None:
+            key = "default"
+        subtree[key].append(i)
+
+    for key, instructions in sorted(subtree.items()):
+        node.subtree[key] = n = InstructionTreeNode()
+        build_instruction_tree(n, instructions, read_size,
+            checked_bits if key == "default" else new_checked_bits,
+            show_subtree_warnings = show_subtree_warnings
+        )
+
+
+def build_instruction_tree_root(instructions, read_size):
+    node = InstructionTreeNode()
+    build_instruction_tree(node, instructions, read_size, set())
+    return node
