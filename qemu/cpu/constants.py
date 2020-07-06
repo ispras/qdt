04553bf0be27964c0dbb30e4ad50b3d6251a71e9
Efimov Vasily <laer.18@gmail.com>
2020-07-06 13:47:46 +0300

    review

--- qemu/cpu/constants.py
+++ qemu/cpu/constants.py
@@ -6,6 +6,8 @@ __all__ = [
 
 
 # sizes in bits
+# XXX: do we support arbitrary byte size or this is for code readability only?
 BYTE_SIZE = 8
 OPERAND_MAX_SIZE = 64
+# XXX: as a constant it should be a tuple
 SUPPORTED_READ_SIZES = [8, 16, 32, 64]
