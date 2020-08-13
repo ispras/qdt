11e0b98a41d9c2d5133ba7f74ff832df4e3403e9
Efimov Vasily <real@ispras.ru>
2020-08-14 00:46:43 +0300

    c2t/tests/_readme_u32_.c: avoid multiaddress breakpoints for MSP430X
    That's c2t limitation. It sets two breakpoints on one line and catch each
    twice.
    
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- c2t/tests/_readme_u32_.c
+++ c2t/tests/_readme_u32_.c
@@ -5,23 +5,33 @@ int main(void) {
 
     for (i = 0; i < 20; i++) {
         if (i % 2) {
+            c = b - a;
+
             // breakpoint lives on the first iteration
-            c = b - a; //$br
+            c = 0; //$br
         } else {
+            c = b + a;
+
             // breakpoint lives on the all iterations
-            c = b + a; //$brc
+            c = 0; //$brc
         }
 
         // Note: value of variable on the one line is checked on the next line
 
         // check values of the all variables on the first iteration
-        c = b & a; //$ch
+        c = b & a;
+
+        c = 0; //$ch
 
         // check just value of 'c' on the first iteration
-        c = b | a; //$ch.c
+        c = b | a;
+
+        c = 0; //$ch.c
 
         // check just value of 'c' on the all iterations
-        c = b ^ a; //$chc.c
+        c = b ^ a;
+
+        c = 0; //$chc.c
 
         /* combination of commands is possible
             on the first iteration: check line number, values of 'a' and 'b'
