33135f67b8d089afdfe049b7e5b2c12c5e48dd9a
Efimov Vasily <real@ispras.ru>
2021-02-19 17:20:51 +0300

    widgets: AutoVarTreeview
    Signed-off-by: Efimov Vasily <real@ispras.ru>

---
+++ widgets/auto_var_treeview.py
@@ -0,0 +1,20 @@
+__all__ = [
+    "AutoVarTreeview",
+]
+
+from .tv_width_helper import (
+    TreeviewWidthHelper,
+)
+from .var_widgets import (
+    VarTreeview,
+)
+
+
+class AutoVarTreeview(VarTreeview, TreeviewWidthHelper):
+    "A VarTreeview with column width manipulation helpers."
+
+    def __init__(self, *a, **kw):
+        VarTreeview.__init__(self, *a, **kw)
+        TreeviewWidthHelper.__init__(self,
+            auto_columns = kw.get("columns", ["#0"])
+        )
