bbe00be63b58f429bc5818586aa1cea63f223ea0
Efimov Vasily <real@ispras.ru>
2021-03-11 18:01:54 +0300

    many: use new "import all" technique (this.py), part 2
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- source/langs/__init__.py
+++ source/langs/__init__.py
@@ -1,9 +1,9 @@
 from common import (
-    iter_submodules,
-    pypath
+    pypath,
+    update_this,
 )
+update_this()
 
 # This module uses custom version of Python Lex-Yacc
 with pypath("ply"):
-    for mod in iter_submodules():
-        exec("from ." + mod + " import *")
+    from .this import *
