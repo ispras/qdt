1ca2e664741271bbb94aa9d2bb96b30ab87c7fa3
Efimov Vasily <real@ispras.ru>
2021-05-24 21:12:57 +0300

    DeviceTreeWidget: split onto `QOMTreeWidget` and `QOMTypeSelectDialog`
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- widgets/device_settings.py
+++ widgets/device_settings.py
@@ -9,7 +9,7 @@ from .var_widgets import (
     VarCheckbutton
 )
 from .device_tree_widget import (
-    DeviceTreeWidget
+    QOMTypeSelectDialog
 )
 from common import (
     mlget as _
@@ -426,7 +426,7 @@ class DeviceSettingsWidget(QOMInstanceSettingsWidget):
             version = self.mach.project.target_version
         ).qvc.device_tree
 
-        device_type = DeviceTreeWidget(self, qom_tree = device_tree).wait()
+        device_type = QOMTypeSelectDialog(self, qom_tree = device_tree).wait()
         if device_type:
             self.qom_type_var.set(device_type)
 
