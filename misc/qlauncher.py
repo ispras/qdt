4bc0fc96a780508bed1578a6f41141d5be452909
Efimov Vasily <real@ispras.ru>
2021-06-10 01:57:11 +0300

    qlauncher: time measurements
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- misc/qlauncher.py
+++ misc/qlauncher.py
@@ -1,5 +1,6 @@
 from common import (
     mlget as _,
+    notifier,
 )
 from qemu import (
     QLaunch,
@@ -8,37 +9,95 @@ from qemu import (
 from widgets import (
     GUITk,
 )
-from collections import (
-    OrderedDict,
+from time import (
+    time,
 )
+from traceback import (
+    format_exc,
+)
+
+
+@notifier(
+    "finished",
+)
+class QemuBootTimeMeasurer(ExampleQemuProcess):
+
+    def qmp_ready(self):
+        print("Resuming...")
+        self.t_resumed = time()
+        self.qmp("cont")
+
+    def co_serial(self, idx, remote):
+        sup_gen = super(QemuBootTimeMeasurer, self).co_serial(idx, remote)
+
+        chunk = None
+
+        while True:
+            chunk = (yield sup_gen.send(chunk))
+            text = chunk.decode("utf-8")
+            if "QDTAgent1" in text:
+                self.t_qdt_agent_started = time()
+                self.qmp("quit")
+
+    def finished(self):
+        self.__notify_finished()
+        try:
+            boot_duration = self.t_qdt_agent_started - self.t_resumed
+        except:
+            print("Can't measure boot duration...\n" + format_exc())
+        else:
+            self.boot_duration = boot_duration
+            print("Boot duration: " + str(boot_duration))
+
+
+class QemuBootTimeMeasureLaunch(QLaunch):
+
+    def __init__(self, binary, **kw):
+        kw["paused"] = True
+        kw["qmp"] = True
+        kw["serials"] = max(1, kw.get("serials", 1))
+        super(QemuBootTimeMeasureLaunch, self).__init__(binary, **kw)
+
+    def launch(self, *a, **kw):
+        kw["ProcessClass"] = QemuBootTimeMeasurer
+        return super(QemuBootTimeMeasureLaunch, self).launch(*a, **kw)
 
 
 def main():
     root = GUITk()
     root.title(_("Qemu Launcher"))
 
-    l = QLaunch(
-        "/home/real/work/qemu/retrace3/noretrace_5.2.0/opt/install/bin/qemu-system-i386",
-        paused = False,
-        qmp = True,
-        serials = 1,
-        extra_args = OrderedDict(
-            accel = "kvm",
-            m = "1G",
-            hda = "/media/data/Docs/ISPRAS/qemu/workloads/WinXPSP3i386/WinXPSP3i386_agent.qcow",
-            snapshot = True,
-            shutdown = False,
-            usb = True,
-            device = [
-                "usb-ehci,id=ehci",
-                "usb-host,vendorid=0x0b95,productid=0x7720",
-            ],
-            netdev = "user,id=n1,smb=/home/real/work/qemu/device_creator/agent",
-            net = "nic,model=rtl8139,netdev=n1",
-        )
-    )
+    launches = [
+        QemuBootTimeMeasureLaunch(
+            "/home/real/work/qemu/retrace3/noretrace_5.2.0/opt/install/bin/qemu-system-i386",
+            extra_args = dict(
+                accel = "kvm",
+                m = "1G",
+                hda = "/media/data/Docs/ISPRAS/qemu/workloads/WinXPSP3i386/WinXPSP3i386_agent.qcow",
+                snapshot = True,
+                # shutdown = False,
+                usb = True,
+                device = [
+                    "usb-ehci,id=ehci",
+                    "usb-host,vendorid=0x0b95,productid=0x7720",
+                ],
+                netdev = "user,id=n1,smb=/home/real/work/qemu/device_creator/agent",
+                net = "nic,model=rtl8139,netdev=n1",
+            )
+        ),
+        QemuBootTimeMeasureLaunch(
+            "/home/real/work/qemu/retrace3/opt/install/bin/qemu-system-i386",
+            extra_args = dict(
+                m = "1G",
+                hda = "/media/data/Docs/ISPRAS/qemu/workloads/WinXPSP3i386/WinXPSP3i386_agent.qcow",
+                snapshot = True,
+            )
+        ),
+    ]
 
-    qproc = l.launch(root.task_manager, ProcessClass = ExampleQemuProcess)
+    qproc = launches[1].launch(root.task_manager)
+
+    qproc.watch_finished(lambda : root.after(1, root.destroy))
 
     root.mainloop()
 
