5071c81f04bf938f4b80648b7d930e999f9bab64
Efimov Vasily <real@ispras.ru>
2021-06-24 17:41:43 +0300

    common.qrepo: `BuildDir`
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- common/qrepo.py
+++ common/qrepo.py
@@ -1,5 +1,10 @@
 __all__ = [
     "QRepo"
+  , "BuildDir"
+  , "BuildDirFailure"
+      , "ConfigureFailure"
+      , "BuildFailure"
+      , "InstallFailure"
 ]
 
 from .extensible import (
@@ -25,6 +30,14 @@ from multiprocessing import (
 )
 from os.path import (
     abspath,
+    exists,
+)
+from .cli_args import (
+    arg_list,
+    configure_arg_list,
+)
+from .os_wrappers import (
+    makedirs,
 )
 
 
@@ -160,11 +173,138 @@ class QWorkTree(Extensible):
             yield False
 
 
-class QBuildDir(object):
+class SavedResult(PopenResult):
 
-    def __init__(self, path, worktree):
-        self.path = path
+    def __on_finish__(self, return_code, out_lines, err_lines):
+        self.out = out_lines
+        self.err = err_lines
+        self.return_code = return_code
+
+    def __str__(self):
+        try:
+            out, err, return_code = self.out, self.err, self.return_code
+        except AttributeError:
+            return "SavedResult: not finished"
+
+        res = ["SavedResult:\n"]
+        res.append("return code: %d\n" % return_code)
+        if out:
+            res.append("stdout:\n")
+            res.extend(l.decode("unicode_escape") for l in out)
+        else:
+            res.append("stdout empty\n")
+
+        if err:
+            res.append("stderr:\n")
+            res.extend(l.decode("unicode_escape") for l in err)
+        else:
+            res.append("stderr empty\n")
+
+        return "".join(res)
+
+
+class BuildDirFailure(Exception): pass
+
+class ConfigureFailure(BuildDirFailure): pass
+class BuildFailure(BuildDirFailure): pass
+class InstallFailure(BuildDirFailure): pass
+
+
+class BuildDir(object):
+
+    def __init__(self, worktree,
+        path = join("..", "build"),
+        prefix = join("..", "install"),
+        build_jobs = 4,
+        extra_configure_args = dict(),
+        extra_build_args = tuple(),
+        extra_install_args = tuple(),
+    ):
         self.worktree = worktree
+
+        if path.startswith('.'):
+            path = join(worktree.path, path)
+
+        if prefix.startswith('.'):
+            prefix = join(worktree.path, prefix)
+
+        self.path =  abspath(path)
+        self.prefix =  abspath(prefix)
+
+        self.build_jobs = build_jobs
+
+        self.extra_configure_args = configure_arg_list(extra_configure_args)
+        self.extra_build_args = arg_list(extra_build_args,
+            long_arg_prefix = "--",
+        )
+        self.extra_install_args = arg_list(extra_install_args,
+            long_arg_prefix = "--",
+        )
+
+    @property
+    def configure_args(self):
+        ret = []
+        ret.append("--prefix=" + self.prefix)
+        ret.extend(self.extra_configure_args)
+        return ret
+
+    @property
+    def build_args(self):
+        ret = []
+        if self.build_jobs:
+            ret.append("-j")
+            ret.append(str(self.build_jobs))
+        ret.extend(self.extra_build_args)
+        return ret
+
+    @property
+    def install_args(self):
+        ret = list(self.extra_install_args)
+        ret.append("install")
+        return ret
+
+    def need_configuration(self):
+        # TODO: check extra_configure_args
+        return not exists(join(self.path, "config.status"))
+
+    def co_configure(self):
+        if not self.need_configuration():
+            return
+
+        makedirs(self.path, exist_ok = True)
+
+        configure = join(self.worktree.path, "configure")
+
+        self.conf_result = res = SavedResult()
+
+        yield PopenWorker(configure, *self.configure_args, cwd = self.path)(
+            res
+        )
+
+        if res.return_code != 0:
+            raise ConfigureFailure(res)
+
+    def co_build(self):
+        yield self.co_configure()
+
+        self.build_result = res = SavedResult()
+
+        yield PopenWorker("make", *self.build_args, cwd = self.path)(res)
+
+        if res.return_code != 0:
+            raise BuildFailure(res)
+
+    def co_install(self):
+        yield self.co_build()
+
+        makedirs(self.prefix, exist_ok = True)
+
+        self.instal_result = res = SavedResult()
+
+        yield PopenWorker("make", *self.install_args, cwd = self.path)(res)
+
+        if res.return_code != 0:
+            raise InstallFailure(res)
 
     def __str__(self):
         return self.path
