d76ca58a816d362d1786b4d5499a39d5ef5e1ef2
Efimov Vasily <real@ispras.ru>
2021-08-13 19:24:58 +0300

    build_instruction_tree_test: process all fixed examples & random
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- misc/build_instruction_tree_test.py
+++ misc/build_instruction_tree_test.py
@@ -16,44 +16,7 @@ from random import (
 )
 
 
-# Parameters for generating a random set of instructions:
-READ_SIZE = 1    # in bytes
-MIN_INS_SIZE = 1 # in bytes
-MAX_INS_SIZE = 2 # in bytes
-INS_COUNT = 5
-
-# Or you can specify a fixed set of instructions. Each instruction must be
-# aligned by the READ_SIZE parameter specified above. Each instruction is
-# written as a unique bit-string (with priority 0 by default) or a tuple of a
-# unique bit-string and an integer priority. Example:
-# READ_SIZE = 1
-# FIXED_INSTRUCTIONS = ["1010xxxx", ("0101xx00xxxx1100", 2)]
-
-FIXED_INSTRUCTIONS = []
-
-# Examples:
-#
-# See "i1" instruction in tree.
-# All subtrees are the same with the "default" subtree.
-# READ_SIZE = 1
-# FIXED_INSTRUCTIONS = ['111xxx1x', ('x1xx01xx', 1)]
-#
-# See "i2" instruction in tree.
-# No "default" case; all infixes used; all subtrees are the same.
-# READ_SIZE = 1
-# FIXED_INSTRUCTIONS = ['1xx1xx11', 'x10100x0', ('x101xx1x', 1)]
-#
-# See "i1" and "i2" instructions in tree.
-# "default" case reassignment helps identify same subtrees.
-# READ_SIZE = 1
-# FIXED_INSTRUCTIONS = [('1xx101xx', 1), ('10xxx1xx', 2), '1xx1xx10', ('xx001111', 1)]
-#
-# See "i2" and "i3" instructions in tree.
-# Some subtrees are the same but different from the "default" subtree.
-# READ_SIZE = 1
-# FIXED_INSTRUCTIONS = ['xxxx0111', 'xx1xx100', ('1x1101xx', 1), ('xxxxx1xx', 2)]
-
-def check_hypothesis(non_opt_tree, opt_tree, instructions):
+def example_hypothesis(non_opt_tree, opt_tree, instructions):
     """ User-defined boolean function that tests a certain hypothesis based on
     the resulting trees and the list of instructions.
 
@@ -139,8 +102,52 @@ def print_tree(node, offset = ""):
             print_tree(subtree, offset + "    ")
 
 
-def main():
-    read_bitsize = READ_SIZE * BYTE_BITSIZE
+def process_fixed_instructions(fixed_instructions, *a, **kw):
+    raw_instructions = [
+        i if (type(i) is tuple) else (i, 0) for i in fixed_instructions
+    ]
+    return process_instructions(raw_instructions, *a, **kw)
+
+
+def process_random_instructions(min_ins_size, max_ins_size, ins_count,
+    *a, **kw
+):
+    kw = dict(kw)
+    kw["print_only_true"] = True
+
+    checked_sets = 0
+    while True:
+        raw_instructions = list(
+            gen_raw_instructions(min_ins_size, max_ins_size, ins_count)
+        )
+        if process_instructions(raw_instructions, *a, **kw):
+            break
+        else:
+            checked_sets += 1
+            if checked_sets % 100 == 0:
+                print("Checked sets: %d" % checked_sets)
+
+
+def gen_raw_instructions(min_ins_size, max_ins_size, ins_count):
+    "sizes are in bytes"
+
+    ins_strings = set()
+    while len(ins_strings) < ins_count:
+        ins_len = randint(min_ins_size, max_ins_size) * BYTE_BITSIZE
+        ins = ""
+        for __ in range(ins_len):
+            ins += "01x"[randint(0, 2)]
+        if ins not in ins_strings:
+            ins_strings.add(ins)
+            yield ins, randint(0, ins_count - 1)
+
+
+def process_instructions(raw_instructions, read_size,
+    check_hypothesis = example_hypothesis,
+    print_only_true = False,
+):
+    read_bitsize = read_size * BYTE_BITSIZE
+
     if read_bitsize not in SUPPORTED_READ_BITSIZES:
         raise RuntimeError(
             "Valid `read_size` values are %s bytes" % (", ".join(
@@ -148,84 +155,133 @@ def main():
             ))
         )
 
-    checked_sets = 0
-    while True:
-        if FIXED_INSTRUCTIONS:
-            raw_instructions = [
-                i if (type(i) is tuple) else (i, 0) for i in FIXED_INSTRUCTIONS
-            ]
-        else:
-            ins_strings = []
-            ins_priorities = []
-            while len(ins_strings) < INS_COUNT:
-                ins_len = randint(MIN_INS_SIZE, MAX_INS_SIZE) * BYTE_BITSIZE
-                ins = ""
-                for _ in range(ins_len):
-                    ins += "01x"[randint(0, 2)]
-                if ins not in ins_strings:
-                    ins_strings.append(ins)
-                    ins_priorities.append(randint(0, INS_COUNT - 1))
-            raw_instructions = list(zip(ins_strings, ins_priorities))
-
-        instructions = []
-        for i, (ins_str, ins_priority) in enumerate(raw_instructions):
-            fields = []
-            op_count = 0
-            for c in ins_str:
-                if c == 'x':
-                    fields.append(Operand(1, "o" + str(op_count)))
-                    op_count += 1
-                else:
-                    fields.append(Opcode(1, int(c)))
-            ins = Instruction("i" + str(i), *fields, priority = ins_priority)
-            ins.name = ins.mnemonic
-            ins.read_bitsize = read_bitsize
-            instructions.append(ins)
-
-        try:
-            non_opt_tree = InstructionTreeNode()
-            build_instruction_tree(non_opt_tree, instructions, read_bitsize,
-                optimizations = False
-            )
-            opt_tree = InstructionTreeNode()
-            build_instruction_tree(opt_tree, instructions, read_bitsize,
-                optimizations = True
-            )
-        except RuntimeError:
-            print("Instructions set: %s" % raw_instructions)
-            break
-
-        if FIXED_INSTRUCTIONS:
-            print("Tree without optimizations:")
-            print_tree(non_opt_tree)
-            print("Tree with optimizations:")
-            print_tree(opt_tree)
-            print_stats(non_opt_tree, instructions, "without optimizations")
-            print_stats(opt_tree, instructions, "with optimizations")
-            check_unreachable_instructions(instructions)
-            print("Hypothesis result: %r" % (
-                check_hypothesis(non_opt_tree, opt_tree, instructions)
-            ))
-            break
-        else:
-            if check_hypothesis(non_opt_tree, opt_tree, instructions):
-                print("Tree without optimizations:")
-                print_tree(non_opt_tree)
-                print("Tree with optimizations:")
-                print_tree(opt_tree)
-                print_stats(non_opt_tree, instructions,
-                    "without optimizations"
-                )
-                print_stats(opt_tree, instructions, "with optimizations")
-                check_unreachable_instructions(instructions)
-                print("Hypothesis result: True\n"
-                    "Instructions set: %s" % raw_instructions
-                )
-                break
+    instructions = []
+    for i, (ins_str, ins_priority) in enumerate(raw_instructions):
+        fields = []
+        op_count = 0
+        for c in ins_str:
+            if c == 'x':
+                fields.append(Operand(1, "o" + str(op_count)))
+                op_count += 1
             else:
-                checked_sets += 1
-                if checked_sets % 100 == 0:
-                    print("Checked sets: %d" % checked_sets)
+                fields.append(Opcode(1, int(c)))
+        ins = Instruction("i" + str(i), *fields, priority = ins_priority)
+        ins.name = ins.mnemonic
+        ins.read_bitsize = read_bitsize
+        instructions.append(ins)
+
+    try:
+        non_opt_tree = InstructionTreeNode()
+        build_instruction_tree(non_opt_tree, instructions, read_bitsize,
+            optimizations = False
+        )
+        opt_tree = InstructionTreeNode()
+        build_instruction_tree(opt_tree, instructions, read_bitsize,
+            optimizations = True
+        )
+    except RuntimeError:
+        print("Instructions set resulting in excepton: %s" % raw_instructions)
+        raise
+
+    res = check_hypothesis(non_opt_tree, opt_tree, instructions)
+
+    if not print_only_true or res:
+        print("Instructions set: %s" % raw_instructions)
+        print("Tree without optimizations:")
+        print_tree(non_opt_tree)
+        print("Tree with optimizations:")
+        print_tree(opt_tree)
+        print_stats(non_opt_tree, instructions, "without optimizations")
+        print_stats(opt_tree, instructions, "with optimizations")
+        check_unreachable_instructions(instructions)
+
+        print("Hypothesis result: %r" % (res,))
+
+    return res
+
+
+def main():
+    print("Fixed instructions")
+    for raw_instructions, read_size, check_hypothesis in (
+        # There you can specify a `list` of fixed instructions.
+        # Each instruction must be aligned by the read_size.
+        # Each instruction is written as a tuple of a unique bit-string and
+        # an integer priority or as a unique bit-string (with priority 0
+        # by default).
+        # There are some examples below.
+
+        (
+            [
+                "1010xxxx",
+               ("0101xx00xxxx1100", 2),
+            ],
+            1, # read_size
+            example_hypothesis,
+        ),
+
+        # See "i1" instruction in tree.
+        # All subtrees are the same with the "default" subtree.
+        (
+            [
+                "111xxx1x",
+               ("x1xx01xx", 1),
+            ],
+            1,
+            example_hypothesis,
+        ),
+
+        # See "i2" instruction in tree.
+        # No "default" case; all infixes used; all subtrees are the same.
+        (
+            [
+                "1xx1xx11",
+                "x10100x0",
+               ("x101xx1x", 1),
+            ],
+            1,
+            example_hypothesis,
+        ),
+
+        # See "i1" and "i2" instructions in tree.
+        # "default" case reassignment helps identify same subtrees.
+        (
+            [
+               ('1xx101xx', 1),
+               ('10xxx1xx', 2),
+                '1xx1xx10',
+               ('xx001111', 1),
+            ],
+            1,
+            example_hypothesis,
+        ),
+
+        # See "i2" and "i3" instructions in tree.
+        # Some subtrees are the same but different from the "default" subtree.
+        (
+            [
+                'xxxx0111',
+                'xx1xx100',
+               ('1x1101xx', 1),
+               ('xxxxx1xx', 2),
+            ],
+            1,
+            example_hypothesis,
+        ),
+    ):
+        print("\n")
+
+        process_fixed_instructions(raw_instructions, read_size,
+            check_hypothesis = check_hypothesis
+        )
+
+    print("\n\nRandom instructions\n")
+
+    process_random_instructions(
+        min_ins_size = 1,
+        max_ins_size = 2,
+        ins_count = 5,
+        read_size = 1
+    )
 
 
 if __name__ == "__main__":
