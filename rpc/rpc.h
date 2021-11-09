b71a1b6fb37d4520b0049bfcb7ccf9f7fda17e22
Efimov Vasily <real@ispras.ru>
2021-11-09 22:08:17 +0300

    RPC: simple server generation with example
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- rpc/rpc.h
+++ rpc/rpc.h
@@ -1,5 +1,5 @@
-#ifndef QDT_RPC_H
-#define QDT_RPC_H
+#ifndef QDT_RPC_RPC_H
+#define QDT_RPC_RPC_H
 
 #include <stddef.h>
 #include <stdint.h>
@@ -10,33 +10,41 @@ typedef enum {
     RPC_ERR_WRITE,
     RPC_ERR_ALLOC,
     RPC_ERR_UNIMPL_CALL,
+    RPC_ERR_BACKEND,
     RPC_ERR_COUNT
 } RPCError;
 
 typedef void * rpc_ptr_t;
 
 typedef rpc_ptr_t (*rpc_alloc_t)(size_t);
-typedef rpc_ptr_t (*rpc_free_t)(rpc_ptr_t);
+typedef void (*rpc_free_t)(rpc_ptr_t);
 
+/* can assign own heap API */
 extern rpc_alloc_t rpc_alloc;
 extern rpc_free_t rpc_free;
 
 typedef rpc_ptr_t rpc_connection_t;
 
-typedef size_t (rpc_read_t)(rpc_connection_t, rpc_ptr_t, size_t);
-typedef size_t (rpc_write_t)(rpc_connection_t, rpc_ptr_t, size_t);
+typedef size_t (*rpc_read_t)(rpc_connection_t, rpc_ptr_t, size_t);
+typedef size_t (*rpc_write_t)(rpc_connection_t, rpc_ptr_t, size_t);
 
-typedef struct RPCServer;
-typedef void *RPCBackEnd;
+typedef struct RPCServer RPCServer;
 
 RPCServer* rpc_server_new(rpc_connection_t, rpc_read_t, rpc_write_t,
         rpc_ptr_t ctx, RPCError*);
 void rpc_server_delete(RPCServer*);
+RPCError rpc_server_poll(RPCServer*);
 
-RPCBackEnd rpc_backend_new(void);
+/* internal */
+
+typedef void *RPCBackEnd;
+
+RPCBackEnd rpc_backend_new(RPCError*);
 void rpc_backend_free(RPCBackEnd, RPCError*);
-RPCError rpc_backend_handle_message(RPCBackEnd,
+RPCError rpc_backend_handle_message(RPCBackEnd, rpc_ptr_t ctx,
         rpc_ptr_t msg, uint32_t msg_size,
-        rpc_ptr_t* response, uint32_t* response_size);
+        uint8_t** response, uint32_t* response_size);
+RPCError rpc_backend_alloc_response(RPCBackEnd, uint32_t response_size,
+        uint8_t** response);
 
-#endif QDT_RPC_H
+#endif /* QDT_RPC_RPC_H */
