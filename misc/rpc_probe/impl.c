219ac088e6155149344076faa390b3a917edd1e8
Efimov Vasily <real@ispras.ru>
2022-01-25 20:15:06 +0300

    rpc_probe: remote process launching
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- misc/rpc_probe/impl.c
+++ misc/rpc_probe/impl.c
@@ -7,6 +7,9 @@
 #include <string.h>
 #include "rpc_probe.h"
 #include <stdbool.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <unistd.h> /* execve, pipe, dup2, close */
 
 #define MIN(A, B) (((B) > (A)) ? (B) : (A))
 
@@ -121,3 +124,183 @@ RPCError TestFrontEnd_version_string(RPCBackEnd be, rpc_ptr_t ctx,
     return RPC_ERR_NO;
 }
 
+int count_strings(const RPCBuffer *b)
+{
+    int n = 0;
+    const uint8_t *p = b->data;
+    const uint8_t *end = p + b->size;
+    while (p < end) {
+        n++;
+        p += strlen(p) + 1;
+    }
+    return n;
+}
+
+int string_chain2string_array(const RPCBuffer *b, char ***arr)
+{
+    int n = count_strings(b);
+    *arr = (char **)malloc(sizeof(char *) * (n + 1));
+    const uint8_t *p = b->data;
+    const uint8_t *end = p + b->size;
+    char **ap = *arr;
+    while (p < end) {
+        int l = strlen(p) + 1;
+        *ap = malloc(l);
+        memcpy(*ap, p, l);
+        ap++;
+        p += l;
+    }
+    *ap = NULL;
+    return n;
+}
+
+#define FOREACH_STRING(VAR, ARRAY, CODE) do { \
+    char **VAR = ARRAY; \
+    while (*VAR != NULL) { \
+        CODE \
+        VAR++; \
+    } \
+} while ((0))
+
+int string_array_len(char **a)
+{
+    int l = 0;
+    while (*a != NULL) {
+        l++;
+        a++;
+    }
+    return l;
+}
+
+char** string_array_join(char **a, char **b)
+{
+    int al = string_array_len(a), bl = string_array_len(b);
+    char **res = (char **) malloc(sizeof(char *) * (al + bl + 1));
+    char **rp = res, **ap = a, **bp = b;
+    int i;
+    for (i = 0; i < al; i++, ap++, rp++) {
+        int l = strlen(*ap);
+        *rp = malloc(l + 1);
+        memcpy(*rp, *ap, l + 1);
+    }
+    for (i = 0; i < bl; i++, bp++, rp++) {
+        int l = strlen(*bp);
+        *rp = malloc(l + 1);
+        memcpy(*rp, *bp, l + 1);
+    }
+    *rp = NULL;
+    return res;
+}
+
+void string_array_free(char **arr)
+{
+    char **ap = arr;
+    while (*ap != NULL) {
+        free(*ap);
+        ap++;
+    }
+    free(arr);
+}
+
+/* TODO: implement for Windows */
+RPCError TestFrontEnd_execve(RPCBackEnd be, rpc_ptr_t ctx, int32_t *ret,
+                             RPCString *filename, RPCBuffer *argv,
+                             RPCBuffer *envp)
+{
+    fprintf(stderr, "impl: execve: %s\n", filename->data);
+
+    fprintf(stderr, "impl: count_strings(argv) = %d\n",
+            count_strings(argv));
+    fprintf(stderr, "impl: count_strings(envp) = %d\n",
+            count_strings(envp));
+
+    int out_link[2], err_link[2];
+    pid_t pid;
+
+    /* TODO: error checking & cleanup */
+
+    pipe(out_link);
+    pipe(err_link);
+
+    pid = fork();
+    if (pid == 0) {
+        /* child */
+        char **argv_arr, **envp_arr;
+        string_chain2string_array(argv, &argv_arr);
+        string_chain2string_array(envp, &envp_arr);
+
+        char *arg0[] = {
+            filename->data,
+            NULL
+        };
+
+        char **full_argv_arr = string_array_join(arg0, argv_arr);
+        string_array_free(argv_arr);
+
+        int i;
+
+        i = 0;
+        FOREACH_STRING(p, full_argv_arr,
+            fprintf(stderr, "impl: argv[%d] = %s\n", i, *p);
+            i++;
+        );
+
+        i = 0;
+        FOREACH_STRING(p, envp_arr,
+            fprintf(stderr, "impl: envp[%d] = %s\n", i, *p);
+            i++;
+        );
+
+        dup2(out_link[1], STDOUT_FILENO);
+        close(out_link[0]);
+        dup2(err_link[1], STDERR_FILENO);
+        close(err_link[0]);
+
+        execve(filename->data, full_argv_arr, envp_arr);
+
+        string_array_free(full_argv_arr );
+        string_array_free(envp_arr);
+
+        exit(0);
+    } else {
+        /* parent */
+        close(out_link[1]);
+        close(err_link[1]);
+
+        RPCProcess *p;
+
+        RPCProbeCtx *probe_ctx = (RPCProbeCtx *)ctx;
+        ARRAY_ALLOC_EL(probe_ctx->processes, p);
+        p->pid = pid;
+        p->fd_out = out_link[0];
+        p->fd_err = err_link[0];
+
+        *ret = ARRAY_INDEX(probe_ctx->processes, p);
+    }
+
+    return RPC_ERR_NO;
+}
+
+RPCError TestFrontEnd_read_err(RPCBackEnd be, rpc_ptr_t ctx, RPCBuffer *ret,
+                               int32_t pid)
+{
+    /* TODO: error checking */
+    RPCProbeCtx *probe_ctx = (RPCProbeCtx *)ctx;
+    RPCProcess *p = &probe_ctx->processes[pid];
+    ret->data = probe_ctx->iobuf;
+    ret->size = read(p->fd_err, probe_ctx->iobuf, sizeof(probe_ctx->iobuf));
+
+    return RPC_ERR_NO;
+}
+
+RPCError TestFrontEnd_read_out(RPCBackEnd be, rpc_ptr_t ctx, RPCBuffer *ret,
+                               int32_t pid)
+{
+    /* TODO: error checking */
+    RPCProbeCtx *probe_ctx = (RPCProbeCtx *)ctx;
+    RPCProcess *p = &probe_ctx->processes[pid];
+    ret->data = probe_ctx->iobuf;
+    ret->size = read(p->fd_out, probe_ctx->iobuf, sizeof(probe_ctx->iobuf));
+
+    return RPC_ERR_NO;
+}
