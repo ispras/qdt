d3df5b7d8e169f8c4bb7150f039b5ba41f89c385
Efimov Vasily <real@ispras.ru>
2022-01-25 20:15:06 +0300

    rpc_probe: remote process launching
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- misc/rpc_probe/rpc_probe.h
+++ misc/rpc_probe/rpc_probe.h
@@ -1,10 +1,21 @@
 #ifndef RPC_PROBE_H
 #define RPC_PROBE_H
 
+#include "array.h"
 #include <stdbool.h>
 
 typedef struct {
+    int pid;
+    int fd_out;
+    int fd_err;
+} RPCProcess;
+
+typedef struct {
     bool working;
+
+    ARRAY_DECL(RPCProcess, processes);
+
+    char iobuf[4096];
 } RPCProbeCtx;
 
 #endif /* RPC_PROBE_H */
