cfdb1b031224f535111059a90949052eaaadd661
Efimov Vasily <real@ispras.ru>
2022-10-18 15:55:29 +0300

    QOMPropertyTypeInteger.build_val: input can be `int`
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- qemu/qom_common.py
+++ qemu/qom_common.py
@@ -51,7 +51,7 @@ class QOMPropertyTypeInteger(QOMPropertyType):
 
     @staticmethod
     def build_val(prop_val):
-        if Type.exists(prop_val):
+        if isinstance(prop_val, str) and Type.exists(prop_val):
             return str(prop_val)
         return "0x%0x" % prop_val
 
