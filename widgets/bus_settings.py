31b19f54b21c32d216c133ca5186410becc247aa
Efimov Vasily <real@ispras.ru>
2022-10-20 13:27:04 +0300

    BusSettingsWidget: `bus` is already stored in `node` of superclass
    `bus` `property` is added for external backward compatibility
     and for prettiness
    
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- widgets/bus_settings.py
+++ widgets/bus_settings.py
@@ -40,11 +40,11 @@ from .gui_frame import (
     GUIFrame
 )
 
-class BusSettingsWidget(SettingsWidget):
+# `object` is for `property`
+class BusSettingsWidget(SettingsWidget, object):
+
     def __init__(self, bus, *args, **kw):
         SettingsWidget.__init__(self, bus, *args, **kw)
-
-        self.bus = bus
 
         self.bus_fr = fr = GUIFrame(self)
         fr.pack(fill = BOTH, expand = False)
@@ -64,7 +64,7 @@ class BusSettingsWidget(SettingsWidget):
         self.cb_parent.grid(row = 0, column = 1, sticky = "NEWS")
 
         self.fields = []
-        if type(bus) is BusNode:
+        if type(self.node) is BusNode:
             self.fields.extend([
                 (_("C type"), "c_type", str),
                 (_("Casting macro"), "cast", str),
@@ -95,9 +95,14 @@ class BusSettingsWidget(SettingsWidget):
             setattr(self, "w_" + field, w)
             setattr(self, "var_" + field, v)
 
+    @property
+    def bus(self):
+        return self.node
+
     def __apply_internal__(self):
+        bus = self.node
         new_parent = self.find_node_by_link_text(self.var_parent.get())
-        cur_parent = self.bus.parent_device
+        cur_parent = bus.parent_device
 
         if new_parent is None:
             new_parent_id = -1
@@ -112,25 +117,27 @@ class BusSettingsWidget(SettingsWidget):
         if not new_parent_id == cur_parent_id:
             if new_parent_id == -1:
                 if not cur_parent_id == -1:
-                    self.mht.disconnect_child_bus(self.bus.id)
+                    self.mht.disconnect_child_bus(bus.id)
             else:
-                self.mht.append_child_bus(new_parent_id, self.bus.id)
+                self.mht.append_child_bus(new_parent_id, bus.id)
 
         for (text, field, _type) in self.fields:
             new_val = getattr(self, "var_" + field).get()
-            cur_val = getattr(self.bus, field)
+            cur_val = getattr(bus, field)
 
             if new_val == cur_val:
                 continue
 
-            self.mht.stage(MOp_SetBusAttr, field, new_val, self.bus.id)
+            self.mht.stage(MOp_SetBusAttr, field, new_val, bus.id)
 
         self.mht.set_sequence_description(
-            _("Bus %d configuration.") % self.bus.id
+            _("Bus %d configuration.") % bus.id
         )
 
     def refresh(self):
         SettingsWidget.refresh(self)
+
+        bus = self.node
 
         values = [
             DeviceSettingsWidget.gen_node_link_text(dev) for dev \
@@ -139,18 +146,19 @@ class BusSettingsWidget(SettingsWidget):
         self.cb_parent.config(values = values)
 
         self.var_parent.set(
-            DeviceSettingsWidget.gen_node_link_text(self.bus.parent_device)
+            DeviceSettingsWidget.gen_node_link_text(bus.parent_device)
         )
 
         for (text, field, _type) in self.fields:
             var = getattr(self, "var_" + field)
-            cur_val = getattr(self.bus, field)
+            cur_val = getattr(bus, field)
 
             var.set(cur_val)
 
     def on_changed(self, op, *args, **kw):
+        bus = self.node
         if isinstance(op, MOp_SetChildBus):
-            if self.bus.id in [ op.prev_bus_id, op.bus_id ]:
+            if bus.id in [ op.prev_bus_id, op.bus_id ]:
                 self.refresh()
             return
 
@@ -158,7 +166,7 @@ class BusSettingsWidget(SettingsWidget):
             return
 
         if op.writes_node():
-            if not self.bus.id in self.mach.id2node:
+            if not bus.id in self.mach.id2node:
                 self.destroy()
             else:
                 self.refresh()
