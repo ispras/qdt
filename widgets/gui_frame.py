b34c0c8c85d5373d7d1f82f1557ead4f995b6599
Efimov Vasily <real@ispras.ru>
2022-10-20 13:10:11 +0300

    GUIFrame: empty `__init__` is not required
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- widgets/gui_frame.py
+++ widgets/gui_frame.py
@@ -12,9 +12,6 @@ from .tk_unbind import (
 class GUIFrame(Frame):
     unbind = unbind
 
-    def __init__(self, *args, **kwargs):
-        Frame.__init__(self, *args, **kwargs)
-
     def enqueue(self, co_task):
         "Its toplevel must be GUITk."
         self.winfo_toplevel().enqueue(co_task)
