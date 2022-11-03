cfea61e83620a02d51acb6839bc8fa817134064d
Efimov Vasily <real@ispras.ru>
2022-11-03 17:32:50 +0300

    qemu.preprocess: some more words
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- qemu/preprocess.py
+++ qemu/preprocess.py
@@ -18,7 +18,16 @@ Making those changed in place may result in...
 Objects those automatically changes self must define a `__preprocess__`
 callable attribute which should return a copy of self (or compatible object)
 which the object is allowed to change as it wish.
+If some referenced (recursively) objects need to be changed too, the object
+must first `__preprocess__` or `copy` them.
     """
+# TODO: After preprocessing an object returned must be save to change.
+#       In general, this requires the copy to be deep.
+#       But, recursion may result in performance degradation and is not
+#       suitable for non-tree-like object graphs.
+#       We, likely, should require the `__preprocess__` method to only
+#       `__preprocess__`/`copy` objects it actually changes.
+#       And external changer must `__preprocess__`/`copy` objects it changes.
 # TODO: There already is a much more complicated mechanism that targets the
 #       same problem. See qemu/qom_desc.py. This one is more generic and
 #       should be used whatever is possible. Even replacing first one.
