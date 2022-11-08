b8b0a922f88750de10b137463df59827dfd6ff88
Danila Bogomolov <bda@ispras.ru>
2022-11-08 09:02:27 +0300

    widgets/device_settings_window.py: work on coding style

--- widgets/device_settings_window.py
+++ widgets/device_settings_window.py
@@ -2,21 +2,24 @@ __all__ = [
     "DeviceSettingsWindow"
 ]
 
-from .sysbusdevset import (
-    SystemBusDeviceSettingsWidget,
-    DeviceSettingsWidget
-)
 from common import (
-    mlget as _
+    mlget as _,
 )
-import qemu
-
 from .pci_device_settings import (
-    PCIDeviceSettingsWidget
+    PCIDeviceSettingsWidget,
+)
+from qemu import (
+    PCIExpressDeviceNode,
+    SystemBusDeviceNode,
 )
 from .settings_window import (
-    SettingsWindow
+    SettingsWindow,
 )
+from .sysbusdevset import (
+    DeviceSettingsWidget,
+    SystemBusDeviceSettingsWidget,
+)
+
 
 class DeviceSettingsWindow(SettingsWindow):
     def __init__(self, *args, **kw):
@@ -31,9 +34,9 @@ class DeviceSettingsWindow(SettingsWindow):
 
         self.rowconfigure(0, weight = 1)
 
-        if isinstance(device, qemu.SystemBusDeviceNode):
+        if isinstance(device, SystemBusDeviceNode):
             dsw_class = SystemBusDeviceSettingsWidget
-        elif isinstance(device, qemu.PCIExpressDeviceNode):
+        elif isinstance(device, PCIExpressDeviceNode):
             dsw_class = PCIDeviceSettingsWidget
         else:
             dsw_class = DeviceSettingsWidget
