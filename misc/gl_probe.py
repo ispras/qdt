829a169f6c0bc9664377f83101b74ea8d77cfdef
Efimov Vasily <real@ispras.ru>
2022-11-23 16:33:05 +0300

    gl_probe: use `libe.graphics.gl.GLArrays`
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- misc/gl_probe.py
+++ misc/gl_probe.py
@@ -3,41 +3,13 @@ sudo python3 -m pip install --upgrade pyopengl_accelerate
 sudo apt install python3-opengl togl-demos
 """
 
-from common import (
-    flatten,
-)
 from libe.graphics.gl import (
+    GLArrays,
     GLSLProgram,
 )
 
-from ctypes import (
-    c_float,
-    c_uint16,
-    c_void_p,
-    sizeof,
-)
 from OpenGL.Tk import (
     Opengl,
-)
-from OpenGL.GL import (
-    GL_ARRAY_BUFFER,
-    glBindBuffer,
-    glBufferData,
-    GL_COLOR_ARRAY,
-    glColorPointer,
-    # glDisableClientState,
-    glDrawElements,
-    GL_DYNAMIC_DRAW,
-    GL_ELEMENT_ARRAY_BUFFER,
-    glEnableClientState,
-    GL_FLOAT,
-    glGenBuffers,
-    # GL_NORMAL_ARRAY,
-    GL_TRIANGLES,
-    # GL_TEXTURE_COORD_ARRAY,
-    GL_UNSIGNED_SHORT,
-    GL_VERTEX_ARRAY,
-    glVertexPointer,
 )
 from six.moves.tkinter import (
     Frame,
@@ -61,8 +33,8 @@ class OpenGLProbe(Opengl):
         pass
 
     def redraw(self, *__, **___):
-        global elements_n
-        glDrawElements(GL_TRIANGLES, elements_n, GL_UNSIGNED_SHORT, None)
+        global a
+        a()
 
 
 b = OpenGLProbe()
@@ -87,53 +59,17 @@ void main() {
 """,
 ).use()
 
-vertices = [
-    (0., 1.),
-    (1., -.5),
-    (-1., -.5),
-]
-colors = [
-    (1., 0., 0., 1.),
-    (0., 1., 0., 1.),
-    (0., 0., 1., 1.),
-]
-indices = [
-    0, 1, 2,
-]
-elements_n = min(len(vertices), len(colors))
-
-vertex_data = list(flatten(zip(vertices, colors)))
-vertex_array = (c_float * len(vertex_data))(*vertex_data)
-
-vao, eao = glGenBuffers(2)
-
-glBindBuffer(GL_ARRAY_BUFFER, vao)
-glBufferData(GL_ARRAY_BUFFER, vertex_array, GL_DYNAMIC_DRAW)
-glBindBuffer(GL_ARRAY_BUFFER, 0)
-
-element_array = (c_uint16 * len(indices))(*indices)
-
-glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eao)
-glBufferData(GL_ELEMENT_ARRAY_BUFFER, element_array, GL_DYNAMIC_DRAW)
-glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0)
-
-glEnableClientState(GL_VERTEX_ARRAY)
-glEnableClientState(GL_COLOR_ARRAY)
-# glDisableClientState(GL_TEXTURE_COORD_ARRAY)
-# glDisableClientState(GL_NORMAL_ARRAY)
-
-glBindBuffer(GL_ARRAY_BUFFER, vao)
-
-vertex_len = len(vertices[0])
-color_len = len(colors[0])
-
-stride = sizeof(c_float) * (vertex_len + color_len)
-
-glVertexPointer(vertex_len, GL_FLOAT, stride, None)
-glColorPointer(color_len, GL_FLOAT, stride,
-    c_void_p(vertex_len * sizeof(c_float))
+a = GLArrays(
+    vertices = [
+        (0., 1.),
+        (1., -.5),
+        (-1., -.5),
+    ],
+    colors = [
+        (1., 0., 0., 1.),
+        (0., 1., 0., 1.),
+        (0., 0., 1., 1.),
+    ],
 )
-
-glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eao)
 
 root.mainloop()
