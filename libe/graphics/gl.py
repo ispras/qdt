cd2bf66ddce0c1c14b9a53d76422585734d80691
Efimov Vasily <real@ispras.ru>
2022-11-24 16:24:10 +0300

    libe: GLSLProgram: uniforms setting helpers based on shader code analyzis
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- libe/graphics/gl.py
+++ libe/graphics/gl.py
@@ -12,12 +12,22 @@ from common import (
     flatten,
     notifier,
 )
+from ..grammars.glsl.v1_50 import (
+    parser as glsl_1_50_parser,
+)
 
 from ctypes import (
     c_float,
     c_uint16,
     c_void_p,
     sizeof,
+)
+from itertools import (
+    chain,
+)
+from lark import (
+    Token,
+    Transformer,
 )
 from OpenGL.GL import (
     GL_ARRAY_BUFFER,
@@ -42,6 +52,7 @@ from OpenGL.GL import (
     glGetProgramiv,
     glGetShaderInfoLog,
     glGetShaderiv,
+    glGetUniformLocation,
     GL_INFO_LOG_LENGTH,
     glLinkProgram,
     GL_LINK_STATUS,
@@ -51,6 +62,12 @@ from OpenGL.GL import (
     GL_TEXTURE_COORD_ARRAY,
     glTexCoordPointer,
     GL_TRIANGLES,
+    glUniform1f,
+    glUniform1i,
+    glUniform2fv,
+    glUniform3fv,
+    glUniform4fv,
+    glUniformMatrix4fv,
     GL_UNSIGNED_SHORT,
     glUseProgram,
     GL_VERTEX_ARRAY,
@@ -138,24 +155,160 @@ def create_gls_program(v, f):
     return p
 
 
+# shader source code analysis
+class GLSLTypeQualifier:
+    pass
+
+UNIFORM = GLSLTypeQualifier()
+
+class GLSLTypeQualifiers(set):
+    pass
+
+class GLSLType(object):
+
+    def __init__(self):
+        self.qualifiers = GLSLTypeQualifiers()
+
+class GLSLVariable(object):
+
+    def __init__(self, glsl_type, name, initializer = None):
+        self.type = glsl_type
+        self.name = str(name)
+        self.initializer = initializer
+
+group = lambda iters: list(i for i in chain(*iters) if i is not None)
+
+class GLSLProgAnalyzer(Transformer):
+
+    def translation_unit(self, s):
+        return group(s)
+
+    def function_definition(self, s):
+        # not required
+        return [None]
+
+    def toplevel_declarations(self, s):
+        return group(s)
+
+    def declarations(self, s):
+        if isinstance(s, GLSLTypeQualifiers):
+            return None
+        if isinstance(s, list):
+            return s
+        else:
+            return [s]
+
+    def precision_declaration(self, __):
+        # Not a type declaration, but a compiller hint.
+        # E.g.: precision mediump float;
+        return None
+
+    def scalar_declaration(self, s):
+        return GLSLVariable(*s)
+
+    def fully_specified_type(self, s):
+        if len(s) == 1:
+            t = s[0]
+        else:
+            t = s[1]
+            t.qualifiers |= s[0]
+        return t
+
+    def type_qualifiers(self, s):
+        return GLSLTypeQualifiers(s)
+
+    def uniform(self, __):
+        return UNIFORM
+
+    def type_specifier_nonarray(self, s):
+        s = s[0]
+        t = GLSLType()
+        if isinstance(s, Token):
+            # CNAME or "void"
+            t.name = str(s)
+        else:
+            # struct_specifier, not implemented yet
+            t.name = "[struct]"
+        return t
+
+    def type_specifier_prec(self, s):
+        # precision_qualifier is not implemented yet
+        __, t = s
+        return t
+
+    def type_specifier_array(self, s):
+        # arrays not implemented yet
+        t = s[0]
+        return t
+
+    def type_specifier(self, s):
+        return s[0]
+
+
+# uniforms loading helpers
+uniform_loaders = dict(
+    float = glUniform1f,
+    sampler2D = glUniform1i,
+    mat4 = lambda uid, buf : glUniformMatrix4fv(uid, 1, False, buf),
+    vec2 = lambda uid, buf : glUniform2fv(uid, 1, buf),
+    vec3 = lambda uid, buf : glUniform3fv(uid, 1, buf),
+    vec4 = lambda uid, buf : glUniform4fv(uid, 1, buf),
+)
+
+
 class GLSLProgram(object):
 
     def __init__(self, vertex_code, fragment_code):
+        uniforms = {}
+
+        for code in (vertex_code, fragment_code):
+            an = GLSLProgAnalyzer()
+            tree = glsl_1_50_parser.parse(code)
+            # print(tree.pretty())
+            for i in an.transform(tree):
+                if isinstance(i, GLSLVariable):
+                    if UNIFORM in i.type.qualifiers:
+                        uniforms[i.name] = i.type.name
+
         self._vertex_code = vertex_code
         self._fragment_code = fragment_code
+        self._uniforms = uniforms  # ; print(uniforms)
 
         _ctx.watch_all(self)
         if _ctx.ready:
             self._on_ready()
 
-    def use(self):
+    def use(self, **uniforms_assignments):
         glUseProgram(self._p)
+
+        loaders = self._loaders
+
+        for n, v in uniforms_assignments.items():
+            loaders[n](v)
 
     def _on_ready(self):
         self._vertex = v = create_shader(self._vertex_code, GL_VERTEX_SHADER)
         f = create_shader(self._fragment_code, GL_FRAGMENT_SHADER)
         self._fragment = f
-        self._p = create_gls_program(v, f)
+        self._p = p = create_gls_program(v, f)
+
+        # auto uniforms handling
+        self._loaders = loaders = {}
+        for n, t in self._uniforms.items():
+            u_loc = glGetUniformLocation(p, n)
+            if u_loc < 0:
+                print("%s: uniform was dropped by linker" % (n,))
+                continue
+
+            try:
+                ldr = uniform_loaders[t]
+            except KeyError:
+                print("%s: uniform type %s loading is not implemented" % (
+                    n, t
+                ))
+                continue
+
+            loaders[n] = lambda val, u_loc = u_loc, ldr = ldr: ldr(u_loc, val)
 
 
 client_states = (
