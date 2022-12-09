43077036583685183036de2914345010e54dc0be
Efimov Vasily <real@ispras.ru>
2022-12-09 19:51:59 +0300

    misc: hdlview, a boilerplate
    Signed-off-by: Efimov Vasily <real@ispras.ru>

---
+++ misc/hdlview.py
@@ -0,0 +1,14 @@
+from libe.grammars.system_verilog.pp import (
+    system_verilog_preproc_parser,
+)
+from libe.grammars.system_verilog.sv import (
+    system_verilog_parser,
+)
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    exit(main() or 0)
