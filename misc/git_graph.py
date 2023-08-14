fc18da26e10f22bd0d3eb86f83acdaa4ba2da802
Efimov Vasily <real@ispras.ru>
2023-08-14 18:44:15 +0300

    GGVWidget: place last added items
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- misc/git_graph.py
+++ misc/git_graph.py
@@ -110,6 +110,10 @@ class GGVWidget(GUIFrame):
                 yield dgp.co_place()
             yield i
 
+        # place last added items
+        while dgp.has_work:
+            yield dgp.co_place()
+
         print("Done")
 
         tags_n = len(mg._edges)
