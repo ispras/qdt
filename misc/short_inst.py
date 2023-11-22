c4e839cfaa0cdbf7b7caacd4f17d27b4a3561da9
Efimov Vasily <real@ispras.ru>
2023-11-22 18:42:46 +0300

    misc: example of short instruction format parsing (draft)
    Signed-off-by: Efimov Vasily <real@ispras.ru>

---
+++ misc/short_inst.py
@@ -0,0 +1,49 @@
+from qemu.cpu.instruction import (
+    Operand,
+)
+from qemu.cpu.short import (
+    Short,
+)
+
+from argparse import (
+    ArgumentParser,
+)
+
+
+def main():
+    ap = ArgumentParser()
+    arg = ap.add_argument
+
+    arg("short_desc_file_name")
+
+    args = ap.parse_args()
+
+    with open(args.short_desc_file_name, "r") as f:
+        short_desc = f.read()
+
+    short_lines = short_desc.splitlines(False)
+    l = short_lines[1]
+
+    try:
+        res = Short.parse(l)
+    except:
+        res = None
+        try:
+            Short.parse(l, debug = True)
+        except:
+            pass
+
+    print(res)
+    res.read_bitsize = 32
+    print(res.bitsize)
+    offset = 0
+    for f in res.fields:
+        print("\t%2d %2d %s" % (
+            offset,
+            f.bitsize,
+            f.name if isinstance(f, Operand) else f.val,
+        ))
+        offset += f.bitsize
+
+if __name__ == "__main__":
+    exit(main() or 0)
