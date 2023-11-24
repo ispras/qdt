7a448f05b8434939c34319ed1c5b712e554e6b8f
Efimov Vasily <real@ispras.ru>
2023-11-24 15:43:57 +0300

    GridRect: fixup `_on_grid_resized` is not `listen` when needed
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- libe/common/gridrect.py
+++ libe/common/gridrect.py
@@ -31,15 +31,20 @@ the `g`rid.
     def g(self, g):
         if g is self._g:
             return
-        gcoords = self._gcoords is not None
-        if self._g is not None and gcoords:
-            self._remove_from_g()
+        gcoords = self._gcoords
+        if self._g is not None:
+            dismiss(self._on_grid_resized)
+            if gcoords is not None:
+                g.remove(self)
+                del self.coords
         if g is None:
             del self._g
         else:
             self._g = g
-            if gcoords:
-                self._add_to_g()
+            if gcoords is not None:
+                g.add(gcoords, self)
+                self.coords = g(gcoords)
+            listen(g, "resized", self._on_grid_resized)
 
     _gcoords = None
     @property
@@ -50,32 +55,21 @@ the `g`rid.
     def gcoords(self, gcoords):
         if gcoords == self._gcoords:
             return
-
-        if gcoords is None:
-            self._remove_from_g()
-            del self._gcoords
-        else:
-            self._gcoords = gcoords
-            if self._g is not None:
-                self._update_coords()
-
-    def _add_to_g(self):
-        self._update_coords()
-        listen(self._g, "resized", self._on_grid_resized)
-
-    def _update_coords(self):
         g = self._g
-        gcoords = self._gcoords
-        g.add(gcoords, self)
-        self.coords = g(gcoords)
+        if gcoords is None:
+            del self._gcoords
+            del self.coords
+            if g is not None:
+                g.remove(self)
+        else:
+            if g is not None:
+                g.add(gcoords, self)
+                self.coords = g(gcoords)
+            self._gcoords = gcoords
 
     def _on_grid_resized(self, axis, coord):
         gcoords = self._gcoords
-        assert gcoords is not None
+        if gcoords is None:
+            return
         if coord < gcoords[axis]:
             self.coords = self._g(gcoords)
-
-    def _remove_from_g(self):
-        dismiss(self._on_grid_resized)
-        self._g.remove(self)
-        del self.coords
