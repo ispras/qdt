965de4016f1b56eadfb98b43fde45356fc9cd0ad
Danila Bogomolov <bda@ispras.ru>
2023-11-29 06:03:03 +0300

    CPUType._co_gen_target_code: use `common.makedirs`

--- qemu/cpu/cpu.py
+++ qemu/cpu/cpu.py
@@ -36,6 +36,7 @@ from collections import (
 )
 from common import (
     execfile,
+    makedirs,
     mlget as _,
     ee,
     pypath,
@@ -43,9 +44,6 @@ from common import (
 )
 from itertools import (
     count,
-)
-from os import (
-    makedirs,
 )
 from os.path import (
     basename,
@@ -421,8 +419,9 @@ class CPUType(QOMCPU):
         target_folder = get_vp("target folder") + self.target_name + sep
         abs_target_folder = join(src, target_folder)
 
-        if not isdir(abs_target_folder):
-            makedirs(abs_target_folder)
+        yield True
+
+        makedirs(abs_target_folder, exist_ok = True)
 
         yield True
 
