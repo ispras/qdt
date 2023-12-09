fb6eb19fc8902feb497ae7e9de04628471e3cc1a
Danila Bogomolov <bda@ispras.ru>
2023-12-09 07:32:34 +0300

    qdc-gui.py: import only necessary names from sys

--- qdc-gui.py
+++ qdc-gui.py
@@ -79,7 +79,10 @@ import qdt
 from traceback import (
     format_exception
 )
-import sys
+from sys import (
+    stderr,
+    version as py_version,
+)
 from os.path import (
     join,
     isfile,
@@ -507,7 +510,7 @@ show it else hide it."),
         self.var_require_device_tree.set(settings.require_device_tree)
 
     def __on_listener_failed(self, e, tb):
-        sys.stderr.write("Listener failed - %s" %
+        stderr.write("Listener failed - %s" %
             "".join(format_exception(type(e), e, tb))
         )
 
@@ -1025,7 +1028,7 @@ class Settings(Persistent):
 
 
 def main():
-    print("QDT GUI on Python %s" % (sys.version,))
+    print("QDT GUI on Python %s" % (py_version,))
     parser = ArgumentParser()
 
     parser.add_argument(
