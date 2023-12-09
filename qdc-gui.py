0c4dd772f49dcdd4eaed8b2b0e6c7c5c81d3aa8f
Danila Bogomolov <bda@ispras.ru>
2023-12-09 22:24:11 +0300

    qdc-gui: preserve window width and height in settings

--- qdc-gui.py
+++ qdc-gui.py
@@ -1027,7 +1027,8 @@ class Settings(UserSettings):
             gen_chunk_graphs = False,
             translate_cpu_semantics = True,
             require_device_tree = True,
-            recent_projects = OrderedSet()
+            recent_projects = OrderedSet(),
+            geometry = (1000, 750),
         )
 
     def account_project(self, file_name):
@@ -1106,9 +1107,12 @@ def main():
     with Settings() as settings:
         root.set_user_settings(settings)
 
-        root.geometry("1000x750")
+        root.set_geometry_delayed(*settings.geometry)
 
         root.mainloop()
+
+        # only save width and height
+        settings.geometry = root.last_geometry[:2]
 
 if __name__ == '__main__':
     main()
