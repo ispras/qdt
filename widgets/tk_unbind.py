897d964cf785a1057b4245c25689545a6b680c4f
Danila Bogomolov <bda@ispras.ru>
2023-12-14 00:37:53 +0300

    widgets/tk_unbind.py: fixup `not in` expressions
    A regexp based substitution was used.
    Pattern:      not (.+) in
    Substitution: \1 not in

--- widgets/tk_unbind.py
+++ widgets/tk_unbind.py
@@ -9,7 +9,7 @@ def unbind(tk_widget, sequence, funcid = None):
         binded = tk_widget.tk.call("bind", tk_widget._w, sequence)
 
         new_binded = '\n'.join([ s for s in binded.split('\n')
-                             if s and not funcid in s ]) + '\n'
+                             if s and funcid not in s ]) + '\n'
 
         tk_widget.tk.call("bind", tk_widget._w, sequence, new_binded)
 
