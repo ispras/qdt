5b773a30f2f1d4eb4aebc5bd0f2400c7392068a4
Efimov Vasily <real@ispras.ru>
2024-01-23 18:08:16 +0300

    cpptest: log_mem_usage: implement using `byN`
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- misc/cpptest.py
+++ misc/cpptest.py
@@ -1,4 +1,5 @@
 from common import (
+    byN,
     qdtdirs,
     makedirs,
 )
@@ -15,7 +16,6 @@ from argparse import (
 )
 from itertools import (
     chain,
-    zip_longest,
 )
 from os import (
     walk,
@@ -100,9 +100,7 @@ def system_cpp(
 def log_mem_usage():
     mu = str(proc.memory_info().rss)
     mu = "_".join(reversed(tuple(
-        "".join(reversed(t)) for t in zip_longest(*[iter(reversed(mu))] * 3,
-            fillvalue = "",
-        )
+        "".join(reversed(t)) for t in byN(3, reversed(mu), fillvalue = "")
     )))
     log("memory:\n\t" + mu)
 
