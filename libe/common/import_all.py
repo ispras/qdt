f03f219fc05f04bc98fb1f2ae23713be74073ebe
Efimov Vasily <real@ispras.ru>
2024-02-01 16:11:32 +0300

    libe.common: import_all, from `common`
    Signed-off-by: Efimov Vasily <real@ispras.ru>

---
+++ libe/common/import_all.py
@@ -0,0 +1,54 @@
+__all__ = [
+    "iter_import_lines"
+  , "gen_import_code"
+  , "update_this"
+]
+
+from .caller_file_name import (
+    caller_file_name,
+)
+from .iter_submodules import (
+    iter_submodules,
+)
+from .shadow_open import (
+    shadow_open,
+)
+
+from os.path import (
+    dirname,
+    join,
+    splitext,
+)
+
+
+def iter_import_lines(module_dir):
+    for n in sorted(iter_submodules(cur_dir = module_dir)):
+        if n == "this":
+            continue
+        yield "from ." + splitext(n)[0] + " import *"
+
+
+def gen_import_code(module_dir):
+    return "\n".join(iter_import_lines(module_dir))
+
+
+def update_this():
+    """ It creates/updates this.py file that imports all submodules and can
+be parsed by IDEs.
+
+Use it in such a way in __init__.py:
+
+from common import (
+    update_this,
+)
+update_this()
+from .this import *
+    """
+
+    cur_dir = dirname(caller_file_name())
+    importer_name = join(cur_dir, "this.py")
+
+    code = gen_import_code(cur_dir)
+
+    with shadow_open(importer_name) as f:
+        f.write(code)
