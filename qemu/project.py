e53b93ee81d89cd166ee804b16b931a4c2baa641
Efimov Vasily <real@ispras.ru>
2024-02-13 19:19:34 +0300

    qemu.project: outline build system registration helpers
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- qemu/project.py
+++ qemu/project.py
@@ -2,8 +2,11 @@ __all__ = [
     "QProject"
 ]
 
+from .build import (
+    register_in_build_system,
+    register_src_in_build_system,
+)
 from common import (
-    add_line_to_file,
     callco,
     co_find_eq,
     makedirs,
@@ -16,60 +19,25 @@ from .cpu import (
 from .machine_description import (
     MachineNode,
 )
-from .makefile_patching import (
-    patch_makefile,
-)
 from source import (
     disable_auto_lock_inclusions,
     enable_auto_lock_inclusions,
     Source,
 )
-from .version import (
-    get_vp,
-)
 from .version_description import (
     QemuVersionDescription,
 )
 
-from codecs import (
-    open,
-)
-from collections import (
-    defaultdict,
-)
 from itertools import (
     count,
 )
 from os.path import (
     isabs,
-    isfile,
     join,
     normpath,
     relpath,
     split,
-    splitext,
 )
-from re import (
-    compile,
-)
-
-re_meson_ss_add = compile(b"(\w+)\s*[.]\s*add\s*[(]")
-re_meson_ss_new = compile(b"(\w+)\s*=\s*\w+\s*[.]\s*source_set\s*[(]")
-
-# TODO: Selection of configuration flag and accumulator variable
-# name is Qemu version specific. Version API must be used there.
-
-obj_var_names = defaultdict(lambda : "obj")
-obj_var_names["pci"] = "common-obj"
-obj_var_names["hw"] = "devices-dirs"
-
-config_flags = defaultdict(lambda: "y")
-config_flags["pci"] = "$(CONFIG_PCI)"
-config_flags["hw"] = "$(CONFIG_SOFTMMU)"
-
-# Note that different subdirectories and modules could be registered in "hw"
-# using other settings. But as this tool generates devices only. So, the
-# settings is chosen this way.
 
 
 class QProject(object):
@@ -154,48 +122,6 @@ class QProject(object):
 
         enable_auto_lock_inclusions()
 
-    def register_in_build_system(self, folder, known_targets):
-        tail, head = split(folder)
-
-        if head == "hw":
-            return
-
-        # Provide Makefiles in ancestors.
-        self.register_in_build_system(tail, known_targets)
-
-        # Register the folder in its parent.
-        # Note that folders whose names match Qemu target CPU architecture
-        # are implicitly included without an entry in "hw/Makefile.objs".
-        parent_dir = split(tail)[1]
-
-        if parent_dir == "hw" and known_targets and head in known_targets:
-            return
-
-        build_system = get_vp("build system")
-
-        if build_system == "Makefile":
-            parent_Makefile_obj = join(tail, "Makefile.objs")
-
-            # Add empty Makefile.objs if no one exists.
-            if not isfile(parent_Makefile_obj):
-                open(parent_Makefile_obj, "w").close()
-
-            patch_makefile(parent_Makefile_obj, head + "/",
-                obj_var_names[parent_dir], config_flags[parent_dir]
-            )
-        elif build_system == "meson":
-            parent_meson_build = join(tail, "meson.build")
-
-            # Add empty meson.build if no one exists.
-            if not isfile(parent_meson_build):
-                open(parent_meson_build, "w").close()
-
-            add_line_to_file(parent_meson_build, "subdir('%s')" % head)
-        else:
-            print("Folder (" + folder + ") registration in build system '"
-                + build_system + "' is not implemented"
-            )
-
     def gen(self, *args, **kw):
         "Backward compatibility wrapper for co_gen"
         callco(self.co_gen(*args, **kw))
@@ -208,8 +134,6 @@ class QProject(object):
         include_paths = tuple(),
         **_
     ):
-        build_system = get_vp("build system")
-
         qom_t = desc.gen_type()
 
         yield qom_t.co_gen_sources()
@@ -253,65 +177,10 @@ class QProject(object):
                 continue
 
             yield
-            self.register_in_build_system(sdir, known_targets)
+            register_in_build_system(sdir, known_targets)
 
             yield True
-
-            if build_system == "Makefile":
-                self.register_src_in_Makefile(src, sname, desc.directory)
-            elif build_system == "meson":
-                self.register_src_in_meson(src, sname, desc.directory)
-            else:
-                print("File (" + sname + ") registration in build system '"
-                    + build_system + "' is not implemented"
-                )
-
-    def register_src_in_Makefile(self, src_root, sname, directory):
-        sbase, __ = splitext(sname)
-        object_name = sbase + ".o"
-
-        hw_path = join(src_root, "hw")
-        class_hw_path = join(hw_path, directory)
-
-        Makefile_objs_class_path = join(class_hw_path, "Makefile.objs")
-
-        # If it's a new `hw` subfolder, it has no `Makefile.objs`.
-        if not isfile(Makefile_objs_class_path):
-            open(Makefile_objs_class_path, "wb").close()
-
-        patch_makefile(Makefile_objs_class_path, object_name,
-            obj_var_names[directory], config_flags[directory]
-        )
-
-    def register_src_in_meson(self, src_root, sname, directory):
-        hw_path = join(src_root, "hw")
-        class_hw_path = join(hw_path, directory)
-        meson_build = join(class_hw_path, "meson.build")
-
-        source_set = "softmmu_ss"
-
-        if isfile(meson_build):
-            # Figure out which source set is most "popular" in this file.
-            with open(meson_build, "rb") as f:
-                meson_build_data = f.read()
-            meson_build_lines = meson_build_data.splitlines()
-            used_source_sets = defaultdict(int)
-            for l in meson_build_lines:
-                mi = re_meson_ss_new.search(l) or re_meson_ss_add.search(l)
-                if mi:
-                    used_source_sets[mi.group(1)] += 1
-
-            if used_source_sets:
-                source_set = sorted(
-                    tuple((-c, s) for (s, c) in used_source_sets.items())
-                )[0][1].decode()
-        else:
-            # If it's a new `hw` subfolder, it has no `meson.build`.
-            open(meson_build, "wb").close()
-
-        line = "%s.add(files('%s'))" % (source_set, sname)
-
-        add_line_to_file(meson_build, line)
+            register_src_in_build_system(src, sname, desc.directory)
 
     # TODO: add path to `QProject`
     # TODO: def lookup_path
