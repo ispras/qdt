4003568546aa9b150aa4c2a18035992e6f986fe4
Efimov Vasily <real@ispras.ru>
2024-04-05 16:55:52 +0300

    config_cortexm3: `c2t.config.edef` usage example
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- c2t/configs/config_cortexm3.py
+++ c2t/configs/config_cortexm3.py
@@ -1,3 +1,37 @@
+from os.path import (
+    isfile,
+)
+
+
+@edef
+def C2T_QEMU_SYSTEM_ARM():
+    "Qemu full system emulator for ARM to be tested"
+    if isfile("/usr/bin/qemu-system-arm"):
+        return "/usr/bin/qemu-system-arm"
+    raise Exception("No qemu-system-arm found")
+
+@edef
+def C2T_ARM_NO_EABI_GCC():
+    "GCC ARM cross compiler to generate target executables"
+    if isfile("/usr/bin/arm-none-eabi-gcc"):
+        return "/usr/bin/arm-none-eabi-gcc"
+    raise Exception("No GCC ARM cross compiler found")
+
+@edef
+def C2T_GDBSERVER():
+    "A GDB RSP server to launch host executables"
+    if isfile("/usr/bin/gdbserver"):
+        return "/usr/bin/gdbserver"
+    raise Exception("No gdbserver found")
+
+@edef
+def C2T_GCC():
+    "GCC to generate host executables"
+    if isfile("/usr/bin/gcc"):
+        return "/usr/bin/gcc"
+    raise Exception("No host targeted GCC found")
+
+
 c2t_cfg = C2TConfig(
     rsp_target = DebugClient(
         march = "cortexm3",
@@ -5,18 +39,18 @@ c2t_cfg = C2TConfig(
         sp = "sp"
     ),
     qemu = DebugServer(Run(
-        executable = "/usr/bin/qemu-system-arm",
+        executable = C2T_QEMU_SYSTEM_ARM,
         args = ("-M netduino2 -cpu cortex-m3 -kernel {bin} -S "
             "-gdb tcp:localhost:{port} -nographic"
         )
     )),
     gdbserver = DebugServer(Run(
-        executable = "/usr/bin/gdbserver",
+        executable = C2T_GDBSERVER,
         args = "localhost:{port} {bin}"
     )),
     target_compiler = TestBuilder(
         Run( # compiler
-            executable = "/usr/bin/arm-none-eabi-gcc",
+            executable = C2T_ARM_NO_EABI_GCC,
             args = ("-DTEST -mno-unaligned-access -g -Wall -O0 "
                 "-mfix-cortex-m3-ldrd -msoft-float -mthumb "
                 "-Wno-strict-aliasing -fomit-frame-pointer -mcpu=cortex-m3 "
@@ -24,14 +58,14 @@ c2t_cfg = C2TConfig(
             )
         ),
         Run( # linker
-            executable = "/usr/bin/arm-none-eabi-gcc",
+            executable = C2T_ARM_NO_EABI_GCC,
             args = ("-mthumb -mcpu=cortex-m3 -fno-common "
                 "-T {test_dir}/misc/cortexm3_memmap -nostartfiles "
                 "-Wl,--gc-sections -Wl,-z,relro {ir}.o -o {bin}"
             )
         ),
         Run( # dissassembler
-            executable = "/usr/bin/arm-none-eabi-gcc",
+            executable = C2T_ARM_NO_EABI_GCC,
             args = ("-DTEST -mno-unaligned-access -g -Wall -O0 "
                 "-mfix-cortex-m3-ldrd -msoft-float -mthumb "
                 "-Wno-strict-aliasing -fomit-frame-pointer -mcpu=cortex-m3 "
@@ -41,7 +75,7 @@ c2t_cfg = C2TConfig(
     ),
     oracle_compiler = TestBuilder(
         Run( # compiler
-            executable = "/usr/bin/gcc",
+            executable = C2T_GCC,
             args = "-g -O0 {src} -o {bin} -no-pie"
         )
     )
