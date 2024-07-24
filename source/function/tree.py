e4153ec912fbb0421d3f3f699b1e7175e48000bf
Efimov Vasily <real@ispras.ru>
2024-07-24 14:50:24 +0300

    OpSDeref: convert `struct` to `@property`
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- source/function/tree.py
+++ source/function/tree.py
@@ -807,7 +807,9 @@ class OpSDeref(Operator):
 
         self.field = field
 
-        struct = value.type
+    @property
+    def struct(self):
+        struct = self.container.type
         # Note, pointer nesting must be at most 1.
         if isinstance(struct, Pointer):
             struct = struct.type
@@ -815,15 +817,14 @@ class OpSDeref(Operator):
         if OPSDEREF_FROM_DEFINITION:
             struct = struct.definition
 
-        # for type collection
-        self.struct = struct
-
         try:
-            struct.fields[field]
+            struct.fields[self.field]
         except KeyError:
             raise RuntimeError('Structure "%s" has no field "%s"' % (
-                struct, field
+                struct, self.field
             ))
+
+        return struct
 
     @property
     def type(self):
