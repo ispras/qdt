6d6d6d92ed3221df80bbc9cee96e20c92244ce9e
Efimov Vasily <real@ispras.ru>
2024-07-24 14:53:27 +0300

    common: `DictStack`
    Signed-off-by: Efimov Vasily <real@ispras.ru>

---
+++ common/dict_stack.py
@@ -0,0 +1,18 @@
+__all__ = [
+    "DictStack"
+]
+
+
+from .empty_dict import (
+    EmptyDict,
+)
+
+
+class DictStack(dict):
+
+    def __init__(self, backing = EmptyDict()):
+        super(DictStack, self).__init__()
+        self.backing = backing
+
+    def __missing__(self, key):
+        return self.backing[key]
