355c12a637d3aaf56f23dfffdbaaee4beeb48e3e
Efimov Vasily <real@ispras.ru>
2024-07-29 18:32:13 +0300

    ForwardDeclarator: `on_visit` -> `__visit__`
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- source/model.py
+++ source/model.py
@@ -947,8 +947,7 @@ class ForwardDeclarator(TypeReferencesVisitor):
     structure declaration with a forward declaration of the structure
     """
 
-    def on_visit(self):
-        t = self.cur
+    def __visit__(self, t):
         if isinstance(t, Structure) and t in self.previous:
             if t.declaration is not None:
                 decl = t.declaration
