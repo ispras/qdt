d3f575f119256ce1a07e33d0a6f464ea12aa61c3
Efimov Vasily <real@ispras.ru>
2024-07-29 17:34:59 +0300

    PyGenerator: do not use Python keywords as variable names
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- common/pygen.py
+++ common/pygen.py
@@ -31,6 +31,9 @@ from inspect import (
 from itertools import (
     count,
 )
+from keyword import (
+    iskeyword,
+)
 from six import (
     binary_type,
     integer_types,
@@ -159,7 +162,7 @@ require reference to the current object.
 
             name = var_base
 
-            if name in self.name2obj:
+            if name in self.name2obj or iskeyword(name):
                 for i in self.name_counter.setdefault(var_base, count(0)):
                     name = "%s%d" % (var_base, i)
                     if name not in self.name2obj:
