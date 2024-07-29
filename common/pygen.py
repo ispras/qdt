84a1efabfd096c54d5860f9a12ebfca0ade32105
Efimov Vasily <real@ispras.ru>
2024-07-29 18:24:59 +0300

    PyGenDepsVisitor: define `__field_name__` instead of `field_name`
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- common/pygen.py
+++ common/pygen.py
@@ -54,10 +54,7 @@ pythonizable = const_types + (list, set, dict, tuple) + imported_types
 
 class PyGenDepsVisitor(ObjectVisitor):
 
-    def __init__(self, root):
-        super(PyGenDepsVisitor, self).__init__(root,
-            field_name = "__pygen_deps__"
-        )
+    __field_name__ = "__pygen_deps__"
 
 
 class PyGenerator(CodeWriter):
