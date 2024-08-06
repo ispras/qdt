93587df3f98af56da08a95fc9ae88c6797bb3d55
Efimov Vasily <real@ispras.ru>
2024-08-06 17:58:25 +0300

    OpPreInc: define `prior`ity at `class` level
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- source/function/tree.py
+++ source/function/tree.py
@@ -876,6 +876,7 @@ class OpPreDec(UnaryOperator):
 class OpPreInc(UnaryOperator):
 
     prefix = "++"
+    prior = 1
 
 
 class OpSizeOf(UnaryOperator):
@@ -1088,7 +1089,6 @@ class OpTernCond(Operator):
 
 
 op_priority = {
-    OpPreInc:        1,
     OpDeref:         2,
     OpAddr:          2,
     OpNot:           2,
