0c24243966f07cb84fe7635ee650b362dd0b0487
Efimov Vasily <real@ispras.ru>
2024-08-06 17:35:51 +0300

    OpNEq.__init__: pass `**kw` thought instead of defining `parenthesis`
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- source/function/tree.py
+++ source/function/tree.py
@@ -988,8 +988,8 @@ class OpEq(BinaryOperator):
 
 class OpNEq(BinaryOperator):
 
-    def __init__(self, arg1, arg2, parenthesis = False):
-        super(OpNEq, self).__init__("!=", arg1, arg2, parenthesis)
+    def __init__(self, arg1, arg2, **kw):
+        super(OpNEq, self).__init__("!=", arg1, arg2, **kw)
 
 
 class OpGE(BinaryOperator):
