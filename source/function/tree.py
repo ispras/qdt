31e21c32e208ddd0d842df55671ad30b2f03a7ee
Efimov Vasily <real@ispras.ru>
2024-08-06 17:55:45 +0300

    CaseRange: define `prior`ity at `class` level
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- source/function/tree.py
+++ source/function/tree.py
@@ -997,6 +997,7 @@ class OpLess(BinaryOperator):
 class CaseRange(BinaryOperator):
 
     op_str = "..."
+    prior = 1
 
 
 class OpTernCond(Operator):
@@ -1023,7 +1024,6 @@ class OpTernCond(Operator):
 
 
 op_priority = {
-    CaseRange:       1,
     OpIndex:         1,
     OpSDeref:        1,
     OpDec:           1,
