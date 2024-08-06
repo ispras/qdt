c7e562e260de3a9e6293f2a9bf08eee56cb106ca
Efimov Vasily <real@ispras.ru>
2024-08-06 17:35:58 +0300

    OpGE.__init__: pass `**kw` thought instead of defining `parenthesis`
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- source/function/tree.py
+++ source/function/tree.py
@@ -993,8 +993,8 @@ class OpNEq(BinaryOperator):
 
 class OpGE(BinaryOperator):
 
-    def __init__(self, arg1, arg2, parenthesis = False):
-        super(OpGE, self).__init__(">=", arg1, arg2, parenthesis)
+    def __init__(self, arg1, arg2, **kw):
+        super(OpGE, self).__init__(">=", arg1, arg2, **kw)
 
 
 class OpLE(BinaryOperator):
