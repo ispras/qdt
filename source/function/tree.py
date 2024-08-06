f8ed69fd77eec352f922feb6475bb09b56496509
Efimov Vasily <real@ispras.ru>
2024-08-06 17:36:21 +0300

    CaseRange.__init__: pass `**kw` thought instead of defining `parenthesis`
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- source/function/tree.py
+++ source/function/tree.py
@@ -1012,8 +1012,8 @@ class OpGreater(BinaryOperator):
 
 class OpLess(BinaryOperator):
 
-    def __init__(self, arg1, arg2, parenthesis = False):
-        super(OpLess, self).__init__("<", arg1, arg2, parenthesis)
+    def __init__(self, arg1, arg2, **kw):
+        super(OpLess, self).__init__("<", arg1, arg2, **kw)
 
 
 class CaseRange(BinaryOperator):
