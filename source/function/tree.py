773b5c7ab84ff13fed00da5f74e051f2bd95c01b
Efimov Vasily <real@ispras.ru>
2024-08-06 17:56:28 +0300

    OpIndex: define `prior`ity at `class` level
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- source/function/tree.py
+++ source/function/tree.py
@@ -735,6 +735,7 @@ class OpIndex(Operator):
 
     delim = "["
     suffix = "]"
+    prior = 1
 
     def add_child(self, child):
         # Note, ignore `Operator.add_child` to suppress unnecessary parentheses
@@ -1024,7 +1025,6 @@ class OpTernCond(Operator):
 
 
 op_priority = {
-    OpIndex:         1,
     OpSDeref:        1,
     OpDec:           1,
     OpInc:           1,
