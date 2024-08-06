702a20cc556120f913d3e49d9ea861a28235b58e
Efimov Vasily <real@ispras.ru>
2024-08-06 17:59:36 +0300

    OpAddr: define `prior`ity at `class` level
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- source/function/tree.py
+++ source/function/tree.py
@@ -888,6 +888,7 @@ class OpSizeOf(UnaryOperator):
 class OpAddr(UnaryOperator):
 
     prefix = "&"
+    prior = 2
 
 
 class OpDeref(UnaryOperator):
@@ -1090,7 +1091,6 @@ class OpTernCond(Operator):
 
 
 op_priority = {
-    OpAddr:          2,
     OpNot:           2,
     OpLogNot:        2,
     OpCast:          2,
