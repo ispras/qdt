51387db1beee0bf70d5d74e8ee85f66b7ccc6f05
Efimov Vasily <real@ispras.ru>
2024-09-30 22:31:15 +0300

    source.langs: `CPunctuation`
    Signed-off-by: Efimov Vasily <real@ispras.ru>

---
+++ source/langs/c_punct.py
@@ -0,0 +1,24 @@
+__all__ = [
+    "CPunctuation"
+]
+
+
+class CPunctuation:
+
+    t_DOT = r"\."
+    t_DOTS = r"\.\.\."
+
+    t_LPAREN = r"\("
+    t_RPAREN = r"\)"
+
+    t_LBRACE = r"\{"
+    t_RBRACE = r"\}"
+
+    t_LBRACKET = r"\["
+    t_RBRACKET = r"\]"
+
+    t_COMMA = ","
+
+    t_COLON = ":"
+
+    t_SEMI = ";"
