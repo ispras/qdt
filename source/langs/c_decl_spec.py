011bfe33373332634f7e06704c629e3f29e5454b
Efimov Vasily <real@ispras.ru>
2024-09-30 22:21:31 +0300

    source.langs: `CDeclSpec`, `CDeclSpecEx`: C declaration specifiers sub-grammars
    Signed-off-by: Efimov Vasily <real@ispras.ru>

---
+++ source/langs/c_decl_spec.py
@@ -0,0 +1,266 @@
+from .c_words import (
+    CWords,
+)
+
+
+class CDeclSpec(CWords):
+    """ This sub-grammar defines self-sufficient productions only.
+See: CDeclSpecEx.
+    """
+
+    @staticmethod
+    def p_declaration_specifiers(declaration_specifier):
+        return [declaration_specifier]
+
+    @staticmethod
+    def p_declaration_specifiers__n(
+        declaration_specifiers, declaration_specifier
+    ):
+        return declaration_specifiers + [declaration_specifier]
+
+    # --
+
+    @staticmethod
+    def p_declaration_specifier__storage_class(STORAGE_CLASS_SPECIFIER):
+        return STORAGE_CLASS_SPECIFIER
+
+    @staticmethod
+    def p_declaration_specifier__type(type_specifier):
+        return type_specifier
+
+    @staticmethod
+    def p_declaration_specifier__type_qualifier(type_qualifier):
+        return type_qualifier
+
+    @staticmethod
+    def p_declaration_specifier__function(FUNCTION_SPECIFIER):
+        return FUNCTION_SPECIFIER
+
+    # --
+
+    @staticmethod
+    def p_type_qualifier(TYPE_QUALIFIER):
+        return TYPE_QUALIFIER
+
+    @staticmethod
+    def p_type_qualifier__atomic(ATOMIC):
+        return ATOMIC
+
+    # --
+
+    @staticmethod
+    def p_type_specifier__base(BASE_TYPE_SPECIFIER):
+        return dict(
+            name = BASE_TYPE_SPECIFIER,
+            base = True,
+        )
+
+    @staticmethod
+    def p_type_specifier__typedef_name(IDENTIFIER):
+        # Note that, `typedef_name: identifier` only
+        return dict(
+            name = IDENTIFIER,
+            base = False,
+        )
+
+    @staticmethod
+    def p_type_specifier__enum(enum_specifier):
+        return enum_specifier
+
+    @staticmethod
+    def p_type_specifier__struct_or_union(struct_or_union_specifier):
+        raise NotImplementedError("struct/union ID_opt {...}_opt")
+
+    # --
+
+    @staticmethod
+    def p_struct_or_union__struct(STRUCT):
+        raise NotImplementedError(STRUCT)
+
+    @staticmethod
+    def p_struct_or_union__union(UNION):
+        raise NotImplementedError(UNION)
+
+    @staticmethod
+    def p_struct_or_union_specifier__ref(struct_or_union, IDENTIFIER):
+        raise NotImplementedError("struct/union " + IDENTIFIER)
+
+    # --
+
+    @staticmethod
+    def p_enum_specifier__ref(ENUM, IDENTIFIER):
+        raise NotImplementedError("enum " + IDENTIFIER)
+
+    @staticmethod
+    def p_enum_specifier__anon(ENUM, enumerator_list_block):
+        raise NotImplementedError("enum { ... }")
+
+    @staticmethod
+    def p_enum_specifier__full(ENUM, IDENTIFIER, enumerator_list_block):
+        raise NotImplementedError(
+            "struct/union " + IDENTIFIER + " { ... }"
+        )
+
+    # --
+
+    @staticmethod
+    def p_enumerator_list_block(LBRACE, enumerator_list, RBRACE):
+        return enumerator_list
+
+    @staticmethod
+    def p_enumerator_list_block__comma(LBRACE, enumerator_list, COMMA, RBRACE):
+        return enumerator_list
+
+    # --
+
+    @staticmethod
+    def p_enumerator_list(enumerator):
+        return [enumerator]
+
+    @staticmethod
+    def p_enumerator_list__n(enumerator_list, COMMA, enumerator):
+        return enumerator_list + [enumerator]
+
+    # --
+
+    @staticmethod
+    def p_enumerator__auto(IDENTIFIER):
+        # Note:
+        #    enumerator : enumeration_constant
+        #    but
+        #    enumeration_constant: identifier
+        #    only
+        raise NotImplementedError(IDENTIFIER + " as `enum` item")
+
+
+class CDeclSpecEx(CDeclSpec):
+    """ This grammar extends CDeclSpec with productions which require external
+productions/tokens to be defined...
+    - SEMI (;)
+    - constant_expression
+    - declorator
+    - static_assert_declaration
+    - type_name
+    - ??? (it's not finished yet)
+    """
+
+    @staticmethod
+    def p_declaration_specifier__alignment(alignment_specifier):
+        return alignment_specifier
+
+    # --
+
+    @staticmethod
+    def p_alignment_specifier__by_type(ALIGN_AS, LPAREN, type_name, RPAREN):
+        raise NotImplementedError(ALIGN_AS + "(%s)" % type_name)
+
+    @staticmethod
+    def p_alignment_specifier__by_const(
+        ALIGN_AS, LPAREN, constant_expression, RPAREN
+    ):
+        raise NotImplementedError(ALIGN_AS + "(constant_expression)")
+
+    # --
+
+    @staticmethod
+    def p_enumerator__manual(IDENTIFIER, ASSIGN, constant_expression):
+        # Note:
+        #    enumerator : enumeration_constant ASSIGN constant_expression
+        #    but
+        #    enumeration_constant: identifier
+        #    only
+        raise NotImplementedError(IDENTIFIER + " = [value] as `enum` item")
+
+    # --
+
+    @staticmethod
+    def p_struct_or_union_specifier__anon(
+            struct_or_union, struct_declaration_block
+    ):
+        raise NotImplementedError("struct/union { ... }")
+
+    @staticmethod
+    def p_struct_or_union_specifier__full(
+            struct_or_union, IDENTIFIER, struct_declaration_block
+    ):
+        raise NotImplementedError(
+            "struct/union " + IDENTIFIER + " { ... }"
+        )
+
+    # --
+
+    @staticmethod
+    def p_struct_declaration_block(
+        LPAREN, struct_declaration_list, RPAREN
+    ):
+        return struct_declaration_list
+
+    # ..
+
+    @staticmethod
+    def p_struct_declaration_list(struct_declaration):
+        return [struct_declaration]
+
+    @staticmethod
+    def p_struct_declaration_list__n(
+        struct_declaration_list, struct_declaration,
+    ):
+        return struct_declaration_list + [struct_declaration]
+
+    # --
+
+    # TODO: struct_declaration: static_assert_declaration
+    # TODO: struct_declaration:
+    #       specifier_qualifier_list struct_declarator_list SEMI
+    # TODO: struct_declaration:
+    #       specifier_qualifier_list SEMI
+
+    # --
+
+    @staticmethod
+    def p_struct_declarator_list(struct_declarator):
+        return [struct_declarator]
+
+    @staticmethod
+    def p_struct_declarator_list__n(
+        struct_declarator_list, COMMA, struct_declarator
+    ):
+        return struct_declarator_list + [struct_declarator]
+
+    # --
+
+    # TODO: struct_declarator: declarator
+    # TODO: struct_declarator: COLON constant_expression
+    # TODO: struct_declarator: declorator COLON constant_expression
+
+    # --
+
+    @staticmethod
+    def p_type_specifier__atomic(ATOMIC, LPAREN, type_name, RPAREN):
+        raise NotImplementedError(ATOMIC + "(%s)" % type_name)
+
+    # --
+
+    @staticmethod
+    def p_specifier_qualifier_list(specifier_qualifier_item):
+        return [specifier_qualifier_item]
+
+    @staticmethod
+    def p_specifier_qualifier_list__n(
+        specifier_qualifier_item, specifier_qualifier_list
+    ):
+        return [specifier_qualifier_item] + specifier_qualifier_list
+
+    # --
+
+    @staticmethod
+    def p_specifier_qualifier_item__type(type_specifier):
+        return type_specifier
+
+    @staticmethod
+    def p_specifier_qualifier_item__type_qualifier(type_qualifier):
+        return type_qualifier
+
+    @staticmethod
+    def p_specifier_qualifier_item__alignment(alignment_specifier):
+        return alignment_specifier
