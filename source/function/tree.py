f1cf5773787471634df17f96b5e28193f4a50585
Efimov Vasily <real@ispras.ru>
2024-10-09 18:23:13 +0300

    source.function.tree.Node.__gen_code__: don't print `children` as kwarg
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- source/function/tree.py
+++ source/function/tree.py
@@ -165,7 +165,7 @@ class Node(TypeContainer):
         self.add_children(children)
 
     def __gen_code__(self, gen):
-        gen.gen_code(self)
+        gen.gen_code(self, skip_kw = "children")
         if self.children:
             gen.line(gen.nameof(self) + "(")
             gen.push_indent()
