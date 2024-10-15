93b7d79f7a6d678b046f261684a8129c7bc30618
Efimov Vasily <real@ispras.ru>
2024-10-15 16:15:05 +0300

    source.function.tree.Nonde.__gen_code__: `skip_kw` must be a container
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- source/function/tree.py
+++ source/function/tree.py
@@ -162,7 +162,7 @@ class Node(TypeContainer):
             self.add_child(child)
 
     def __gen_code__(self, gen):
-        gen.gen_code(self, skip_kw = "children")
+        gen.gen_code(self, skip_kw = ("children",))
         if self.children:
             gen.line(gen.nameof(self) + "(")
             gen.push_indent()
