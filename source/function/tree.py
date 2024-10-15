54d3037b8ea27728cd156bdb70fda266ad29a7c7
Efimov Vasily <real@ispras.ru>
2024-10-15 16:55:29 +0300

    source.function.tree.Node.__call__: redirect to `add_children`
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- source/function/tree.py
+++ source/function/tree.py
@@ -173,8 +173,7 @@ class Node(TypeContainer):
             gen.line(")")
 
     def __call__(self, *children):
-        for c in children:
-            self.add_child(c)
+        self.add_children(children)
         return self
 
     def add_child(self, child):
