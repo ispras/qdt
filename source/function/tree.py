0430799e70ebaf36d2f4d45a53d2d75037919e9e
Efimov Vasily <real@ispras.ru>
2024-10-15 16:28:54 +0300

    source.function.tree.CId: update `val`ue handling
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- source/function/tree.py
+++ source/function/tree.py
@@ -251,6 +251,10 @@ class CId(CNode):
     "C Identifier"
     new_line = None
 
+    def __init__(self, val, **kw):
+        super(CId, self).__init__(**kw)
+        self.val = val
+
 
 class Comment(Node):
 
