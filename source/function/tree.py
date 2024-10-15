17fbc58f56e0e3fb6dae77766a81a83f5f38db58
Efimov Vasily <real@ispras.ru>
2024-10-15 15:59:50 +0300

    source.function.tree.Node: pass `**kw` through
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- source/function/tree.py
+++ source/function/tree.py
@@ -153,9 +153,10 @@ class Node(TypeContainer):
 
     def __init__(self,
         val = None,
-        children = []
+        children = [],
+        **kw
     ):
-        super(Node, self).__init__()
+        super(Node, self).__init__(**kw)
 
         if val is not None:
             self.val = val
