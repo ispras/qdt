1d6ef0476d8f3a2819678c3ba46484e985249c21
Danila Bogomolov <bda@ispras.ru>
2024-11-02 05:19:12 +0300

    define_python_operators: add missing arguments

--- source/function/tree.py
+++ source/function/tree.py
@@ -1189,7 +1189,7 @@ construction simpler. Can be a class @decorator.
     """
     for attr, value in PYTHON_OPERATORS.items():
         if hasattr(cls, attr):
-            print("%s.%s: is already defined")
+            print("%s.%s: is already defined" % (cls, attr))
             return
         setattr(cls, attr, value)
     return cls
