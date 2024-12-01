0bc97e270ec395c8e77decdd35ebbfa10cc88bcc
Efimov Vasily <real@ispras.ru>
2024-12-01 18:51:14 +0300

    misc: textview: now an executable
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- misc/textview.py
+++ misc/textview.py
@@ -1,6 +1,17 @@
+#!/usr/bin/env python3
 from argparse import (
     ArgumentParser,
 )
+from os.path import (
+	abspath,
+	dirname,
+)
+from sys import (
+	path as PYTHONPATH,
+)
+
+PYTHONPATH.insert(0, dirname(dirname(abspath(__file__))))
+
 from widgets import (
     QDTUserSettings,
     TextViewerTk,
