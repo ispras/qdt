d19a2ba5fdd193d0d7ed4f530474edb92f6c0556
Efimov Vasily <real@ispras.ru>
2025-02-24 23:11:04 +0300

    cpu: "default" encoding is not `None`
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- qemu/cpu/code_generation.py
+++ qemu/cpu/code_generation.py
@@ -468,7 +468,7 @@ def fill_decode_opc_body(cputype, function, cpu_env):
     ]
 
     ParseTreeCodeBuilder(
-        cputype.encodings[None].tree,
+        cputype.encodings["default"].tree,
         cputype.target_bigendian,
         cputype.read_bitsize,
         body,
@@ -1197,7 +1197,7 @@ def fill_print_insn_body(cputype, function):
     ]
 
     ParseTreeCodeBuilder(
-        cputype.encodings[None].tree,
+        cputype.encodings["default"].tree,
         cputype.target_bigendian,
         cputype.read_bitsize,
         body,
