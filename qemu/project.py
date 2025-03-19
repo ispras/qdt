983ebb0cc949a09b652e7aa866f162ef844b9e23
Efimov Vasily <real@ispras.ru>
2025-03-19 21:58:48 +0300

    qemu/project.py: unused import
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- qemu/project.py
+++ qemu/project.py
@@ -15,9 +15,6 @@ from common import (
 from .cpu import (
     CPUDescription,
 )
-from libe.common.add_line import (
-    add_line_to_file,
-)
 from libe.common.shadow_open import (
     shadow_open,
 )
