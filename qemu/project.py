9de1582437e2171c4113e6a3e38b629ebd1261f5
Efimov Vasily <real@ispras.ru>
2025-03-24 20:29:49 +0300

    QProject.co_gen `__` for ignored **kwargs
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- qemu/project.py
+++ qemu/project.py
@@ -173,7 +173,7 @@ class QProject(object):
         known_targets = None,
         with_debug_comments = False,
         include_paths = tuple(),
-        **_
+        **__
     ):
         qom_t = desc.gen_type()
 
