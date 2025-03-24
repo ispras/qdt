555df11b2b0ff2e1e09ef44f6d72a65e0af4862b
Efimov Vasily <real@ispras.ru>
2025-03-24 20:17:34 +0300

    get_default_args: use `__` for unused vars
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- common/reflection.py
+++ common/reflection.py
@@ -22,7 +22,7 @@ def get_default_args(func):
     """
     returns a dictionary of arg_name:default_values for the input function
     """
-    args, varargs, keywords, defaults = getargspec(func)
+    args, __, __, defaults = getargspec(func)
     if defaults is not None:
         return dict(zip(args[-len(defaults):], defaults))
     else:
