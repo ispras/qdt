0cd887906e21b1f5bf90bf7d65e959d379cd5677
Efimov Vasily <real@ispras.ru>
2025-06-06 19:43:38 +0300

    qemu..Short: update `s_`hort productions definitions
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- qemu/cpu/short.py
+++ qemu/cpu/short.py
@@ -29,23 +29,23 @@ class Short(object):
         ))
 
     @staticmethod
-    def p_short__0(instruction):
+    def s_short__0(instruction):
         return instruction
 
     @staticmethod
-    def p_short__1(fields):
+    def s_short__1(fields):
         return fields
 
     @staticmethod
-    def p_bit_place__1(SPACE):
+    def s_bit_place__1(SPACE):
         return 1
 
     @staticmethod
-    def p_bit_place__n(bit_place, SPACE):
+    def s_bit_place__n(bit_place, SPACE):
         return bit_place + 1
 
     @staticmethod
-    def p_bit_place_ignored__0(bit_place):
+    def s_bit_place_ignored__0(bit_place):
         # Bit places are ignored if operand length is given explicitly.
         # They are for visual alignment only.
         # Bit places are always ignored for opcodes.
@@ -53,43 +53,43 @@ class Short(object):
         pass
 
     @staticmethod
-    def p_bit_place_ignored__1():
+    def s_bit_place_ignored__1():
         pass
 
     @staticmethod
-    def p_opcode(UINT):
+    def s_opcode(UINT):
         # Bit places are ignored for opcode.
         # They are for visual alignment only.
         # Opcode length is equal to length of token.
         return Opcode(len(UINT), val = int(UINT, base = 2))
 
     @staticmethod
-    def p_operand_na(ID):
+    def s_operand_na(ID):
         # not aligned
         return Operand(len(ID), ID)
 
     @staticmethod
-    def p_operand_la(ID, bit_place):
+    def s_operand_la(ID, bit_place):
         # left aligned
         return Operand(len(ID) + bit_place, ID)
 
     @staticmethod
-    def p_operand_ra(bit_place, ID):
+    def s_operand_ra(bit_place, ID):
         # right aligned
         return Operand(len(ID) + bit_place, ID)
 
     @staticmethod
-    def p_operand_ma(bit_place__l, ID, bit_place__r):
+    def s_operand_ma(bit_place__l, ID, bit_place__r):
         # middle aligned
         return Operand(len(ID) + bit_place__l + bit_place__r, ID)
 
     @staticmethod
-    def p_operand_el(ID, COLON, UINT):
+    def s_operand_el(ID, COLON, UINT):
         # explicit lenght
         return Operand(int(UINT), ID)
 
     @staticmethod
-    def p_operand_el__bit(ID, LBRACKET, UINT, RBRACKET):
+    def s_operand_el__bit(ID, LBRACKET, UINT, RBRACKET):
         # When an operand is fragmented in spread parts of the instruction word
         # this production represent one bit of the operand.
         # Brackets then encloses position of the bit inside the operand.
@@ -99,7 +99,7 @@ class Short(object):
         return Operand(1, ID, num = int(UINT))
 
     @staticmethod
-    def p_operand_el__part(ID, LBRACKET, UINT__0, COLON, UINT__1, RBRACKET):
+    def s_operand_el__part(ID, LBRACKET, UINT__0, COLON, UINT__1, RBRACKET):
         # When an operand is fragmented in spread parts of the instruction word
         # this production represent one continuous part of the operand.
         # Brackets then encloses position of this part inside the operand.
@@ -111,56 +111,56 @@ class Short(object):
         return Operand(UINT_MAX - UINT_MIN + 1, ID, num = int(UINT_MIN))
 
     @staticmethod
-    def p_field__1(first_field):
+    def s_field__1(first_field):
         return first_field
 
     @staticmethod
-    def p_field__ra(operand_ra):
+    def s_field__ra(operand_ra):
         return operand_ra
 
     @staticmethod
-    def p_field__ma(operand_ma):
+    def s_field__ma(operand_ma):
         return operand_ma
 
     @staticmethod
-    def p_field__el(bit_place, operand_el, bit_place_ignored):
+    def s_field__el(bit_place, operand_el, bit_place_ignored):
         return operand_el
 
     @staticmethod
-    def p_field__o(bit_place, opcode, bit_place_ignored):
+    def s_field__o(bit_place, opcode, bit_place_ignored):
         return opcode
 
     @staticmethod
-    def p_first_field__0(operand_el, bit_place_ignored):
+    def s_first_field__0(operand_el, bit_place_ignored):
         return operand_el
 
     @staticmethod
-    def p_first_field__1(operand_na):
+    def s_first_field__1(operand_na):
         return operand_na
 
     @staticmethod
-    def p_first_field__2(operand_la):
+    def s_first_field__2(operand_la):
         return operand_la
 
     @staticmethod
-    def p_first_field__3(opcode, bit_place_ignored):
+    def s_first_field__3(opcode, bit_place_ignored):
         return opcode
 
     @staticmethod
-    def p_fields_list__start(first_field):
+    def s_fields_list__start(first_field):
         return [first_field]
 
     @staticmethod
-    def p_fields_list(fields_list, CONCAT, field):
+    def s_fields_list(fields_list, CONCAT, field):
         return fields_list + [field]
 
     @staticmethod
-    def p_fields(fields_list):
+    def s_fields(fields_list):
         operand_num_from_bitoffset(fields_list)
         return fields_list
 
     @staticmethod
-    def p_instruction(ID, bit_place, fields):
+    def s_instruction(ID, bit_place, fields):
         return Instruction(ID, *fields)
 
     @staticmethod
