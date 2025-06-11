955653c3847d5e904177e3c37e24cb50ed49c7af
Efimov Vasily <real@ispras.ru>
2025-06-11 20:16:48 +0300

    misc/langs_exp: examples of `struct`
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- misc/langs_exp.py
+++ misc/langs_exp.py
@@ -1,6 +1,8 @@
 from source import (
+    CDeclaration,
     CEnum,
     CExpr,
+    CStructOrUnion,
     short_ply_grammar,
     Type,
 )
@@ -24,6 +26,81 @@ class Common:
     @staticmethod
     def p_error(p):
         raise SyntaxError(repr(p))
+
+
+@short_ply_grammar(
+    debugfile = True,
+    start = "type_specifier",
+)
+class CStructOrUnion0(CStructOrUnion, CDeclaration, CExpr, Common):
+    pass
+
+
+# for usage in structures below
+Type("long", base = True, incomplete = False)
+
+res = CStructOrUnion0.parse("""
+struct STestBitfield { long X:30; }
+""",
+    debug = True,
+)
+print(repr(res))
+
+res = CStructOrUnion0.parse("""
+struct STestBitfields { long X:30, Y:34; }
+""",
+    debug = True,
+)
+print(repr(res))
+
+res = CStructOrUnion0.parse("""
+struct STestBitfieldsManyTypes { long X:30, Y:34; long z:10; long Z:54; }
+""",
+    debug = True,
+)
+print(repr(res))
+
+res = CStructOrUnion0.parse("""
+struct STestSingle { long X; }
+""",
+    debug = True,
+)
+print(repr(res))
+
+res = CStructOrUnion0.parse("""
+struct STestCommonType { long X, Y, Z; }
+""",
+    debug = True,
+)
+print(repr(res))
+
+res = CStructOrUnion0.parse("""
+struct STestPerFieldType { long X; long Y; long Z; }
+""",
+    debug = True,
+)
+print(repr(res))
+
+res = CStructOrUnion0.parse("""
+struct STestPerFieldTypespecs { void * X; void *Y; void* Z; }
+""",
+    debug = True,
+)
+print(repr(res))
+
+res = CStructOrUnion0.parse("""
+struct STestCommonType
+""",
+    debug = True,
+)
+print(repr(res))
+
+res = CStructOrUnion0.parse("""
+struct STestTypespecsOfCommonType { void *X, *Y, *Z; }
+""",
+    debug = True,
+)
+print(repr(res))
 
 
 @short_ply_grammar(
