11882ae13e0f2133b447fa1b1f9d223debc2f055
Efimov Vasily <real@ispras.ru>
2025-06-11 22:26:02 +0300

    source.langs.c_type: outline `CEnum`
    Signed-off-by: Efimov Vasily <real@ispras.ru>

---
+++ source/langs/c_enum.py
@@ -0,0 +1,70 @@
+__all__ = [
+    "CEnum"
+]
+
+from .c_punct import (
+    CPunctuation,
+)
+from .c_words import (
+    CWords,
+)
+from ..model import (
+    Enumeration,
+    Type,
+)
+
+
+class CEnum(CPunctuation, CWords):
+
+    # Sourced productions
+
+    @staticmethod
+    def s_enum_specifier__ref(ENUM, IDENTIFIER):
+        res = Type[IDENTIFIER]
+        assert isinstance(res, Enumeration)
+        return res
+
+    @staticmethod
+    def s_enum_specifier__anon(ENUM, enumerator_list_block):
+        return Enumeration(enumerator_list_block)
+
+    @staticmethod
+    def s_enum_specifier__full(ENUM, IDENTIFIER, enumerator_list_block):
+        return Enumeration(enumerator_list_block, enum_name = IDENTIFIER)
+
+    # --
+
+    @staticmethod
+    def s_enumerator_list_block(LBRACE, enumerator_list, RBRACE):
+        return enumerator_list
+
+    @staticmethod
+    def s_enumerator_list_block__comma(LBRACE, enumerator_list, COMMA, RBRACE):
+        return enumerator_list
+
+    # --
+
+    @staticmethod
+    def s_enumerator_list(enumerator):
+        return [enumerator]
+
+    @staticmethod
+    def s_enumerator_list__n(enumerator_list, COMMA, enumerator):
+        return enumerator_list + [enumerator]
+
+    # --
+
+    @staticmethod
+    def s_enumerator__auto(IDENTIFIER):
+        # Note:
+        #    enumerator : enumeration_constant
+        #    but
+        #    enumeration_constant: identifier
+        #    only
+        return IDENTIFIER
+
+    # Extensions to other productions
+
+    @staticmethod
+    def s_type_specifier__enum(enum_specifier):
+        return enum_specifier
--- source/langs/c_type.py
+++ source/langs/c_type.py
@@ -1,6 +1,5 @@
 __all__ = [
     "CStructOrUnion"
-  , "CEnum"
   , "CTypeSpecifier"
   , "CTypeQualifier"
   , "CSpecQualList"
@@ -16,7 +15,6 @@ from .c_words import (
     CWords,
 )
 from ..model import (
-    Enumeration,
     Structure,
     Type,
 )
@@ -96,62 +94,6 @@ class CAtomic(CWords):
     @staticmethod
     def s_type_specifier__atomic(ATOMIC, LPAREN, type_name, RPAREN):
         raise NotImplementedError(ATOMIC + "(%s)" % type_name)
-
-
-class CEnum(CPunctuation, CWords):
-
-    # Sourced productions
-
-    @staticmethod
-    def s_enum_specifier__ref(ENUM, IDENTIFIER):
-        res = Type[IDENTIFIER]
-        assert isinstance(res, Enumeration)
-        return res
-
-    @staticmethod
-    def s_enum_specifier__anon(ENUM, enumerator_list_block):
-        return Enumeration(enumerator_list_block)
-
-    @staticmethod
-    def s_enum_specifier__full(ENUM, IDENTIFIER, enumerator_list_block):
-        return Enumeration(enumerator_list_block, enum_name = IDENTIFIER)
-
-    # --
-
-    @staticmethod
-    def s_enumerator_list_block(LBRACE, enumerator_list, RBRACE):
-        return enumerator_list
-
-    @staticmethod
-    def s_enumerator_list_block__comma(LBRACE, enumerator_list, COMMA, RBRACE):
-        return enumerator_list
-
-    # --
-
-    @staticmethod
-    def s_enumerator_list(enumerator):
-        return [enumerator]
-
-    @staticmethod
-    def s_enumerator_list__n(enumerator_list, COMMA, enumerator):
-        return enumerator_list + [enumerator]
-
-    # --
-
-    @staticmethod
-    def s_enumerator__auto(IDENTIFIER):
-        # Note:
-        #    enumerator : enumeration_constant
-        #    but
-        #    enumeration_constant: identifier
-        #    only
-        return IDENTIFIER
-
-    # Extensions to other productions
-
-    @staticmethod
-    def s_type_specifier__enum(enum_specifier):
-        return enum_specifier
 
 
 class CStructOrUnion(CPunctuation, CWords):
