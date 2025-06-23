e068ce602a5b93156a54a62381db4c2aef38d237
Efimov Vasily <real@ispras.ru>
2025-06-23 16:00:26 +0300

    widgets.logo: use `Pictures`
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- widgets/logo.py
+++ widgets/logo.py
@@ -5,39 +5,28 @@ __all__ = [
 from common import (
     mlget as _,
 )
+from .pictures import (
+    Pictures,
+)
 
-from os.path import (
-    join,
-    split,
-)
-from six.moves.tkinter import (
-    PhotoImage,
-)
 from traceback import (
     print_exc,
 )
 
 
 reported = False
+logo = Pictures(
+    logo = (
+        "logo.png",
+        # Some versions of Tkinter do not support PNG. Use GIF instead.
+        "logo.gif",
+    )
+)
 
 def set_logo(self):
-
-    script_dir, __ = split(__file__)
-    if not script_dir:
-        script_dir = "."
-
-    png_logo_file = join(script_dir, "logo.png")
-
     try:
         # see: https://stackoverflow.com/questions/18537918/set-window-icon
-        try:
-            icon = PhotoImage(file = png_logo_file)
-        except:
-            # Some versions of Tkinter do not support PNG. Use GIF instead.
-            gif_logo_file = join(script_dir, "logo.gif")
-            icon = PhotoImage(file = gif_logo_file)
-
-        self.tk.call("wm", "iconphoto", self._w, icon)
+        self.tk.call("wm", "iconphoto", self._w, logo.logo)
     except:
         global reported
 
