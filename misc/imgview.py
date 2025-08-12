941ec16c1face6f8895676f988dbbbde91935247
Efimov Vasily <real@ispras.ru>
2025-08-12 18:06:07 +0300

    imgview: performance measuring
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- misc/imgview.py
+++ misc/imgview.py
@@ -58,6 +58,11 @@ from time import (
     time,
 )
 
+# temp debug stuff
+import linecache
+import tracemalloc
+
+
 FS_EMPTY = frozenset()
 
 def common_supers(*class_iterables):
@@ -1089,6 +1094,8 @@ def main():
 
     args = ap.parse_args()
 
+    tracemalloc.start()
+
     vroot = VirtualDirectory()
 
     paths = args.path
@@ -1138,6 +1145,35 @@ def main():
     drop_fs_cache()
 
     root.mainloop()
+
+    sn = tracemalloc.take_snapshot()
+    display_top(sn)
+
+
+def display_top(snapshot, key_type  = "lineno", limit = 10):
+    snapshot = snapshot.filter_traces((
+        tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
+        tracemalloc.Filter(False, "<unknown>"),
+    ))
+    top_stats = snapshot.statistics(key_type)
+
+    print("Top %s lines" % limit)
+    for index, stat in enumerate(top_stats[:limit], 1):
+        frame = stat.traceback[0]
+        # replace "/path/to/module/file.py" with "module/file.py"
+        filename = join(*split(frame.filename)[-2:])
+        print("#%s: %s:%s: %.1f KiB"
+              % (index, filename, frame.lineno, stat.size / 1024))
+        line = linecache.getline(frame.filename, frame.lineno).strip()
+        if line:
+            print('    %s' % line)
+
+    other = top_stats[limit:]
+    if other:
+        size = sum(stat.size for stat in other)
+        print("%s other: %.1f KiB" % (len(other), size / 1024))
+    total = sum(stat.size for stat in top_stats)
+    print("Total allocated size: %.1f KiB" % (total / 1024))
 
 
 if __name__ == "__main__":
