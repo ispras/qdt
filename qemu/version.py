63b5b335f22cb56c1dab8dbb7153757dba8572ec
Efimov Vasily <real@ispras.ru>
2025-08-13 16:42:32 +0300

    qemu.version: more softfloat functions
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- qemu/version.py
+++ qemu/version.py
@@ -1096,6 +1096,14 @@ def define_only_qemu_2_6_0_types():
             )
         ),
         Function(
+            name = "float32_round_to_int",
+            ret_type = m_float32,
+            args = (
+                m_float32("a"),
+                m_p_float_status("status")
+            )
+        ),
+        Function(
             name = "float32_to_float64",
             ret_type = m_float64,
             args = (
@@ -1112,6 +1120,14 @@ def define_only_qemu_2_6_0_types():
             )
         ),
         Function(
+            name = "float64_round_to_int",
+            ret_type = m_float64,
+            args = (
+                m_float64("a"),
+                m_p_float_status("status")
+            )
+        ),
+        Function(
             name = "float64_to_float32",
             ret_type = m_float32,
             args = (
@@ -1122,6 +1138,14 @@ def define_only_qemu_2_6_0_types():
         Function(
             name = "float64_to_int32",
             ret_type = m_int32_t,
+            args = (
+                m_float64("a"),
+                m_p_float_status("status")
+            )
+        ),
+        Function(
+            name = "float64_to_int64",
+            ret_type = m_int64_t,
             args = (
                 m_float64("a"),
                 m_p_float_status("status")
