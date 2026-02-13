152543766e7aeb4a815578aaec110f594948d655
Efimov Vasily <real@ispras.ru>
2026-02-13 19:24:19 +0300

    c2t/tests/_readme_u32_.c: `FLUSH_PIPELINE`
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- c2t/tests/_readme_u32_.c
+++ c2t/tests/_readme_u32_.c
@@ -18,15 +18,18 @@ int main(void) {
         c = b & a; //$ch
 
         // check just value of 'c' on the first iteration
+        FLUSH_PIPELINE;
         c = b | a; //$ch.c
 
         // check just value of 'c' on the all iterations
+        FLUSH_PIPELINE;
         c = b ^ a; //$chc.c
 
         /* combination of commands is possible
             on the first iteration: check line number, values of 'a' and 'b'
             on the all iterations: check value of 'c'
         */
+        FLUSH_PIPELINE;
         c = 0; //$br, chc.c, ch.a, ch.b
     }
 
