2a5d9bf06efe1c9ffd0876785cae4f77f6c8a21b
Efimov Vasily <real@ispras.ru>
2026-03-10 17:29:31 +0300

    AutoPanedWindow._on_configure: use orient invariant helpers
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- widgets/auto_paned.py
+++ widgets/auto_paned.py
@@ -50,31 +50,19 @@ class AutoPanedWindow(PanedWindow):
             # The widget has been to tight previously
             csize = int(size / clen)
             for i, coord in enumerate(range(csize, int(size - csize), csize)):
-                if horiz:
-                    new_coords.append((coord, 0))
-                else:
-                    new_coords.append((0, coord))
+                new_coords.append(coord)
 
             self._prev_size = size
             self.after_idle(self._update_sashes)
         else:
             scale = size / prev
             for i in range(clen - 1):
-                x, y = self.sash_coord(i)
-                if horiz:
-                    new_x = int(x * scale)
-                    if abs(new_x - x) < 2:
-                        # resizing is too small
-                        break
-                    new_y = y
-                else:
-                    new_x = x
-                    new_y = int(y * scale)
-                    if abs(new_y - y) < 2:
-                        # resizing is too small
-                        break
-
-                new_coords.append((new_x, new_y))
+                p = self.sash_pos(i)
+                new_p = int(p * scale)
+                if abs(new_p - p) < 2:
+                    # resizing is too small
+                    break
+                new_coords.append(new_p)
             else:
                 self._prev_size = size
                 self.after_idle(self._update_sashes)
@@ -101,5 +89,5 @@ class AutoPanedWindow(PanedWindow):
         """ Immediate sashes moving during `_on_configure` is buggy. Instead,
 move them after a while.
         """
-        for i, (x, y) in enumerate(self._new_coords):
-            self.sash_place(i, x, y)
+        for i, p in enumerate(self._new_coords):
+            self.sash_place(i, p)
