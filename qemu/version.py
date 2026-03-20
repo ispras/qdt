c9036feb190e0ca0dba64bed53b9a33ef038363f
Efimov Vasily <real@ispras.ru>
2026-03-20 17:49:17 +0300

    qemu..machine: add `default_ram_size` to the boilerplate
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- qemu/version.py
+++ qemu/version.py
@@ -6,6 +6,7 @@ from source import (
     add_base_types,
     BodyTree,
     Call,
+    CINT,
     Declare,
     Enumeration,
     Function,
@@ -599,7 +600,9 @@ def define_only_qemu_2_6_0_types():
             Pointer(Type["const char"])("desc"),
             Function(
                 args = [ Pointer(Type["MachineState"])("machine") ]
-            )("init")
+            )("init"),
+            # ram_addr_t, actually
+            Type["int"]("default_ram_size"),
         )
     ]).add_reference(osdep_fake_type)
 
@@ -1491,6 +1494,11 @@ def machine_register_2_5(mach):
             mach.instance_init
         )
     )
+    if mach.default_ram_size is not None:
+        class_init.body(OpAssign(
+            OpSDeref(mc, "default_ram_size"),
+            mach.default_ram_size
+        ))
     mach.class_init = class_init
     mach.source.add_type(class_init)
 
@@ -1571,6 +1579,11 @@ def machine_register_2_6(mach):
             mach.instance_init
         )
     )
+    if mach.default_ram_size is not None:
+        class_init.body(OpAssign(
+            OpSDeref(mc, "default_ram_size"),
+            mach.default_ram_size
+        ))
     mach.class_init = class_init
     mach.source.add_type(class_init)
 
