cf1a425211de0c51c29b41cbe89526dfed2e3a69
Efimov Vasily <real@ispras.ru>
2026-05-06 16:59:31 +0300

    c2t/tests: `float`/`double` <-> `u?int((32)|(64))_t` casts
    Signed-off-by: Efimov Vasily <real@ispras.ru>

---
+++ c2t/tests/c_cast_f64_7.c
@@ -0,0 +1,15 @@
+/* Cast instruction */
+
+#include "common.h"
+
+void main(void)
+{
+    volatile double a;
+    volatile int32_t b = -2345324;
+
+    a = b;
+    FLUSH_PIPELINE;
+    a = 0;     //$ch.a
+
+    return;    //$bre
+}
