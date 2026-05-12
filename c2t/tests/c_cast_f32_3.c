dd709544794f61933dee3790ad77974338c5b8a6
Efimov Vasily <real@ispras.ru>
2026-05-12 13:33:46 +0300

    c2t/tests/c_cast_f*: size part must match size of compared variables
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- c2t/tests/c_cast_f32_3.c
+++ c2t/tests/c_cast_f32_3.c
@@ -4,8 +4,8 @@
 
 void main(void)
 {
-    volatile float a = 20363956191232.0;
-    volatile int64_t b;
+    volatile double a = 1370898521487.079;
+    volatile int32_t b;
 
     b = a;
     FLUSH_PIPELINE;
