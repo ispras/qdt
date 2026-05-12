12a25f7cdacff21c8de7ed12b21b7de7ac959f0c
Efimov Vasily <real@ispras.ru>
2026-05-12 15:34:46 +0300

    misc/floats.c: more examples
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- misc/floats.c
+++ misc/floats.c
@@ -8,6 +8,11 @@ void print_float(float f)
     printf("%e %u 0x%x", f, u, u);
 }
 
+#define U32_F(U) ({ \
+    uint32_t _U32_F_tmp_u = (U); \
+    *(float*)&_U32_F_tmp_u; \
+    })
+
 #define PRINT_FOP(A, OP, B) do { \
     float _PRINT_FOP_c = (A) OP (B); \
     print_float(A); \
@@ -21,6 +26,11 @@ void print_float(float f)
     PRINT_FOP(__VA_ARGS__); \
     printf("\n"); \
 } while ((0))
+
+#define U64_D(U) ({ \
+    uint64_t _U64_D_tmp_u = (U); \
+    *(double*)&_U64_D_tmp_u; \
+    })
 
 void print_double(double f)
 {
@@ -43,10 +53,18 @@ void print_double(double f)
     printf("\n"); \
 } while ((0))
 
+#define NL printf("\n")
+
 void main(void)
 {
     PRINTLN_FOP(-1.448528141878569e-08, /, -72498544640.0);
     PRINTLN_DOP(-4.562228484338099e+252, -, 1.370898521487079e+56);
     PRINTLN_DOP(2.5774648840236446e-169, +, -1.7333702738031197e+296);
+    PRINTLN_DOP(U64_D(0xbf72a7ddffffffff), *, U64_D(0xbc0f1b5e20000000));
+    PRINTLN_FOP(U32_F(0xbf72a7dd), *, U32_F(0xbf870a00));
+    PRINTLN_FOP(U32_F(0x40000000), -, U32_F(0x3f800000));
+    print_double((double)(U32_F(0xb278daf1)));
+    NL;
+    PRINTLN_DOP(U32_F(0xbf870a00), *, U32_F(0xbf72a7dd));
 }
 
