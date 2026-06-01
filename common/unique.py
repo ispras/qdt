4c6ffecb1cafcf2db225b126ba42f838fcfa0c93
Efimov Vasily <real@ispras.ru>
2026-06-01 19:12:38 +0300

    common.unique: `iter_unique_tuples`
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- common/unique.py
+++ common/unique.py
@@ -1,7 +1,13 @@
 __all__ = [
     "iter_unique",
     "list_of_unique"
+  , "iter_unique_tuples"
 ]
+
+
+from .ordered_default_dict import (
+    OrderedDefaultDict,
+)
 
 
 def iter_unique(i):
@@ -17,3 +23,28 @@ def iter_unique(i):
 
 def list_of_unique(i):
     return list(iter_unique(i))
+
+
+def iter_unique_tuples(pairs):
+    d = OrderedDefaultDict(list)
+    for k, v in pairs:
+        # `v` can be an iterator already
+        i = iter(v)
+        head = next(i)
+        tail = i
+        d[head].append((k, tail))
+    if len(d) == 1:
+        # `head` is common for all, skip it
+        for __, tails in d.items():
+            if len(tails) == 1:
+                yield tails[0][0], tuple()
+                continue
+            for k, v in iter_unique_tuples(tails):
+                yield k, v
+    else:
+        for head, tails in d.items():
+            if len(tails) == 1:
+                yield tails[0][0], (head,)
+                continue
+            for k, v in iter_unique_tuples(tails):
+                yield k, (head,) + v
