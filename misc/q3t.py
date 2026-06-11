f8b1cfa117e70a9ece9c6216950007f94120d13f
Efimov Vasily <real@ispras.ru>
2026-06-11 17:54:46 +0300

    q3t: batch of `q3t_*` script helpers
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- misc/q3t.py
+++ misc/q3t.py
@@ -20,7 +20,11 @@ from debug import (
 from argparse import (
     ArgumentParser,
 )
+from itertools import (
+    starmap,
+)
 from functools import (
+    lru_cache,
     wraps,
 )
 from os.path import (
@@ -77,6 +81,30 @@ def q3t_join_be(*uu, **kw):
         res |= u
     return res
 
+def q3t_jbe8(*a, **kw):
+    kw["shift"] = 8
+    return q3t_join_be(*a, **kw)
+
+def q3t_jbe16(*a, **kw):
+    kw["shift"] = 16
+    return q3t_join_be(*a, **kw)
+
+def q3t_jbe32(*a, **kw):
+    kw["shift"] = 32
+    return q3t_join_be(*a, **kw)
+
+def q3t_jle8(*a, **kw):
+    kw["shift"] = 8
+    return q3t_join_le(*a, **kw)
+
+def q3t_jle16(*a, **kw):
+    kw["shift"] = 16
+    return q3t_join_le(*a, **kw)
+
+def q3t_jle32(*a, **kw):
+    kw["shift"] = 32
+    return q3t_join_le(*a, **kw)
+
 def q3t_join_le(*uu, **kw):
     return q3t_join_be(*reversed(uu), **kw)
 
@@ -94,6 +122,30 @@ def q3t_iter_be(*a, **kw):
 
 def q3t_be(*a, **kw):
     return tuple(q3t_iter_be(*a, **kw))
+
+def q3t_be8(*a, **kw):
+    kw["shift"] = 8
+    return q3t_be(*a, **kw)
+
+def q3t_be16(*a, **kw):
+    kw["shift"] = 16
+    return q3t_be(*a, **kw)
+
+def q3t_be32(*a, **kw):
+    kw["shift"] = 32
+    return q3t_be(*a, **kw)
+
+def q3t_le8(*a, **kw):
+    kw["shift"] = 8
+    return q3t_le(*a, **kw)
+
+def q3t_le16(*a, **kw):
+    kw["shift"] = 16
+    return q3t_le(*a, **kw)
+
+def q3t_le32(*a, **kw):
+    kw["shift"] = 32
+    return q3t_le(*a, **kw)
 
 def q3t_bits(*shifts):
     res = 0
@@ -125,6 +177,80 @@ def q3t_neg80(i):
 
 def q3t_neg128(i):
     return q3t_negN(i, 128)
+
+def q3t_o(bitsize, i):
+    "integer overflow emulation in python"
+    return i & ((1 << bitsize) - 1)
+
+def q3t_o8(i):
+    return q3t_o(8, i)
+
+def q3t_o16(i):
+    return q3t_o(16, i)
+
+def q3t_o32(i):
+    return q3t_o(32, i)
+
+def q3t_o40(i):
+    return q3t_o(40, i)
+
+def q3t_o64(i):
+    return q3t_o(64, i)
+
+def q3t_o80(i):
+    return q3t_o(80, i)
+
+def q3t_o128(i):
+    return q3t_o(128, i)
+
+def q3t_vo8(v):
+    return type(v)(map(q3t_o8, v))
+
+def q3t_vo16(v):
+    return type(v)(map(q3t_o16, v))
+
+def q3t_vo32(v):
+    return type(v)(map(q3t_o32, v))
+
+def q3t_vo40(v):
+    return type(v)(map(q3t_o40, v))
+
+def q3t_vo64(v):
+    return type(v)(map(q3t_o64, v))
+
+def q3t_vo128(v):
+    return type(v)(map(q3t_o128, v))
+
+@lru_cache
+def op_lambda(op):
+    return eval("lambda a, b : a %s b" % op)
+
+def q3t_vop(op, va, vb):
+    return type(va)(starmap(op_lambda(op), zip(va, vb)))
+
+def q3t_vadd(*vv):
+    return q3t_vop("+", *vv)
+
+def q3t_vsub(*vv):
+    return q3t_vop("-", *vv)
+
+def q3t_vmul(*vv):
+    return q3t_vop("*", *vv)
+
+def q3t_vfdiv(*vv):
+    return q3t_vop("/", *vv)
+
+def q3t_vidiv(*vv):
+    return q3t_vop("//", *vv)
+
+def q3t_vand(*vv):
+    return q3t_vop("&", *vv)
+
+def q3t_vor(*vv):
+    return q3t_vop("|", *vv)
+
+def q3t_vxor(*vv):
+    return q3t_vop("^", *vv)
 
 class Q3T(object):
     "Qemu Target Test Tool configuration"
