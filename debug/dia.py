558e486c149f4fe399a97db4efefd2bd44888312
Efimov Vasily <real@ispras.ru>
2026-06-19 16:14:07 +0300

    DWARFInfoAccelerator.iter_CUs_by_reversed_path: use `debug..rpath2path`
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- debug/dia.py
+++ debug/dia.py
@@ -9,6 +9,10 @@ from common import (
     trie_find,
     intervalmap
 )
+from .rpath import (
+    rpath2path,
+)
+
 from os.path import (
     join
 )
@@ -459,12 +463,12 @@ pyelftools's `DWARFInfo`.
                 return d[None]
 
             raise ValueError("Given name suffix %s is not long enough to look"
-                " CU up. There are several CUs with such suffix." % join(
-                    *reversed(rpath)
+                " CU up. There are several CUs with such suffix." % rpath2path(
+                    rpath
                 )
             )
 
-        raise KeyError("No CU with name suffix %s" % join(*reversed(rpath)))
+        raise KeyError("No CU with name suffix %s" % rpath2path(rpath))
 
     def iter_CUs(self):
         idx2cu = self.idx2cu
