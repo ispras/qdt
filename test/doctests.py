36e96c102fb2ee025867e16bd366737dbd1313d7
Efimov Vasily <real@ispras.ru>
2026-07-17 21:36:38 +0300

    doctests: embed `common.diag_xy`
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- test/doctests.py
+++ test/doctests.py
@@ -1,5 +1,6 @@
 from common import (
     attr_change_notifier,
+    diag_xy,
     hex_stream,
     intervalmap,
     line_no_stream,
@@ -17,6 +18,7 @@ from unittest import (
 def load_tests(loader, tests, ignore):
     tests.addTests(map(DocTestSuite, [
         attr_change_notifier,
+        diag_xy,
         offsets_stream,
         line_no_stream,
         intervalmap.__module__,
