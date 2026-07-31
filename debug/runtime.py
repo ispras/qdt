52149eca2c6edcb1ea29b7c7298ea6582811d785
Efimov Vasily <real@ispras.ru>
2026-08-01 01:50:36 +0300

    debug..Runtime: `exit`, see __doc__
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- debug/runtime.py
+++ debug/runtime.py
@@ -199,6 +199,11 @@ not actual now.
 
         return val
 
+    def exit(self):
+        "exit `co_run_target` during next or current breakpoint"
+        self._exiting = True
+        self.pause()
+
     def co_run_target(self):
         target = self.target
 
